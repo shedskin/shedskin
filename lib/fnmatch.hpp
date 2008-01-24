85deed17899a6a434a02fe762acc3055e34a7eb8
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2008-01-24 19:34:40 +0000

    bootstrapped glob and fnmatch (ghop; thanks!!)
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@67 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- lib/fnmatch.hpp
+++ lib/fnmatch.hpp
@@ -2,14 +2,24 @@
 #define __FNMATCH_HPP
 
 #include "builtin.hpp"
+#include "os/path.hpp"
+#include "os/__init__.hpp"
+#include "re.hpp"
 
 using namespace __shedskin__;
 namespace __fnmatch__ {
 
+extern str *const_10, *const_11, *const_12, *const_13, *const_14, *const_15, *const_16, *const_17, *const_18, *const_5, *const_6, *const_7, *const_8, *const_9;
 
-void __init();
-int fnmatch(str *f, str *p);
-int fnmatchcase(str *f, str *p);
+extern dict<str *, __re__::re_object *> *_cache;
+extern str *__name__;
+
+int fnmatch(str *name, str *pat);
+list<str *> *filter(list<str *> *names, str *pat);
+int fnmatchcase(str *name, str *pat);
+str *translate(str *pat);
+
+void __init(void);
 
 } // module namespace
 #endif
---
+++ lib/glob.hpp
@@ -0,0 +1,27 @@
+#ifndef __GLOB_HPP
+#define __GLOB_HPP
+
+#include "builtin.hpp"
+#include "os/path.hpp"
+#include "fnmatch.hpp"
+#include "re.hpp"
+#include "os/__init__.hpp"
+
+using namespace __shedskin__;
+namespace __glob__ {
+
+extern str *const_0, *const_2, *const_3;
+
+extern str *__name__;
+extern __re__::re_object *magic_check;
+
+list<str *> *glob(str *pathname);
+__iter<str *> *iglob(str *pathname);
+list<str *> *glob1(str *dirname, str *pattern);
+list<str *> *glob0(str *dirname, str *basename);
+int has_magic(str *s);
+
+void __init(void);
+
+} // module namespace
+#endif
