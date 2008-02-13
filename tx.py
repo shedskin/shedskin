6799351ace35064467b2d9241f57d89df2cf03b4
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2008-02-13 11:00:53 +0000

    multi-core test script
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@104 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

---
+++ tx.py
@@ -0,0 +1,16 @@
+import sys, os
+
+total = 181
+parts, part = map(int, sys.argv[1:])
+
+tests = range(total)[part::parts]
+
+os.system('rm -fR tx%d' % part)
+os.system('mkdir tx%d' % part)
+os.system('cp -R lib tx%d' % part)
+os.system('cp -R testdata tx%d' % part)
+os.system('cp *.py tx%d' % part)
+os.system('cp FLAGS tx%d' % part)
+os.system('cd tx%d' % part)
+
+os.system('python unit.py -l %s' % ' '.join(map(str, tests)))
