d5c3eed33474d52163c78e2de64e4d68c2959393
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2008-02-18 19:53:47 +0000

    bump version number
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@145 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- lib/sys.cpp
+++ lib/sys.cpp
@@ -15,10 +15,10 @@ int hexversion, maxint;
 void __init(int c, char **v) {
     argv = new list<str *>();
 
-    version = new str("Shed Skin Python-to-C++ Compiler 0.0.26\n[GCC ");
+    version = new str("Shed Skin Python-to-C++ Compiler 0.0.27\n[GCC ");
     version = version->__add__(new str(__VERSION__))->__add__(new str("]"));
-    version_info = new tuple2<int, int>(5, 0, 0, 26, 0, 0);
-    hexversion = 0x0001900;
+    version_info = new tuple2<int, int>(5, 0, 0, 27, 0, 0);
+    hexversion = 0x0001a00;
 
     copyright = new str("Copyright (c) Mark Dufour 2005-2008.\nAll Rights Reserved.");
     platform = new str("linux2");
