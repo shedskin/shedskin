eeb335fa4761561841ee78b680cdafedcb7a4e3f
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2008-06-06 07:04:03 +0000

    revert to /bin/sh
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@252 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ import sys, os
 
 # create compiler startup script
 
-if sys.platform != 'win32':
-    ss = file('shedskin','w')
-    ss.write('#!/bin/bash\n')
-    ss.write('SHEDSKIN_ROOT="%s"\n' % os.getcwd()) 
-    ss.write('%s "$SHEDSKIN_ROOT/ss.py" $*\n' % sys.executable)
-    ss.close()
+ss = file('shedskin','w')
+ss.write('#!/bin/sh\n')
+ss.write('SHEDSKIN_ROOT="%s"\n' % os.getcwd()) 
+ss.write('%s "$SHEDSKIN_ROOT/ss.py" $*\n' % sys.executable)
+ss.close()
 
-    os.system('chmod a+wrx shedskin')
-
+os.system('chmod a+wrx shedskin')
