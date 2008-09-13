a515cd657688215d8c7edd41390dd88935969d7f
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2008-09-13 07:36:50 +0000

    minor adjustments
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@349 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- scripts/testprogs.py
+++ scripts/testprogs.py
@@ -3,18 +3,17 @@ import os
 def do_cmd(cmd):
     print '\n\n', cmd, '\n'
     assert os.system(cmd) == 0
-    # XXX segfaults, ss error code?
 
 files = ['neural1.py', 'mask_alg.py', 'mandelbrot.py', 'sudoku3.py', 'pystone.py', 'nbody.py', 'genetic.py', 'richards.py', 'voronoi.py', 'oliva2.py', 'sieve.py', 'linalg.py', 'brainfuck.py', 'pisang.py', 'sudoku2.py', 'life.py', 'sudoku1.py', 'othello.py', 'chess.py', 'msp_ss.py', 'pygmy.py', 'tictactoe.py', 'yopyra.py', 'dijkstra.py', 'amaze.py', 'neural2.py'] # XXX fysphun.py
 
-print 'examples: %d' % len(files)
+print 'examples: %d' % len(set(files))
 
 # cmd-line options
 
 print '*** cmd-line options:'
 
-#do_cmd('shedskin -b othello && make run')
-#do_cmd('shedskin -w othello && make run')
+do_cmd('shedskin -b othello && make run')
+do_cmd('shedskin -w othello && make run')
 
 # ss-progs
 
