cba356bf94b9df8f4ddcf88f3f0667ae205c65ca
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2008-12-19 11:48:45 +0000

    array implementation used to compile rdb.py
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@499 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

---
+++ examples/lib/array.py
@@ -0,0 +1,26 @@
+
+class array:
+    def __init__(self, flags, arg=None):
+        pass
+
+    def fromstring(self, s):
+        pass
+    def fromlist(self, l):
+        pass
+    def fromfile(self, f, n):
+        pass
+    def tolist(self):
+        return [1]
+    def tofile(self, f):
+        pass
+    def tostring(self):
+        return ''
+            
+    def __slice__(self, x, l, u, s):  
+        return self
+    def __len__(self):
+        return 1
+
+    def __delete__(self, x, a=1, b=1, s=1):
+        pass
+
