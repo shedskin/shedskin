08b50eec12afe394435ae3c598e18efa62396470
Mark Dufour <mark.dufour@gmail.com>
2008-12-26 11:28:16 +0000

    TonyJpegDecoder main()
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@512 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- examples/TonyJpegDecoder.py
+++ examples/TonyJpegDecoder.py
@@ -1,3 +1,5 @@
+import sys
+
 M_SOF0  = 0xc0
 M_SOF1  = 0xc1
 M_SOF2  = 0xc2
@@ -1134,8 +1136,74 @@ class TonyJpegDecoder:
     else:
       return nOffset
 
+def dw2c(word):
+  return chr(word & 0xff) + chr((word >> 8) & 0xff) + chr((word >> 16) & 0xff) + chr((word >> 24) & 0xff)
+
+def w2c(word):
+  return chr(word & 0xff) + chr((word >> 8) & 0xff)
+
+class BMPFile:
+  def __init__(self, width, height, rgbstr):
+    self.data = rgbstr
+    self.width = width
+    self.height = height
+
+  def __str__(self):
+    return self.getheader() + self.getinfoheader() + self.getcolortable() + self.data
+
+  def getheader(self):
+    return "BM" + dw2c(self.filesize()) + dw2c(0) + dw2c(self.dataoffset())
+
+  def filesize(self):
+    return self.dataoffset() + self.imagesize()
+
+  def dataoffset(self):
+    headerlen = 14
+    infoheaderlen = 40
+    colortablelen = 0
+    return headerlen + infoheaderlen + colortablelen
+
+  def imagesize(self):
+    """compressed size of image"""
+    return len(self.data)
+
+  def getinfoheader(self):
+    planes = 1
+    bitcount = 24
+    compression = 0
+    xpixelsperm = 1
+    ypixelsperm = 1
+    colorsused = 0
+    colorsimportant = 0 # all
+    return dw2c(40) + dw2c(self.width) + dw2c(self.height) + w2c(planes) + w2c(bitcount) + dw2c(compression) + dw2c(self.imagesize()) + dw2c(xpixelsperm) + dw2c(ypixelsperm) + dw2c(colorsused) + dw2c(colorsimportant)
+
+  def getcolortable(self):
+    # blank for 24bit color
+    return ""
+
+def bgr2rgb(bmpstr):
+  return "".join([bmpstr[i*3+2]+bmpstr[i*3+1]+bmpstr[i*3] for i in range(len(bmpstr)/3)])
+
+def padrgb(bmpstr):
+  return "".join([bmpstr[i*3:i*3+3] + chr(0) for i in range(len(bmpstr)/3)])
+
+def avg(chrs):
+  if chrs:
+    return chr(int(sum([ord(c) for c in chrs])/len(chrs)))
+  else:
+    return ""
+
+def main():
+    inputfile = open(sys.argv[1], 'rb')
+    jpgsrc = inputfile.read()
+    inputfile.close()
+    decoder = TonyJpegDecoder()
+    bmpout = decoder.DecompressImage(jpgsrc)
+    bmpstr = "".join([chr(x) for x in bmpout])
+    bmpstr2 = bgr2rgb(bmpstr)
+    bmp = BMPFile(decoder.Width, decoder.Height, bmpstr)
+    bmpfile = sys.argv[2]
+    open(bmpfile, "wb").write(str(bmp))
 
 if __name__ == '__main__':
-    decoder = TonyJpegDecoder()
-    jpgsrc = ''
-    bmpout = decoder.DecompressImage(jpgsrc)
+    main()
