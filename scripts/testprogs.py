04712f0de53080c119614424186148a153cd044b
Mark Dufour <mark.dufour@gmail.com>
2008-12-29 14:37:46 +0000

    update test scripts
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@527 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- scripts/testprogs.py
+++ scripts/testprogs.py
@@ -5,7 +5,7 @@ def do_cmd(cmd):
     print '\n\n', cmd, '\n'
     assert os.system(cmd) == 0
 
-files = ['neural1.py', 'mask_alg.py', 'mandelbrot.py', 'sudoku3.py', 'pystone.py', 'nbody.py', 'genetic.py', 'richards.py', 'voronoi.py', 'oliva2.py', 'sieve.py', 'linalg.py', 'brainfuck.py', 'pisang.py', 'sudoku2.py', 'life.py', 'sudoku1.py', 'othello.py', 'chess.py', 'msp_ss.py', 'pygmy.py', 'tictactoe.py', 'yopyra.py', 'dijkstra.py', 'amaze.py', 'neural2.py'] # XXX fysphun.py
+files = ['neural1.py', 'mask_alg.py', 'mandelbrot.py', 'sudoku3.py', 'pystone.py', 'nbody.py', 'genetic.py', 'richards.py', 'voronoi.py', 'oliva2.py', 'sieve.py', 'linalg.py', 'brainfuck.py', 'pisang.py', 'sudoku2.py', 'life.py', 'sudoku1.py', 'othello.py', 'chess.py', 'msp_ss.py', 'pygmy.py', 'tictactoe.py', 'yopyra.py', 'dijkstra.py', 'amaze.py', 'neural2.py', 'mastermind', 'rdb', 'TonyJpegDecoder'] # XXX fysphun.py
 
 print 'examples: %d' % len(set(files))
 
--- scripts/tx.py
+++ scripts/tx.py
@@ -1,6 +1,6 @@
 import sys, os
 
-total = 183
+total = 185
 parts, part = map(int, sys.argv[1:])
 
 tests = range(total)[part::parts]
