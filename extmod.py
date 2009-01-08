b6eef81e361fd2674d8dfa7c281171162a568155
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2009-01-08 19:36:00 +0000

    typo
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@577 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- extmod.py
+++ extmod.py
@@ -217,7 +217,7 @@ def do_extmod_class(gv, cl):
         else: 
             print >>gv.out, '        self->__ss_object->%s = __to_ss<%s>(value);' % (gv.cpp_name(var.name), typ)
         print >>gv.out, '    } catch (Exception *e) {'
-        print >>gv.out, '        PyErr_SetString(__to_py(e), ((e->msg)?(e->msg->unit.c_str()):"");'
+        print >>gv.out, '        PyErr_SetString(__to_py(e), ((e->msg)?(e->msg->unit.c_str()):""));'
         print >>gv.out, '        return -1;'
         print >>gv.out, '    }'
 
