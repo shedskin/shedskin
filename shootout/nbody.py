0d8fa4e80f860fd17253fd4297e8173c209643bc
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2009-01-09 10:36:04 +0000

    classes are faster than tuples
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@579 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- shootout/nbody.py
+++ shootout/nbody.py
@@ -7,117 +7,129 @@
 # modified by Tupteq (again)
 
 import sys
+from math import sqrt, pi
 
-pi = 3.14159265358979323
 solar_mass = 4 * pi * pi
-days_per_year = 365.24
+
+def advance(bodies, dt, nbodies):
+    for i in xrange(nbodies):
+        b = bodies[i]
+        b_mass = b.mass
+        b_x = b.x
+        b_y = b.y
+        b_z = b.z
+
+        for j in xrange(i + 1, nbodies):
+            b2 = bodies[j]
+
+            dx = b_x - b2.x
+            dy = b_y - b2.y
+            dz = b_z - b2.z
+
+            distance = sqrt(dx*dx + dy*dy + dz*dz)
+            aux = dt / (distance*distance*distance)
+            b_mass_x_mag = b_mass * aux
+            b2_mass_x_mag = b2.mass * aux
+
+            b.vx -= dx * b2_mass_x_mag
+            b.vy -= dy * b2_mass_x_mag
+            b.vz -= dz * b2_mass_x_mag
+            b2.vx += dx * b_mass_x_mag
+            b2.vy += dy * b_mass_x_mag
+            b2.vz += dz * b_mass_x_mag
+
+    for i in xrange(nbodies):
+        b = bodies[i]
+        b.x += dt * b.vx
+        b.y += dt * b.vy
+        b.z += dt * b.vz
 
 
-def advance(bodies, dt, n):
-    # Create all possible pairs first
-    #pairs = [(b, b2) for i, b in enumerate(bodies) for b2 in bodies[i+1:]] XXX doesnt compile
-    pairs = []
-    for i in range(len(bodies)):
-        for j in range(i+1, len(bodies)):
-            pairs.append((bodies[i], bodies[j]))
-
-    for i in xrange(n):
-        for b, b2 in pairs:
-            dx = b[0] - b2[0]
-            dy = b[1] - b2[1]
-            dz = b[2] - b2[2]
-
-            mag = dt * (dx*dx + dy*dy + dz*dz)**-1.5
-            b_mm = b[6] * mag
-            b2_mm = b2[6] * mag
-
-            b[3] -= dx * b2_mm
-            b[4] -= dy * b2_mm
-            b[5] -= dz * b2_mm
-            b2[3] += dx * b_mm
-            b2[4] += dy * b_mm
-            b2[5] += dz * b_mm
-
-        for b in bodies:
-            b[0] += dt * b[3]
-            b[1] += dt * b[4]
-            b[2] += dt * b[5]
-
-def energy(bodies):
+def energy(bodies, nbodies):
     e = 0.0
-    bodies2 = bodies[1:]
-    for b in bodies:
-        e += 0.5 * b[6] * (b[3]*b[3] + b[4]*b[4] + b[5]*b[5])
-        for b2 in bodies2:
-            dx = b[0] - b2[0]
-            dy = b[1] - b2[1]
-            dz = b[2] - b2[2]
-            distance = (dx*dx + dy*dy + dz*dz)**0.5
-            e -= (b[6] * b2[6]) / distance
-        del bodies2[:1]
+    for i in xrange(nbodies):
+        b = bodies[i]
+        e += 0.5 * b.mass * (b.vx*b.vx + b.vy*b.vy + b.vz*b.vz)
 
+        for j in xrange(i + 1, nbodies):
+            b2 = bodies[j]
+
+            dx = b.x - b2.x
+            dy = b.y - b2.y
+            dz = b.z - b2.z
+            distance = sqrt(dx*dx + dy*dy + dz*dz)
+
+            e -= (b.mass * b2.mass) / distance
     return e
 
-def offset_momentum(bodies):
-    global sun
+
+def offset_momentum(bodies, sun):
     px = py = pz = 0.0
 
     for b in bodies:
-        px -= b[3] * b[6]
-        py -= b[4] * b[6]
-        pz -= b[5] * b[6]
+        px += b.vx * b.mass
+        py += b.vy * b.mass
+        pz += b.vz * b.mass
 
-    sun[3] = px / solar_mass
-    sun[4] = py / solar_mass
-    sun[5] = pz / solar_mass
+    sun.vx = - px / solar_mass
+    sun.vy = - py / solar_mass
+    sun.vz = - pz / solar_mass
 
-sun = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, solar_mass]
 
-jupiter = [
-    4.84143144246472090e+00,
-    -1.16032004402742839e+00,
-    -1.03622044471123109e-01,
-    1.66007664274403694e-03 * days_per_year,
-    7.69901118419740425e-03 * days_per_year,
-    -6.90460016972063023e-05 * days_per_year,
-    9.54791938424326609e-04 * solar_mass]
-
-saturn = [
-    8.34336671824457987e+00,
-    4.12479856412430479e+00,
-    -4.03523417114321381e-01,
-    -2.76742510726862411e-03 * days_per_year,
-    4.99852801234917238e-03 * days_per_year,
-    2.30417297573763929e-05 * days_per_year,
-    2.85885980666130812e-04 * solar_mass]
-
-uranus = [
-    1.28943695621391310e+01,
-    -1.51111514016986312e+01,
-    -2.23307578892655734e-01,
-    2.96460137564761618e-03 * days_per_year,
-    2.37847173959480950e-03 * days_per_year,
-    -2.96589568540237556e-05 * days_per_year,
-    4.36624404335156298e-05 * solar_mass]
-
-neptune = [
-    1.53796971148509165e+01,
-    -2.59193146099879641e+01,
-    1.79258772950371181e-01,
-    2.68067772490389322e-03 * days_per_year,
-    1.62824170038242295e-03 * days_per_year,
-    -9.51592254519715870e-05 * days_per_year,
-    5.15138902046611451e-05 * solar_mass]
-
+class Body:
+    pass
 
 def main():
+    days_per_year = 365.24
+
+    sun = Body()
+    sun.x = sun.y = sun.z = sun.vx = sun.vy = sun.vz = 0.0
+    sun.mass = solar_mass
+
+    jupiter = Body()
+    jupiter.x = 4.84143144246472090e+00
+    jupiter.y = -1.16032004402742839e+00
+    jupiter.z = -1.03622044471123109e-01
+    jupiter.vx = 1.66007664274403694e-03 * days_per_year
+    jupiter.vy = 7.69901118419740425e-03 * days_per_year
+    jupiter.vz = -6.90460016972063023e-05 * days_per_year
+    jupiter.mass = 9.54791938424326609e-04 * solar_mass
+
+    saturn = Body()
+    saturn.x = 8.34336671824457987e+00
+    saturn.y = 4.12479856412430479e+00
+    saturn.z = -4.03523417114321381e-01
+    saturn.vx = -2.76742510726862411e-03 * days_per_year
+    saturn.vy = 4.99852801234917238e-03 * days_per_year
+    saturn.vz = 2.30417297573763929e-05 * days_per_year
+    saturn.mass = 2.85885980666130812e-04 * solar_mass
+
+    uranus = Body()
+    uranus.x = 1.28943695621391310e+01
+    uranus.y = -1.51111514016986312e+01
+    uranus.z = -2.23307578892655734e-01
+    uranus.vx = 2.96460137564761618e-03 * days_per_year
+    uranus.vy = 2.37847173959480950e-03 * days_per_year
+    uranus.vz = -2.96589568540237556e-05 * days_per_year
+    uranus.mass = 4.36624404335156298e-05 * solar_mass
+
+    neptune = Body()
+    neptune.x = 1.53796971148509165e+01
+    neptune.y = -2.59193146099879641e+01
+    neptune.z = 1.79258772950371181e-01
+    neptune.vx = 2.68067772490389322e-03 * days_per_year
+    neptune.vy = 1.62824170038242295e-03 * days_per_year
+    neptune.vz = -9.51592254519715870e-05 * days_per_year
+    neptune.mass = 5.15138902046611451e-05 * solar_mass
+
     n = int(sys.argv[1])
     bodies = [sun, jupiter, saturn, uranus, neptune]
-    offset_momentum(bodies)
-    print "%.9f" % energy(bodies)
-    advance(bodies, 0.01, n)
-    print "%.9f" % energy(bodies)
+    nbodies = len(bodies)
+    offset_momentum(bodies, sun)
+    print round(energy(bodies, nbodies), 9)
 
+    for i in xrange(n):
+        advance(bodies, 0.01, nbodies)
+    print round(energy(bodies, nbodies), 9)
 
 main()
-
