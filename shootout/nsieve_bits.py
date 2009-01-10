ad73e2df5dd6b9e131582c1cb85391839588f679
Mark Dufour <mark.dufour@gmail.com>
2009-01-10 12:02:27 +0000

    shootout: nsieve bits shedskin
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@596 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- shootout/nsieve_bits.py
+++ shootout/nsieve_bits.py
@@ -5,16 +5,16 @@
 # optimized by Giovanni Bajo
 # modified by Heinrich Acker
 
-import sys, itertools
+import sys
 
 def primes_in_range(M):
     bits = [0xFF]*((M + 7) // 8)
     set_bits = [1 << (j+2 & 7) for j in range(8)]
     unset_bits = [~(1 << j) for j in range(8)]
-
     count = 0
-    for i, sb in itertools.izip(xrange(2, M), itertools.cycle(set_bits)):
-        if bits[i>>3] & sb:
+
+    for i in xrange(2, M):
+        if bits[i>>3] & set_bits[(i-2) % 8]:
             for j in xrange(i+i, M, i):
                 bits[j>>3] &= unset_bits[j&7]
             count += 1
