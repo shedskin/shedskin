f69c490e75611fb065b913e9304c23fbb63a4851
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2009-01-16 13:13:28 +0000

    update test script
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@626 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- scripts/testprogs.py
+++ scripts/testprogs.py
@@ -5,7 +5,7 @@ def do_cmd(cmd):
     print '\n\n', cmd, '\n'
     assert os.system(cmd) == 0
 
-files = ['neural1.py', 'mask_alg.py', 'mandelbrot.py', 'sudoku3.py', 'pystone.py', 'nbody.py', 'genetic.py', 'richards.py', 'voronoi.py', 'oliva2.py', 'sieve.py', 'linalg.py', 'brainfuck.py', 'pisang.py', 'sudoku2.py', 'life.py', 'sudoku1.py', 'othello.py', 'chess.py', 'msp_ss.py', 'pygmy.py', 'tictactoe.py', 'yopyra.py', 'dijkstra.py', 'amaze.py', 'neural2.py', 'mastermind', 'rdb', 'TonyJpegDecoder'] # XXX fysphun.py
+files = ['neural1.py', 'mask_alg.py', 'mandelbrot.py', 'sudoku3.py', 'pystone.py', 'nbody.py', 'genetic.py', 'richards.py', 'voronoi.py', 'oliva2.py', 'sieve.py', 'linalg.py', 'brainfuck.py', 'pisang.py', 'sudoku2.py', 'life.py', 'sudoku1.py', 'othello.py', 'chess.py', 'msp_ss.py', 'pygmy.py', 'tictactoe.py', 'yopyra.py', 'dijkstra.py', 'amaze.py', 'neural2.py', 'mastermind.py', 'rdb.py', 'TonyJpegDecoder.py'] # XXX fysphun.py
 
 print 'examples: %d' % len(set(files))
 
@@ -35,7 +35,10 @@ for (i, file) in enumerate(files):
         options += ' -i'
 
     do_cmd('shedskin %s %s' % (options.strip(), file)) 
-    do_cmd('make run')
+    if file in ['mastermind.py', 'rdb.py', 'TonyJpegDecoder.py']: 
+        do_cmd('make')
+    else:
+        do_cmd('make run')
     do_cmd('shedskin -e %s %s' % (options.strip(), file))
     do_cmd('make')
 
