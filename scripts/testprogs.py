8748e1e0fc00daf71eace8116d72ce42f5d79558
Mark Dufour <mark.dufour@gmail.com>
2009-07-02 09:08:02 +0000

    update testprogs script
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@798 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- scripts/testprogs.py
+++ scripts/testprogs.py
@@ -5,7 +5,7 @@ def do_cmd(cmd):
     print '\n\n', cmd, '\n'
     assert os.system(cmd) == 0
 
-files = ['neural1.py', 'mask_alg.py', 'mandelbrot.py', 'sudoku3.py', 'pystone.py', 'nbody.py', 'genetic.py', 'richards.py', 'voronoi.py', 'oliva2.py', 'sieve.py', 'linalg.py', 'brainfuck.py', 'pisang.py', 'sudoku2.py', 'life.py', 'sudoku1.py', 'othello.py', 'chess.py', 'pygmy.py', 'tictactoe.py', 'yopyra.py', 'dijkstra.py', 'amaze.py', 'neural2.py', 'mastermind.py', 'rdb.py', 'TonyJpegDecoder.py', 'mao.py', 'sudo.py', 'mastermind2.py', 'minilight.py', 'circle.py', 'voronoi2', 'ant'] # XXX fysphun
+files = ['neural1.py', 'mask_alg.py', 'mandelbrot.py', 'sudoku3.py', 'pystone.py', 'nbody.py', 'genetic.py', 'richards.py', 'voronoi.py', 'oliva2.py', 'sieve.py', 'linalg.py', 'brainfuck.py', 'pisang.py', 'sudoku2.py', 'life.py', 'sudoku1.py', 'othello.py', 'chess.py', 'pygmy.py', 'tictactoe.py', 'yopyra.py', 'dijkstra.py', 'amaze.py', 'neural2.py', 'mastermind.py', 'rdb.py', 'TonyJpegDecoder.py', 'mao.py', 'sudo.py', 'mastermind2.py', 'minilight.py', 'circle.py', 'voronoi2', 'ant', 'LZ2', 'ac_encode', 'block'] # XXX fysphun
 
 if sys.platform != 'win32':
     files.extend(['msp_ss.py'])
@@ -38,7 +38,7 @@ for (i, file) in enumerate(files):
         options += ' -r'
 
     do_cmd('shedskin %s %s' % (options.strip(), file)) 
-    if file in ['mastermind.py', 'rdb.py', 'TonyJpegDecoder.py', 'voronoi2']: 
+    if file in ['neural1', 'mastermind.py', 'rdb.py', 'TonyJpegDecoder.py', 'voronoi2']: 
         do_cmd('make')
     else:
         do_cmd('make run')
