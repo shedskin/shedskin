231fed53e176c841ef57f7e6b135cb0fee157b28
Mark Dufour <mark.dufour@gmail.com>
2009-07-08 17:52:44 +0000

    gtp interface to go player
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@840 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

---
+++ examples/engine.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import sys
+import gtp
+import go
+
+class Engine:
+    name = 'Disco'
+
+    def __init__(self):
+        self.board = go.Board()
+
+    def boardsize(self, size):
+        if int(size) != 9:
+            raise 'illegal board size'
+
+    def clear_board(self):
+        pass
+
+    def komi(self, value):
+        pass
+
+    def play(self, color, vertex):
+        vertex = gtp.parse_vertex(vertex)
+        if vertex is None:
+            return
+        i, j = vertex
+        pos = go.to_pos(i, j)
+        self.board.play_move(pos)
+
+#    def undo(self):
+#        pass
+
+    def genmove(self, color):
+        options = [pos for pos in self.board.empties if self.board.legal_move(pos) and self.board.useful_move(pos)]
+        if not options:
+            return 'pass'
+        pos, score = go.pgo(self.board.history, options)
+        self.board.play_move(pos)
+        x,y = go.to_xy(pos)
+        return gtp.make_vertex(x, y)
+
+#    def showboard(self):
+#        pass
+
+def main():
+    engine = Engine()
+    gtp.run(engine)
+
+if __name__ == '__main__':
+    main()
