ff77d72637f0378321c5ac578339c8a0c9f3d80c
Mark Dufour <mark.dufour@gmail.com>
2009-08-27 18:18:33 +0000

    here comes the new engine
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@906 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- examples/go.py
+++ examples/go.py
@@ -1,15 +1,15 @@
-import random
-import math
+import random, math
 
 SIZE = 9
-GAMES = 10000
-KOMI = 7.5
+GAMES = 100000
 WHITE, BLACK, EMPTY = 0, 1, 2
 SHOW = {EMPTY: '.', WHITE: 'o', BLACK: 'x'}
 PASS = -1
+MOVES = 0
+TIMESTAMP = 0
 
 def to_pos(x,y):
-    return y*SIZE+x
+    return y * SIZE + x
 
 def to_xy(pos):
     y, x = divmod(pos, SIZE)
@@ -20,388 +20,219 @@ class Square:
         self.board = board
         self.pos = pos
         self.color = EMPTY
-        self.group = None
-        self.fast_group = Group(board)
+        self.timestamp = TIMESTAMP
 
     def set_neighbours(self): 
         x, y = self.pos % SIZE, self.pos / SIZE;
         self.neighbours = []
-        for dx, dy in [(-1,0), (1,0), (0,-1), (0,1)]:
-            newx, newy = x+dx, y+dy
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            newx, newy = x + dx, y + dy
             if 0 <= newx < SIZE and 0 <= newy < SIZE:
                 self.neighbours.append(self.board.squares[to_pos(newx, newy)])
+
+    def move(self, color):
+        global MOVES
+        MOVES += 1
+        self.color = color
+        self.reference = self
+        self.ledges = 0
+        for neighbour in self.neighbours:
+            if neighbour.color == EMPTY: 
+                self.ledges += 1
+            else:
+                neighbour_ref = neighbour.find()
+                if neighbour.color == color:
+                    if neighbour_ref.reference != self:
+                        self.ledges += neighbour_ref.ledges 
+                        neighbour_ref.reference = self
+                    self.ledges -= 1
+                else:
+                    neighbour_ref.ledges -= 1
+                    if neighbour_ref.ledges == 0:
+                        neighbour.remove(neighbour_ref)
+
+    def remove(self, reference):
+        self.color = EMPTY
+        self.board.emptyset.add(self.pos)
+        for neighbour in self.neighbours:
+            if neighbour.color != EMPTY:
+                neighbour_ref = neighbour.find()
+                if neighbour_ref == reference:
+                    neighbour.remove(reference)
+                else:
+                    neighbour_ref.ledges += 1
+
+    def find(self): 
+        if self.reference.pos != self.pos:
+            self.reference = self.reference.find()
+        return self.reference
+
+    def __repr__(self):
+        return repr(to_xy(self.pos))
+ 
+class EmptySet:
+    def __init__(self, board):
+        self.board = board
+        self.empties = range(SIZE*SIZE)
+        self.empty_pos = range(SIZE*SIZE)
+
+    def random_choice(self):
+        choices = len(self.empties)
+        while choices:  
+            i = int(random.random()*choices)
+            pos = self.empties[i]
+            if self.board.useful_move(pos):
+                return pos
+            choices -= 1
+            self.set(i, self.empties[choices])
+            self.set(choices, pos)
+        return PASS
+
+    def add(self, pos):
+        self.empty_pos[pos] = len(self.empties)
+        self.empties.append(pos)
+
+    def remove(self, pos):
+        self.set(self.empty_pos[pos], self.empties[len(self.empties)-1])
+        self.empties.pop()
+
+    def set(self, i, pos): 
+        self.empties[i] = pos
+        self.empty_pos[pos] = i
 
 class Board:
     def __init__(self):
         self.squares = [Square(self, pos) for pos in range(SIZE*SIZE)]
         for square in self.squares:
             square.set_neighbours()
-        self.empties = range(SIZE*SIZE)
-        self.empty_pos = range(SIZE*SIZE)
+        self.reset()
+
+    def reset(self):
+        for square in self.squares:
+            square.color = EMPTY
+        self.emptyset = EmptySet(self)
         self.color = BLACK
         self.finished = False
         self.lastmove = -2
-        self.history = []
-        self.white_dead = 0
-        self.black_dead = 0
-
-    def legal_move(self, pos):
-        """ determine if move is legal """
-        square = self.squares[pos]
-        if square.color != EMPTY:
-            return False
-        other = 1-self.color
-        for neighbour in square.neighbours:
-            ncolor = neighbour.color
-            if ncolor == EMPTY:
-                return True
-            elif ncolor == self.color:
-                liberties = len(neighbour.group.liberties)
-                if liberties >= 2:
-                    return True
-            elif ncolor == other:
-                members = len(neighbour.group.members)
-                liberties = len(neighbour.group.liberties)
-                if liberties == 1 and not (neighbour.pos == self.lastmove and members == 1):
-                    return True
-        return False
-
-    def useful_move(self, pos):
-        """ determine whether move can be useful """
-        square = self.squares[pos]
-        buddies = 0
-        for neighbour in square.neighbours:
-            if neighbour.color == self.color:
-                if len(neighbour.group.liberties) == 1:
-                    return True
-                buddies += 1
-        return buddies != len(square.neighbours)
 
     def random_move(self):
-        """ return random, possibly useful move """
-        choices = len(self.empties)
-        while choices:  
-            i = random.randrange(choices)
-            pos = self.empties[i]
-            if self.legal_move(pos) and self.useful_move(pos):
-                return pos
-            choices -= 1
-            self.set_empty(i, self.empties[choices])
-            self.set_empty(choices, pos)
-        return PASS
-  
-    def possible_moves(self):
-        """ legal and useful moves """
-        return [pos for pos in self.empties if self.legal_move(pos) and self.useful_move(pos)]
+        return self.emptyset.random_choice()
 
-    def new_group(self, square): # XXX to Group class
-        group = square.group = square.fast_group
-        group.color = square.color
-        group.members.clear()
-        group.members.add(square.pos)
-        group.liberties.clear()
+    def useful_move(self, pos): 
+        global TIMESTAMP
+        TIMESTAMP += 1
+        square = self.squares[pos]
+        opps = weak_opps = neighs = weak_neighs = 0
         for neighbour in square.neighbours:
             if neighbour.color == EMPTY:
-                group.liberties.add(neighbour.pos)
-
-    def score(self, color):
-        """ score according to chinese rules (area, instead of territory) """ 
-        if color == WHITE:
-            count = KOMI + self.black_dead
-        else:
-            count = self.white_dead
-        for square in self.squares:
-            if square.color == color:
-                count += 1
-            elif square.color == EMPTY:
-                surround = 0
-                for neighbour in square.neighbours:
-                    if neighbour.color == color:
-                        surround += 1
-                if surround == len(square.neighbours): 
-                    count += 1
-        return count
+                return True
+            neighbour_ref = neighbour.find()
+            if neighbour_ref.timestamp != TIMESTAMP:
+                if neighbour.color == self.color:  
+                    neighs += 1
+                else: 
+                    opps += 1
+                neighbour_ref.timestamp = TIMESTAMP
+                neighbour_ref.temp_ledges = neighbour_ref.ledges
+            neighbour_ref.temp_ledges -= 1
+            if neighbour_ref.temp_ledges == 0:
+                if neighbour.color == self.color:  
+                    weak_neighs += 1
+                else:
+                    weak_opps += 1
+        strong_neighs = neighs-weak_neighs
+        strong_opps = opps-weak_opps
+        return weak_opps or (strong_neighs and (strong_opps or weak_neighs))
 
     def play_move(self, pos):
-        """ administer move or PASS """
+        square = self.squares[pos]
         if pos != PASS:
-            self._move(pos, self.color)
+            self.move(square)
         elif self.lastmove == PASS:
             self.finished = True
-        self.color = 1-self.color
+        self.color = 1 - self.color
         self.lastmove = pos
-        self.history.append(pos)
 
-    def _move(self, pos, color):
-        """ actual move: update groups, empties, remove captives """
-        square = self.squares[pos]
-        other = 1-color
-        # connect to surrounding stones
-        prev_group = None
-        for neighbour in square.neighbours:
-            group = neighbour.group
-            if neighbour.color == color:
-                if square.pos in group.liberties:
-                    group.liberties.remove(square.pos)
-                if prev_group:
-                    if prev_group != group:
-                        group.add_group(prev_group)
-                else:
-                    group.add_stone(square)
-                prev_group = group
-
-        # fill in square
-        square.color = color
-        self.remove_empty(pos)
-        if not prev_group:
-            self.new_group(square)
-
-        # remove captives
-        for neighbour in square.neighbours:
-            if neighbour.color == other:
-                neighbour.group.take_liberty(square)
-
-    def add_empty(self, pos):
-        self.empty_pos[pos] = len(self.empties)
-        self.empties.append(pos)
-
-    def remove_empty(self, pos):
-        self.set_empty(self.empty_pos[pos], self.empties[len(self.empties)-1])
-        self.empties.pop()
-
-    def set_empty(self, i, pos): 
-        self.empties[i] = pos
-        self.empty_pos[pos] = i
-
-    def replay(self, history):
-        """ replay steps """
-        for pos in history:
-            self.play_move(pos)
+    def move(self, square):
+        square.move(self.color)
+        self.emptyset.remove(square.pos)
 
     def __repr__(self):
         result = []
         for y in range(SIZE):
             start = to_pos(0, y)
             result.append(''.join([SHOW[square.color]+' ' for square in self.squares[start:start+SIZE]]))
-        result.append('W %s B %s (%s)' % (self.score(WHITE), self.score(BLACK), SHOW[self.color]))
         return '\n'.join(result)
 
-class Group:
-    def __init__(self, board):
-        self.board = board
-        self.color = -1
-        self.members = set()
-        self.liberties = set()
+def check(board):
+   for square in board.squares:
+       if square.color == EMPTY:
+           continue
 
-    def take_liberty(self, square):
-        if square.pos in self.liberties:
-            self.liberties.remove(square.pos)
-        if not self.liberties: 
-            other = 1-self.color
-            for member in self.members:
-                self.board.add_empty(member)
-            if self.color == BLACK:
-                self.board.black_dead += len(self.members)
-            else:
-                self.board.white_dead += len(self.members)
-            for pos in self.members:
-                square = self.board.squares[pos]
-                square.color = EMPTY
-                for neighbour in square.neighbours:
-                    if neighbour.color == other:
-                        neighbour.group.liberties.add(pos)
+       members1 = set([square])
+       changed = True
+       while changed:
+           changed = False
+           for member in members1.copy():
+               for neighbour in member.neighbours:
+                   if neighbour.color == square.color and neighbour not in members1:
+                       changed = True
+                       members1.add(neighbour)
+       ledges1 = 0
+       for member in members1:
+           for neighbour in member.neighbours:
+               if neighbour.color == EMPTY:
+                   ledges1 += 1
 
-    def add_stone(self, square):
-        self.members.add(square.pos)
-        square.group = self
-        for neighbour in square.neighbours:
-            if neighbour.color == EMPTY:
-                self.liberties.add(neighbour.pos)
-        
-    def add_group(self, group):
-        for pos in group.members:
-            self.board.squares[pos].group = self
-        self.members.update(group.members)
-        self.liberties.update(group.liberties)
+   #    print 'members1', square, members1
+   #    print 'ledges1', ledges1
 
-class UCTNode:
-    def __init__(self):
-        self.bestchild = None
-        self.pos = -1
-        self.wins = 0
-        self.losses = 0
-        self.pos_child = [None for x in range(SIZE*SIZE)]
-        self.parent = None
+       members2 = set()
+       root = square.find()
+       for square2 in board.squares:
+           if square2.color != EMPTY and square2.find() == root:
+               members2.add(square2)
 
-    def play(self, board, options=None):
-        """ uct tree search """
-        color = board.color
-        node = self
-        path = [node]
-        while True:
-            pos = node.select(board)
-            if pos == PASS:
-                break
-            board.play_move(pos)
+       ledges2 = root.ledges
+   #    print 'members2', square, members1
+   #    print 'ledges2', ledges2
 
-            child = node.pos_child[pos]
-            if not child:
-                child = node.pos_child[pos] = UCTNode()
-                child.unexplored = board.possible_moves()
-                child.pos = pos
-                child.parent = node
-                path.append(child)
-                break
+       assert ledges1 == ledges2
+       assert members1 == members2
 
-            path.append(child)
-            node = child
+   empties1 = set(board.emptyset.empties)
+#    print 'empties1', empties1
 
-        self.random_playout(board)
-        self.update_path(board, color, path)
+   empties2 = set()
+   for square in board.squares:
+       if square.color == EMPTY:
+           empties2.add(square.pos)
 
-    def select(self, board):
-        """ select move; unexplored children first, then according to uct value """
-        if self.unexplored:
-            i = random.randrange(len(self.unexplored))
-            pos = self.unexplored[i]
-            self.unexplored[i] = self.unexplored[len(self.unexplored)-1]
-            self.unexplored.pop()
-            return pos
-        elif self.bestchild:
-            return self.bestchild.pos
-        else:
-            return PASS
+#    print 'empties2', empties2
 
-    def random_playout(self, board):
-        """ random play until both players pass """
-        for x in range(1000): # XXX while not self.finished?
+   assert empties1 == empties2
+
+def main():
+    random.seed(1)
+    board = Board()
+    for game in range(GAMES):
+#        print 'GAME'
+        board.reset()
+        for x in range(1000): 
             if board.finished:
                 break
-#            print 'checking'
-#            for i, pos in enumerate(board.empties):
-#                if board.empty_pos[pos] != i:
-#                    print 'FOUT %d staat op %d, maar volgens empty_pos op %d' % (pos, i, board.empty_pos[pos])
-#                    import sys
-#                    sys.exit()
             pos = board.random_move()
+#            print 'TURN', SHOW[board.color]
+#            if pos == PASS:
+#                print 'PASS'
+#            else:
+#                print 'SELECT', to_xy(pos)
             board.play_move(pos)
-
-    def update_path(self, board, color, path):
-        """ update win/loss count along path """
-        global treedepth
-        treedepth = max(treedepth, len(path))
-        wins = board.score(BLACK) >= board.score(WHITE)
-        for node in path:
-            color = 1-color
-            if wins == (color == BLACK):
-                node.wins += 1
-            else:
-                node.losses += 1
-            if node.parent:
-                node.parent.bestchild = node.parent.best_child()
-
-    def score(self):
-        winrate = self.wins/float(self.wins+self.losses)
-        parentvisits = self.parent.wins+self.parent.losses
-        if not parentvisits:
-            return winrate
-        nodevisits = self.wins+self.losses
-        return winrate + math.sqrt((math.log(parentvisits))/(5*nodevisits))
-
-    def best_child(self):
-        maxscore = -1
-        maxchild = None
-        for child in self.pos_child:
-            if child and child.score() > maxscore:
-                maxchild = child
-                maxscore = child.score()
-        return maxchild
-
-    def best_visited(self):
-        maxvisits = -1
-        maxchild = None
-        for child in self.pos_child:
-#            if child:
-#                print to_xy(child.pos), child.wins, child.losses, child.score()
-            if child and (child.wins+child.losses) > maxvisits:
-                maxvisits, maxchild = (child.wins+child.losses), child
-        return maxchild
-
-def user_move(board):
-    while True:
-        text = raw_input('?').strip()
-        if text == 'p':
-            return PASS
-        if text == 'q':
-            raise EOFError
-        try:
-            x, y = [int(i) for i in text.split()]
-        except ValueError:
-            continue
-        if not (0 <= x < SIZE and 0 <= y < SIZE):
-            continue
-        pos = to_pos(x, y)
-        if board.legal_move(pos):
-            return pos
-
-def computer_move(board):
-    global treedepth
-    treedepth = 0
-    pos = board.random_move()
-    if pos == PASS:
-        return PASS
-    history = board.history[:]
-    tree = UCTNode()
-    tree.unexplored = board.possible_moves()
-    for game in range(GAMES):
-        node = tree
-        nboard = Board()
-        nboard.replay(history)
-        node.play(nboard)
-    return tree.best_visited().pos
-
-def pgo(history, options):
-    tree = UCTNode()
-    tree.unexplored = options
-    for game in range(GAMES):
-        node = tree
-        nboard = Board()
-        nboard.replay(history)
-        node.play(nboard, options)
-    best = tree.best_visited()
-    return best.pos, (best.wins+best.losses)
-
-def versus_cpu():
-    board = Board()
-    while True:
-        if board.lastmove != PASS:
-            print board
-        print 'thinking..'
-        pos = computer_move(board)
-        print 'tree depth:', treedepth
-        if pos == PASS:
-            print 'I pass.'
-        else:
-            print 'I move here:', to_xy(pos)
-        board.play_move(pos)
-        if board.finished:
-            break
-        if board.lastmove != PASS:
-            print board
-        pos = user_move(board)
-        board.play_move(pos)
-        if board.finished:
-            break
-    print 'WHITE:', board.score(WHITE)
-    print 'BLACK:', board.score(BLACK)
-
-if False: # type model for extmod
-    pgo([1], [1])
-    UCTNode().play(None, [1])
+#            print x
+#            print board
+#            print
+#            check(board)
+    print 'MOVES', MOVES
 
 if __name__ == '__main__':
-    random.seed(1)
-    try:
-        versus_cpu()
-    except EOFError:
-        pass
+    main()
