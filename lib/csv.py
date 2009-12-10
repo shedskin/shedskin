7c53150af76fb653e254c48aa34b4eb94bd3a884
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2009-12-10 09:54:26 +0000

    enough for basic example
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@1033 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

---
+++ lib/csv.py
@@ -0,0 +1,12 @@
+class reader:
+    def __init__(self, csvfile):
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        self.count += 1
+        if self.count == 5:
+            raise StopIteration
+        return ['hoei', 'hop', 18, 'hurk']
