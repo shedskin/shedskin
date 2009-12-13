e0fff114191dbb64de36c68624a7f6ddaab9a6c6
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2009-12-13 16:08:36 +0000

    next are csv.dictreader/writer
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@1063 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- lib/csv.hpp
+++ lib/csv.hpp
@@ -6,33 +6,44 @@
 using namespace __shedskin__;
 namespace __csv__ {
 
-extern str *const_0, *const_1, *const_2, *const_3, *const_4, *const_5, *const_6, *const_7, *const_8, *const_9;
+extern str *const_0, *const_1, *const_10, *const_11, *const_12, *const_13, *const_14, *const_15, *const_16, *const_17, *const_18, *const_19, *const_2, *const_3, *const_4, *const_5, *const_6, *const_7, *const_8, *const_9;
 
-class Dialect;
+class Error;
+class Excel;
 class reader;
 class writer;
 
 extern str *__name__;
 extern list<int> *__0, *__1;
+extern int EAT_CRNL, ESCAPED_CHAR, ESCAPE_IN_QUOTED_FIELD, IN_FIELD, IN_QUOTED_FIELD, QUOTE_ALL, QUOTE_IN_QUOTED_FIELD, QUOTE_MINIMAL, QUOTE_NONE, QUOTE_NONNUMERIC, START_FIELD, START_RECORD, _field_limit;
 extern OSError *__exception;
-extern int EAT_CRNL, ESCAPED_CHAR, ESCAPE_IN_QUOTED_FIELD, IN_FIELD, IN_QUOTED_FIELD, QUOTE_ALL, QUOTE_IN_QUOTED_FIELD, QUOTE_MINIMAL, QUOTE_NONE, QUOTE_NONNUMERIC, START_FIELD, START_RECORD, field_limit;
-extern Dialect *excel;
 
-extern class_ *cl_Dialect;
-class Dialect : public pyobj {
+extern class_ *cl_Error;
+class Error : public Exception {
 public:
-    str *escapechar;
+
+    Error() {}
+    Error(str *msg) {
+        this->__class__ = cl_Error;
+        __init__(msg);
+    }
+};
+
+extern class_ *cl_Excel;
+class Excel : public pyobj {
+public:
+    str *lineterminator;
     int skipinitialspace;
     int quoting;
     int strict;
     str *delimiter;
-    str *lineterminator;
+    str *escapechar;
     str *quotechar;
     int doublequote;
 
-    Dialect() {}
-    Dialect(int __ss_init) {
-        this->__class__ = cl_Dialect;
+    Excel() {}
+    Excel(int __ss_init) {
+        this->__class__ = cl_Excel;
         __init__();
     }
     void *__init__();
@@ -48,49 +59,65 @@ public:
 extern class_ *cl_reader;
 class reader : public pyiter<list<str *> *> {
 public:
-    Dialect *dialect;
-    int state;
-    list<str *> *fields;
+    Excel *dialect;
     int line_num;
+    list<str *> *fields;
+    int state;
     list<str *> *field;
+    int numeric_field;
     file *input_iter;
 
     reader() {}
-    reader(file *input_iter) {
+    reader(file *input_iter, str *dialect, str *delimiter, str *quotechar, int doublequote, int skipinitialspace, str *lineterminator, int quoting, str *escapechar, int strict) {
         this->__class__ = cl_reader;
-        __init__(input_iter);
+        __init__(input_iter, dialect, delimiter, quotechar, doublequote, skipinitialspace, lineterminator, quoting, escapechar, strict);
     }
-    int parse_process_char(str *c);
+    void *parse_process_char(str *c);
+    void *parse_reset();
     list<str *> *next();
     __csviter *__iter__();
-    int parse_add_char(str *c);
-    int parse_save_field();
-    void *parse_reset();
-    void *__init__(file *input_iter);
+    void *__init__(file *input_iter, str *dialect, str *delimiter, str *quotechar, int doublequote, int skipinitialspace, str *lineterminator, int quoting, str *escapechar, int strict);
+    void *parse_save_field();
+    void *parse_add_char(str *c);
 };
 
 extern class_ *cl_writer;
 class writer : public pyobj {
 public:
-    Dialect *dialect;
+    Excel *dialect;
     int num_fields;
     file *output_file;
     list<str *> *rec;
 
     writer() {}
-    writer(file *output_file) {
+    writer(file *output_file, str *dialect, str *delimiter, str *quotechar, int doublequote, int skipinitialspace, str *lineterminator, int quoting, str *escapechar, int strict) {
         this->__class__ = cl_writer;
-        __init__(output_file);
+        __init__(output_file, dialect, delimiter, quotechar, doublequote, skipinitialspace, lineterminator, quoting, escapechar, strict);
     }
     int join_append_data(str *field, int quote_empty, int quoted);
     void *writerow(list<str *> *seq);
     void *join_reset();
     void *writerows(list<list<str *> *> *seqs);
     int join_append(str *field, int quoted, int quote_empty);
-    void *__init__(file *output_file);
+    void *__init__(file *output_file, str *dialect, str *delimiter, str *quotechar, int doublequote, int skipinitialspace, str *lineterminator, int quoting, str *escapechar, int strict);
 };
 
+extern void * default_5;
+extern void * default_6;
+extern void * default_3;
+extern void * default_4;
+extern void * default_8;
+extern void * default_1;
+extern void * default_2;
+extern void * default_9;
+extern void * default_7;
+extern void * default_10;
+extern void * default_0;
+
 void __init();
+list<str *> *list_dialects();
+Excel *_get_dialect(str *name, str *delimiter, str *quotechar, int doublequote, int skipinitialspace, str *lineterminator, int quoting, str *escapechar, int strict);
+int field_size_limit(int new_limit);
 
 } // module namespace
 #endif
--- lib/csv.py
+++ lib/csv.py
@@ -1,13 +1,28 @@
+START_RECORD = START_FIELD = ESCAPED_CHAR = IN_FIELD = IN_QUOTED_FIELD = ESCAPE_IN_QUOTED_FIELD = QUOTE_IN_QUOTED_FIELD = EAT_CRNL = 0
+QUOTE_MINIMAL = QUOTE_ALL = QUOTE_NONNUMERIC = QUOTE_NONE = 0
+
+class Error(Exception):
+    pass
+
+def list_dialects():
+    return ['excel', 'excel-tab']
+
+def field_size_limit(new_limit=-1):
+    return new_limit
+
 class reader:
-    def __init__(self, csvfile):
-        self.csvfile = csvfile
+    def __init__(self, input_iter, dialect=None, delimiter=None, quotechar=None, doublequote=-1, skipinitialspace=-1, lineterminator=None, quoting=-1, escapechar=None, strict=-1):
+        pass
 
     def __iter__(self):
         return __iter([''])
 
+    def next(self):
+        return ['']
+
 class writer:
-    def __init__(self, csvfile):
-        self.csvfile = csvfile
+    def __init__(self, output_file, dialect=None, delimiter=None, quotechar=None, doublequote=-1, skipinitialspace=-1, lineterminator=None, quoting=-1, escapechar=None, strict=-1):
+        pass
 
     def writerow(self, seq):
         pass
