20f87c64f32c1885fd81fa14a83b3ebce5966937
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2009-12-14 20:03:42 +0000

    most of csv.{DictReader, DictWriter}
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@1066 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- lib/csv.cpp
+++ lib/csv.cpp
@@ -2,23 +2,40 @@
 
 namespace __csv__ {
 
-str *const_0, *const_1, *const_10, *const_11, *const_12, *const_13, *const_14, *const_15, *const_16, *const_17, *const_18, *const_19, *const_2, *const_3, *const_4, *const_5, *const_6, *const_7, *const_8, *const_9;
+tuple2<str *, str *> *const_3;
+str *const_1, *const_10, *const_11, *const_12, *const_13, *const_14, *const_15, *const_16, *const_17, *const_18, *const_19, *const_2, *const_20, *const_21, *const_22, *const_23, *const_24, *const_25, *const_26, *const_27, *const_4, *const_5, *const_6, *const_7, *const_8, *const_9;
+list<void *> *const_0;
 
 str *__name__;
 list<int> *__0, *__1;
 int EAT_CRNL, ESCAPED_CHAR, ESCAPE_IN_QUOTED_FIELD, IN_FIELD, IN_QUOTED_FIELD, QUOTE_ALL, QUOTE_IN_QUOTED_FIELD, QUOTE_MINIMAL, QUOTE_NONE, QUOTE_NONNUMERIC, START_FIELD, START_RECORD, _field_limit;
 OSError *__exception;
 
-void * default_5;
-void * default_6;
-void * default_3;
-void * default_4;
-void * default_8;
-void * default_1;
-void * default_2;
-void * default_9;
 void * default_7;
+void * default_3;
+void * default_9;
+void * default_21;
 void * default_10;
+void * default_11;
+void * default_13;
+void * default_15;
+void * default_17;
+void * default_24;
+str * default_19;
+str * default_20;
+void * default_22;
+str * default_18;
+void * default_25;
+void * default_1;
+void * default_6;
+void * default_4;
+void * default_5;
+void * default_12;
+void * default_14;
+void * default_16;
+void * default_23;
+void * default_8;
+void * default_2;
 void * default_0;
 
 __csviter::__csviter(reader *r) {
@@ -31,6 +48,50 @@ list<str *> *__csviter::next() {
 
 __csviter *reader::__iter__() {
     return new __csviter(this);
+}
+
+__driter::__driter(DictReader *r) {
+    this->r = r;
+}
+
+dict<str *, str *> *__driter::next() {
+    return r->next();
+}
+
+__driter *DictReader::__iter__() {
+    return new __driter(this);
+}
+
+static inline list<str *> *list_comp_0(DictWriter *self, dict<str *, str *> *rowdict) {
+    __iter<str *> *__35;
+    str *k;
+    int __36;
+    dict<str *, str *> *__34;
+    list<str *> *__ss_result = new list<str *>();
+
+    FOR_IN(k,rowdict,35)
+        if ((!(self->fieldnames)->__contains__(k))) {
+            __ss_result->append(k);
+        }
+    END_FOR
+
+    return __ss_result;
+}
+
+static inline list<str *> *list_comp_1(DictWriter *self, dict<str *, str *> *rowdict) {
+    list<str *> *__37;
+    __iter<str *> *__38;
+    int __39;
+    str *key;
+    list<str *> *__ss_result = new list<str *>();
+
+    __37 = self->fieldnames;
+    __ss_result->resize(len(__37));
+    FOR_IN_SEQ(key,__37,37,39)
+        __ss_result->units[__39] = rowdict->get(key, self->restval);
+    END_FOR
+
+    return __ss_result;
 }
 
 /**
@@ -47,11 +108,11 @@ class_ *cl_Excel;
 
 void *Excel::__init__() {
     
-    this->delimiter = const_0;
-    this->quotechar = const_1;
+    this->delimiter = const_4;
+    this->quotechar = const_5;
     this->doublequote = 1;
     this->skipinitialspace = 0;
-    this->lineterminator = const_2;
+    this->lineterminator = const_6;
     this->quoting = QUOTE_MINIMAL;
     this->escapechar = ((str *)(NULL));
     this->strict = 0;
@@ -70,19 +131,19 @@ void *reader::parse_process_char(str *c) {
 
     dialect = this->dialect;
     if ((this->state==START_RECORD)) {
-        if (__eq(c, const_3)) {
+        if (__eq(c, const_7)) {
             return NULL;
         }
-        else if ((const_4)->__contains__(c)) {
+        else if ((const_8)->__contains__(c)) {
             this->state = EAT_CRNL;
             return NULL;
         }
         this->state = START_FIELD;
     }
     if ((this->state==START_FIELD)) {
-        if ((const_5)->__contains__(c)) {
+        if ((const_9)->__contains__(c)) {
             this->parse_save_field();
-            if (__eq(c, const_3)) {
+            if (__eq(c, const_7)) {
                 this->state = START_RECORD;
             }
             else {
@@ -95,7 +156,7 @@ void *reader::parse_process_char(str *c) {
         else if (__eq(c, dialect->escapechar)) {
             this->state = ESCAPED_CHAR;
         }
-        else if (__AND(__eq(c, const_6), dialect->skipinitialspace, 9)) {
+        else if (__AND(__eq(c, const_10), dialect->skipinitialspace, 9)) {
         }
         else if (__eq(c, dialect->delimiter)) {
             this->parse_save_field();
@@ -109,16 +170,16 @@ void *reader::parse_process_char(str *c) {
         }
     }
     else if ((this->state==ESCAPED_CHAR)) {
-        if (__eq(c, const_3)) {
-            c = const_7;
+        if (__eq(c, const_7)) {
+            c = const_11;
         }
         this->parse_add_char(c);
         this->state = IN_FIELD;
     }
     else if ((this->state==IN_FIELD)) {
-        if ((const_8)->__contains__(c)) {
+        if ((const_12)->__contains__(c)) {
             this->parse_save_field();
-            if (__eq(c, const_3)) {
+            if (__eq(c, const_7)) {
                 this->state = START_RECORD;
             }
             else {
@@ -137,7 +198,7 @@ void *reader::parse_process_char(str *c) {
         }
     }
     else if ((this->state==IN_QUOTED_FIELD)) {
-        if (__eq(c, const_3)) {
+        if (__eq(c, const_7)) {
         }
         else if (__eq(c, dialect->escapechar)) {
             this->state = ESCAPE_IN_QUOTED_FIELD;
@@ -155,8 +216,8 @@ void *reader::parse_process_char(str *c) {
         }
     }
     else if ((this->state==ESCAPE_IN_QUOTED_FIELD)) {
-        if (__eq(c, const_3)) {
-            c = const_7;
+        if (__eq(c, const_7)) {
+            c = const_11;
         }
         this->parse_add_char(c);
         this->state = IN_QUOTED_FIELD;
@@ -170,9 +231,9 @@ void *reader::parse_process_char(str *c) {
             this->parse_save_field();
             this->state = START_FIELD;
         }
-        else if ((const_8)->__contains__(c)) {
+        else if ((const_12)->__contains__(c)) {
             this->parse_save_field();
-            if (__eq(c, const_3)) {
+            if (__eq(c, const_7)) {
                 this->state = START_RECORD;
             }
             else {
@@ -184,17 +245,17 @@ void *reader::parse_process_char(str *c) {
             this->state = IN_FIELD;
         }
         else {
-            throw ((new Error(__modct(const_9, 2, dialect->delimiter, dialect->quotechar))));
+            throw ((new Error(__modct(const_13, 2, dialect->delimiter, dialect->quotechar))));
         }
     }
     else if ((this->state==EAT_CRNL)) {
-        if ((const_4)->__contains__(c)) {
+        if ((const_8)->__contains__(c)) {
         }
-        else if (__eq(c, const_3)) {
+        else if (__eq(c, const_7)) {
             this->state = START_RECORD;
         }
         else {
-            throw ((new Error(const_10)));
+            throw ((new Error(const_14)));
         }
     }
     return 0;
@@ -222,13 +283,13 @@ list<str *> *reader::next() {
         this->line_num = (this->line_num+1);
 
         FOR_IN(c,line,5)
-            if (__eq(c, const_3)) {
-                throw ((new Error(const_11)));
+            if (__eq(c, const_7)) {
+                throw ((new Error(const_15)));
             }
             this->parse_process_char(c);
         END_FOR
 
-        this->parse_process_char(const_3);
+        this->parse_process_char(const_7);
         if ((this->state==START_RECORD)) {
             break;
         }
@@ -249,7 +310,7 @@ void *reader::__init__(file *input_iter, str *dialect, str *delimiter, str *quot
 void *reader::parse_save_field() {
     str *field;
 
-    field = (const_12)->join(this->field);
+    field = (const_16)->join(this->field);
     this->field = (new list<str *>());
     if (this->numeric_field) {
         this->numeric_field = 0;
@@ -261,7 +322,7 @@ void *reader::parse_save_field() {
 void *reader::parse_add_char(str *c) {
     
     if ((len(this->field)>=_field_limit)) {
-        throw ((new Error(__modct(const_13, 1, __box(_field_limit)))));
+        throw ((new Error(__modct(const_17, 1, __box(_field_limit)))));
     }
     (this->field)->append(c);
     return NULL;
@@ -290,7 +351,7 @@ int writer::join_append_data(str *field, int quote_empty, int quoted) {
 
     FOR_IN(c,field,16)
         want_escape = 0;
-        if (__eq(c, const_3)) {
+        if (__eq(c, const_7)) {
             break;
         }
         if (__OR(__eq(c, dialect->delimiter), __OR(__eq(c, dialect->escapechar), __OR(__eq(c, dialect->quotechar), lineterm->__contains__(c), 20), 19), 18)) {
@@ -312,7 +373,7 @@ int writer::join_append_data(str *field, int quote_empty, int quoted) {
             }
             if (want_escape) {
                 if ((!___bool(dialect->escapechar))) {
-                    throw ((new Error(const_14)));
+                    throw ((new Error(const_18)));
                 }
                 (this->rec)->append(dialect->escapechar);
             }
@@ -322,7 +383,7 @@ int writer::join_append_data(str *field, int quote_empty, int quoted) {
 
     if (__AND((!___bool(field)), quote_empty, 22)) {
         if ((dialect->quoting==QUOTE_NONE)) {
-            throw ((new Error(const_15)));
+            throw ((new Error(const_19)));
         }
         else {
             quoted = 1;
@@ -353,7 +414,7 @@ void *writer::writerow(list<str *> *seq) {
             quoted = 1;
         }
         if ((field==NULL)) {
-            quoted = this->join_append(const_12, quoted, (len(seq)==1));
+            quoted = this->join_append(const_16, quoted, (len(seq)==1));
         }
         else {
             quoted = this->join_append(__str(field), quoted, (len(seq)==1));
@@ -361,7 +422,7 @@ void *writer::writerow(list<str *> *seq) {
     END_FOR
 
     (this->rec)->append((this->dialect)->lineterminator);
-    (this->output_file)->write((const_12)->join(this->rec));
+    (this->output_file)->write((const_16)->join(this->rec));
     return NULL;
 }
 
@@ -400,34 +461,162 @@ void *writer::__init__(file *output_file, str *dialect, str *delimiter, str *quo
     return NULL;
 }
 
+/**
+class DictReader
+*/
+
+class_ *cl_DictReader;
+
+void *DictReader::setfieldnames(list<str *> *value) {
+    
+    this->_fieldnames = value;
+    return NULL;
+}
+
+dict<str *, str *> *DictReader::next() {
+    str *key;
+    list<str *> *__31, *row;
+    __iter<str *> *__32;
+    int __33, lf, lr;
+    dict<str *, str *> *d;
+
+    if ((this->line_num==0)) {
+        this->getfieldnames();
+    }
+    row = (this->_reader)->next();
+    this->line_num = (this->_reader)->line_num;
+
+    while (row->empty()) {
+        row = (this->_reader)->next();
+    }
+    d = __dict(__zip2(this->getfieldnames(), row));
+    lf = len(this->getfieldnames());
+    lr = len(row);
+    if ((lf<lr)) {
+        throw ((new Error(const_20)));
+    }
+    else if ((lf>lr)) {
+
+        FOR_IN_SEQ(key,(this->getfieldnames())->__slice__(1, lr, 0, 0),31,33)
+            d->__setitem__(key, this->restval);
+        END_FOR
+
+    }
+    return d;
+}
+
+list<str *> *DictReader::getfieldnames() {
+    
+    if ((this->_fieldnames==NULL)) {
+        try {
+            this->_fieldnames = (this->_reader)->next();
+        } catch (StopIteration *) {
+        }
+    }
+    this->line_num = (this->_reader)->line_num;
+    return this->_fieldnames;
+}
+
+void *DictReader::__init__(file *f, list<str *> *fieldnames, str *restkey, str *restval, void *dialect, str *delimiter, void *quotechar, int doublequote, int skipinitialspace, void *lineterminator, int quoting, void *escapechar, int strict) {
+    
+    this->_fieldnames = fieldnames;
+    this->restkey = restkey;
+    this->restval = restval;
+    this->_reader = (new reader(f, ((str *)(dialect)), delimiter, ((str *)(quotechar)), doublequote, skipinitialspace, ((str *)(lineterminator)), quoting, ((str *)(escapechar)), strict));
+    this->dialect = dialect;
+    this->line_num = 0;
+    return NULL;
+}
+
+/**
+class DictWriter
+*/
+
+class_ *cl_DictWriter;
+
+list<str *> *DictWriter::_dict_to_list(dict<str *, str *> *rowdict) {
+    list<str *> *wrong_fields;
+
+    if (__eq(this->extrasaction, const_1)) {
+        wrong_fields = list_comp_0(this, rowdict);
+        if (___bool(wrong_fields)) {
+            throw ((new ValueError((const_21)->__add__((const_22)->join(wrong_fields)))));
+        }
+    }
+    return list_comp_1(this, rowdict);
+}
+
+void *DictWriter::writerow(dict<str *, str *> *rowdict) {
+    
+    return (this->_writer)->writerow(this->_dict_to_list(rowdict));
+}
+
+void *DictWriter::writerows(list<dict<str *, str *> *> *rowdicts) {
+    __iter<dict<str *, str *> *> *__41;
+    list<list<str *> *> *rows;
+    list<dict<str *, str *> *> *__40;
+    int __42;
+    dict<str *, str *> *rowdict;
+
+    rows = (new list<list<str *> *>());
+
+    FOR_IN_SEQ(rowdict,rowdicts,40,42)
+        rows->append(this->_dict_to_list(rowdict));
+    END_FOR
+
+    return (this->_writer)->writerows(rows);
+}
+
+void *DictWriter::__init__(file *f, list<str *> *fieldnames, str *restval, str *extrasaction, str *dialect, void *delimiter, void *quotechar, int doublequote, int skipinitialspace, void *lineterminator, int quoting, void *escapechar, int strict) {
+    
+    this->fieldnames = fieldnames;
+    this->restval = restval;
+    if ((!(const_3)->__contains__(extrasaction->lower()))) {
+        throw ((new ValueError(__modct(const_23, 1, extrasaction))));
+    }
+    this->extrasaction = extrasaction;
+    this->_writer = (new writer(f, dialect, ((str *)(delimiter)), ((str *)(quotechar)), doublequote, skipinitialspace, ((str *)(lineterminator)), quoting, ((str *)(escapechar)), strict));
+    return NULL;
+}
+
 void __init() {
-    const_0 = new str(",");
-    const_1 = new str("\"");
-    const_2 = new str("\r\n");
-    const_3 = new str("\000", 1);
-    const_4 = new str("\n\r");
-    const_5 = new str("\n\r\000", 3);
-    const_6 = new str(" ");
-    const_7 = new str("\n");
-    const_8 = new str("\000\n\r", 3);
-    const_9 = new str("'%c' expected after '%c'");
-    const_10 = new str("new-line character seen in unquoted field - do you need to open the file in universal-newline mode?");
-    const_11 = new str("line contains NULL byte");
-    const_12 = new str("");
-    const_13 = new str("field larger than field limit (%d)");
-    const_14 = new str("need to escape, but no escapechar set");
-    const_15 = new str("single empty field record must be quoted");
-    const_16 = new str("excel");
-    const_17 = new str("excel-tab");
-    const_18 = new str("\t");
-    const_19 = new str("unknown dialect");
+    const_0 = (new list<void *>());
+    const_1 = new str("raise");
+    const_2 = new str("ignore");
+    const_3 = (new tuple2<str *, str *>(2, const_1, const_2));
+    const_4 = new str(",");
+    const_5 = new str("\"");
+    const_6 = new str("\r\n");
+    const_7 = new str("\000", 1);
+    const_8 = new str("\n\r");
+    const_9 = new str("\n\r\000", 3);
+    const_10 = new str(" ");
+    const_11 = new str("\n");
+    const_12 = new str("\000\n\r", 3);
+    const_13 = new str("'%c' expected after '%c'");
+    const_14 = new str("new-line character seen in unquoted field - do you need to open the file in universal-newline mode?");
+    const_15 = new str("line contains NULL byte");
+    const_16 = new str("");
+    const_17 = new str("field larger than field limit (%d)");
+    const_18 = new str("need to escape, but no escapechar set");
+    const_19 = new str("single empty field record must be quoted");
+    const_20 = new str("no go");
+    const_21 = new str("dict contains fields not in fieldnames: ");
+    const_22 = new str(", ");
+    const_23 = new str("extrasaction (%s) must be 'raise' or 'ignore'");
+    const_24 = new str("excel");
+    const_25 = new str("excel-tab");
+    const_26 = new str("\t");
+    const_27 = new str("unknown dialect");
 
     __name__ = new str("csv");
 
-    cl_writer = new class_("writer", 44, 44);
-    cl_reader = new class_("reader", 3, 3);
-    cl_Excel = new class_("Excel", 43, 43);
+    cl_writer = new class_("writer", 42, 42);
+    cl_DictReader = new class_("DictReader", 1, 1);
+    cl_Excel = new class_("Excel", 4, 4);
+    cl_reader = new class_("reader", 46, 46);
     cl_Error = new class_("Error", 26, 26);
+    cl_DictWriter = new class_("DictWriter", 2, 2);
 
     __0 = range(8);
     START_RECORD = __0->__getfast__(0);
@@ -454,26 +643,41 @@ void __init() {
     default_7 = NULL;
     default_8 = NULL;
     default_9 = NULL;
+    default_10 = NULL;
+    default_11 = NULL;
+    default_12 = NULL;
+    default_13 = NULL;
+    default_14 = NULL;
+    default_15 = NULL;
+    default_16 = NULL;
+    default_17 = NULL;
+    default_18 = const_16;
+    default_19 = const_1;
+    default_20 = const_24;
+    default_21 = NULL;
+    default_22 = NULL;
+    default_23 = NULL;
+    default_24 = NULL;
 }
 
 list<str *> *list_dialects() {
     
-    return (new list<str *>(2, const_16, const_17));
+    return (new list<str *>(2, const_24, const_25));
 }
 
 Excel *_get_dialect(str *name, str *delimiter, str *quotechar, int doublequote, int skipinitialspace, str *lineterminator, int quoting, str *escapechar, int strict) {
     Excel *dialect;
     int __2, __3;
 
-    if (__OR((name==NULL), (__eq(name,const_16)), 2)) {
+    if (__OR((name==NULL), __eq(name, const_24), 2)) {
         dialect = (new Excel(1));
     }
-    else if (__eq(name, const_17)) {
+    else if (__eq(name, const_25)) {
         dialect = (new Excel(1));
-        dialect->delimiter = const_18;
+        dialect->delimiter = const_26;
     }
     else {
-        throw ((new Error(const_19)));
+        throw ((new Error(const_27)));
     }
     if ((delimiter!=NULL)) {
         dialect->delimiter = delimiter;
--- lib/csv.hpp
+++ lib/csv.hpp
@@ -6,12 +6,16 @@
 using namespace __shedskin__;
 namespace __csv__ {
 
-extern str *const_0, *const_1, *const_10, *const_11, *const_12, *const_13, *const_14, *const_15, *const_16, *const_17, *const_18, *const_19, *const_2, *const_3, *const_4, *const_5, *const_6, *const_7, *const_8, *const_9;
+extern tuple2<str *, str *> *const_3;
+extern list<void *> *const_0;
+extern str *const_1, *const_10, *const_11, *const_12, *const_13, *const_14, *const_15, *const_16, *const_17, *const_18, *const_19, *const_2, *const_20, *const_21, *const_22, *const_23, *const_24, *const_25, *const_26, *const_27, *const_4, *const_5, *const_6, *const_7, *const_8, *const_9;
 
 class Error;
 class Excel;
 class reader;
 class writer;
+class DictReader;
+class DictWriter;
 
 extern str *__name__;
 extern list<int> *__0, *__1;
@@ -32,12 +36,12 @@ public:
 extern class_ *cl_Excel;
 class Excel : public pyobj {
 public:
-    str *lineterminator;
+    str *escapechar;
     int skipinitialspace;
     int quoting;
     int strict;
     str *delimiter;
-    str *escapechar;
+    str *lineterminator;
     str *quotechar;
     int doublequote;
 
@@ -60,12 +64,12 @@ extern class_ *cl_reader;
 class reader : public pyiter<list<str *> *> {
 public:
     Excel *dialect;
+    file *input_iter;
     int line_num;
     list<str *> *fields;
-    int state;
     list<str *> *field;
+    int state;
     int numeric_field;
-    file *input_iter;
 
     reader() {}
     reader(file *input_iter, str *dialect, str *delimiter, str *quotechar, int doublequote, int skipinitialspace, str *lineterminator, int quoting, str *escapechar, int strict) {
@@ -102,16 +106,79 @@ public:
     void *__init__(file *output_file, str *dialect, str *delimiter, str *quotechar, int doublequote, int skipinitialspace, str *lineterminator, int quoting, str *escapechar, int strict);
 };
 
-extern void * default_5;
-extern void * default_6;
-extern void * default_3;
-extern void * default_4;
-extern void * default_8;
-extern void * default_1;
-extern void * default_2;
-extern void * default_9;
+class __driter : public __iter<dict<str *, str *> *> {
+public:
+    DictReader *r;
+    __driter(DictReader *reader);
+    dict<str *, str *> *next();
+};
+
+extern class_ *cl_DictReader;
+class DictReader : public pyiter<dict<str *, str *> *> {
+public:
+    str *restval;
+    void *dialect;
+    int line_num;
+    str *restkey;
+    list<str *> *_fieldnames;
+    reader *_reader;
+
+    DictReader() {}
+    DictReader(file *f, list<str *> *fieldnames, str *restkey, str *restval, void *dialect, str *delimiter, void *quotechar, int doublequote, int skipinitialspace, void *lineterminator, int quoting, void *escapechar, int strict) {
+        this->__class__ = cl_DictReader;
+        __init__(f, fieldnames, restkey, restval, dialect, delimiter, quotechar, doublequote, skipinitialspace, lineterminator, quoting, escapechar, strict);
+    }
+    void *setfieldnames(list<str *> *value);
+    dict<str *, str *> *next();
+    __driter *__iter__();
+    list<str *> *getfieldnames();
+    void *__init__(file *f, list<str *> *fieldnames, str *restkey, str *restval, void *dialect, str *delimiter, void *quotechar, int doublequote, int skipinitialspace, void *lineterminator, int quoting, void *escapechar, int strict);
+};
+
+extern class_ *cl_DictWriter;
+class DictWriter : public pyobj {
+public:
+    str *restval;
+    writer *_writer;
+    list<str *> *fieldnames;
+    str *extrasaction;
+
+    DictWriter() {}
+    DictWriter(file *f, list<str *> *fieldnames, str *restval, str *extrasaction, str *dialect, void *delimiter, void *quotechar, int doublequote, int skipinitialspace, void *lineterminator, int quoting, void *escapechar, int strict) {
+        this->__class__ = cl_DictWriter;
+        __init__(f, fieldnames, restval, extrasaction, dialect, delimiter, quotechar, doublequote, skipinitialspace, lineterminator, quoting, escapechar, strict);
+    }
+    list<str *> *_dict_to_list(dict<str *, str *> *rowdict);
+    void *writerow(dict<str *, str *> *rowdict);
+    void *writerows(list<dict<str *, str *> *> *rowdicts);
+    void *__init__(file *f, list<str *> *fieldnames, str *restval, str *extrasaction, str *dialect, void *delimiter, void *quotechar, int doublequote, int skipinitialspace, void *lineterminator, int quoting, void *escapechar, int strict);
+};
+
 extern void * default_7;
+extern void * default_3;
+extern void * default_9;
+extern void * default_21;
 extern void * default_10;
+extern void * default_11;
+extern void * default_13;
+extern void * default_15;
+extern void * default_17;
+extern void * default_24;
+extern str * default_19;
+extern str * default_20;
+extern void * default_22;
+extern str * default_18;
+extern void * default_25;
+extern void * default_1;
+extern void * default_6;
+extern void * default_4;
+extern void * default_5;
+extern void * default_12;
+extern void * default_14;
+extern void * default_16;
+extern void * default_23;
+extern void * default_8;
+extern void * default_2;
 extern void * default_0;
 
 void __init();
