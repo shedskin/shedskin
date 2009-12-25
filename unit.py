61288c05fd816b1a81b86785c2776243f8859ac3
Mark Dufour <mark.dufour@gmail.com>
2009-12-25 15:48:02 +0000

    no support is better than half support
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@1082 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- unit.py
+++ unit.py
@@ -8464,29 +8464,29 @@ output()
 #'''),
 
 ('__class__ and __name__ attributes','''
-class evert:                             # __class__: [class evert]*
-    pass
-
-bla = evert()                            # [evert()]
-
-cl = bla.__class__                       # [class evert]
-
-print cl                                 # [class evert]
-print cl.__name__                        # [str]
-
-print type(bla)                          # [class evert]
-print type(bla).__name__                 # [str]
-
-if cl == type(evert()):                  # [int]
-   print 'equal!'                        # [str]
-
-if type('2') == type('3'):               # [int]
-   print 'equal str!'                    # [str]
-
-if type(1) == type('1'):                 # [int]
-   print 'equal non-equal!'              # [str]
+#class evert:                             # __class__: [class evert]*
+#    pass
+#
+#bla = evert()                            # [evert()]
+#
+#cl = bla.__class__                       # [class evert]
+#
+#print cl                                 # [class evert]
+#print cl.__name__                        # [str]
+#
+#print type(bla)                          # [class evert]
+#print type(bla).__name__                 # [str]
+#
+#if cl == type(evert()):                  # [int]
+#   print 'equal!'                        # [str]
+#
+#if type('2') == type('3'):               # [int]
+#   print 'equal str!'                    # [str]
+#
+#if type(1) == type('1'):                 # [int]
+#   print 'equal non-equal!'              # [str]
 ''', '''
-output('class evert\\nevert\\nclass evert\\nevert\\nequal!\\nequal str!\\n')
+
 '''),
 
 ('mod operator on string and tuple XXX', '''
