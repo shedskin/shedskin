9a54f59b6686151656aaf431940ca4362d842cb2
Mark Dufour <mark.dufour@gmail.com>
2010-01-09 10:55:38 +0000

    finally, distutils support (thomas spura)
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@1142 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-import os
-ss = file('shedskin.py', 'w')
-print >>ss, '''#!/usr/bin/env python
-import sys
-sys.path.append("%s")
-import shedskin
-shedskin.main()
-''' % os.getcwd()
-ss.close()
-os.system('chmod a+wrx shedskin.py')
+
+from distutils.core import setup
+
+setup(name='shedskin',
+      version='0.3',
+      description='Shed Skin is an experimental compiler, that can translate pure, but implicitly statically typed Python programs into optimized C++. It can generate stand-alone programs or extension modules that can be imported and used in larger Python programs.',
+      url='http://code.google.com/p/shedskin/',
+      scripts=['scripts/shedskin'],
+      packages=['shedskin'],
+      package_data={'shedskin': ['lib/*.cpp', 'lib/*.hpp', 'lib/*.py', 'lib/os/*.cpp', 'lib/os/*.hpp', 'lib/os/*.py', 'FLAGS']},
+     )
