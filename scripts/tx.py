dcdbe8e71b98b58bbaeae318c10e0aa36a6ed252
Mark Dufour <mark.dufour@gmail.com>
2010-01-20 18:46:09 +0000

    update tx.py
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@1169 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- scripts/tx.py
+++ scripts/tx.py
@@ -1,16 +1,15 @@
 import sys, os
 
-total = 191
+total = 192
 parts, part = map(int, sys.argv[1:])
 
 tests = range(total)[part::parts]
 
 os.system('rm -fR tx%d' % part)
 os.system('mkdir tx%d' % part)
-os.system('cp -R lib tx%d' % part)
 os.system('cp -R testdata tx%d' % part)
 os.system('cp -R shedskin tx%d' % part)
 os.system('cp unit.py tx%d' % part)
-os.system('cp FLAGS tx%d' % part)
+os.system('cp shedskin/FLAGS tx%d/shedskin' % part)
 
 os.system('cd tx%d; python unit.py -f -l %s' % (part, ' '.join(map(str, tests))))
