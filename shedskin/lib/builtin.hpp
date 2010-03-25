50ecfc35353b635171c177855729ce3747272be3
Mark Dufour <mark.dufour@gmail.com>
2010-03-25 11:57:01 +0000

    64-bit humm
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@1307 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- shedskin/lib/builtin.hpp
+++ shedskin/lib/builtin.hpp
@@ -1439,6 +1439,7 @@ template<> inline __ss_bool ___bool(bool x) { return __mbool(x); }
 template<> inline __ss_bool ___bool(__ss_bool x) { return x; }
 template<> inline __ss_bool ___bool(double x) { return __mbool(x!=0); }
 template<> inline __ss_bool ___bool(void *x) { return False; }
+template<> inline __ss_bool ___bool(long int) { return False; } /* XXX bool(None) 64-bit */
 
 /* logical and, or */
 
