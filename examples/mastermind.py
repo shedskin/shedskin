8f05425c2f05d31a6e60ca1606c1a9805d7f3f75
mark.dufour <mark.dufour@acccb5f4-fd3f-0410-a51f-a7a3fbc0418d>
2010-03-27 12:08:58 +0000

    copyright
    git-svn-id: http://shedskin.googlecode.com/svn/trunk@1327 acccb5f4-fd3f-0410-a51f-a7a3fbc0418d

--- examples/mastermind.py
+++ examples/mastermind.py
@@ -1,5 +1,7 @@
 from mm import mastermind
 
+""" copyright Sean McCarthy, license GPL v2 or later """
+
 def main():
     mastermind.main()
 
--- examples/minilight.py
+++ examples/minilight.py
@@ -1,5 +1,10 @@
 from ml import minilight
 
+'''
+  Copyright (c) 2008, Harrison Ainsworth / HXA7241 and Juraj Sukop.
+  http://www.hxa7241.org/minilight/
+'''
+
 def main():
     minilight.main('ml/cornellbox.txt')
 
