1675c089402e673ee85a2954a17b29c973c30def
Administrator <srepmub@ubuntu.ubuntu-domain>
2010-07-02 10:07:59 +0800

    add unsigned int types to ___box (issue 81)

--- shedskin/lib/builtin.hpp
+++ shedskin/lib/builtin.hpp
@@ -4710,6 +4710,9 @@ template<class T> T ___box(T t) { return t; } /* XXX */
 int_ *___box(__ss_int);
 #endif
 int_ *___box(int);
+int_ *___box(unsigned int); /* XXX */
+int_ *___box(unsigned long);
+int_ *___box(unsigned long long);
 bool_ *___box(__ss_bool);
 float_ *___box(double);
 
