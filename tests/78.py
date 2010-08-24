4a162113bc8b473a97aae96554b54fa6dedd25d7
Mark Dufour <mark.dufour@gmail.com>
2010-08-24 17:06:07 +0200

    improve inheritance support in extmods

--- tests/78.py
+++ tests/78.py
@@ -1,5 +1,5 @@
 
-cnf = ['']                               # [list(str)]
+cnf = ['8']                               # [list(str)]
 
 for x in cnf:                            # [list(str)]
     d = int(x)                               # [int]
