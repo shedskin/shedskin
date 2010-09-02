263ee1f9c707a9183af3317895eedc19c389a7c3
Thomas Spura <tomspur@fedoraproject.org>
2010-09-03 01:28:20 +0200

    Use own implementation of hash function
    This implementation was inspirated from:
    http://www.cplusplus.com/forum/beginner/26747/
    
    Now the hash function from Paul Hsieh can be deleted, so there should be
    no licensing issues anymore.
    
    Signed-off-by: Thomas Spura <tomspur@fedoraproject.org>

--- shedskin/lib/builtin.cpp
+++ shedskin/lib/builtin.cpp
@@ -750,61 +750,14 @@ str *str::__imul__(__ss_int n) {
 
 /* ======================================================================== */
 
-/* (C) Paul Hsieh. http://www.azillionmonkeys.com/qed/{hash,weblicense}.html */
-
-#define get16bits(d) (*((const unsigned short int *) (d)))
-
-static inline unsigned int SuperFastHash (const char * data, int len) {
-    unsigned int hash = 0, tmp;
-    int rem;
-
-    if (len <= 0 || data == NULL) return 0;
-
-    rem = len & 3;
-    len >>= 2;
-
-    /* Main loop */
-    for (;len > 0; len--) {
-        hash  += get16bits (data);
-        tmp    = (get16bits (data+2) << 11) ^ hash;
-        hash   = (hash << 16) ^ tmp;
-        data  += 2*sizeof (unsigned short int);
-        hash  += hash >> 11;
-    }
-
-    /* Handle end cases */
-    switch (rem) {
-        case 3: hash += get16bits (data);
-                hash ^= hash << 16;
-                hash ^= data[sizeof (unsigned short int)] << 18;
-                hash += hash >> 11;
-                break;
-        case 2: hash += get16bits (data);
-                hash ^= hash << 11;
-                hash += hash >> 17;
-                break;
-        case 1: hash += *data;
-                hash ^= hash << 10;
-                hash += hash >> 1;
-    }
-
-    /* Force "avalanching" of final 127 bits */
-    hash ^= hash << 3;
-    hash += hash >> 5;
-    hash ^= hash << 4;
-    hash += hash >> 17;
-    hash ^= hash << 25;
-    hash += hash >> 6;
-
-    return hash;
-}
-
-/* ======================================================================== */
-
 int str::__hash__() {
-    if(hash)
-        return hash;
-    hash = SuperFastHash(unit.c_str(), unit.size());
+    unsigned int hash = 0;
+    const char *c = unit.c_str();
+    int pow = 1;
+    for(int i = 0; i < unit.length(); ++i){
+        pow *= 31;
+        hash += pow * (int)(c[i]);
+    }
     return hash;
 
     //return __gnu_cxx::hash<char *>()(unit.c_str());
