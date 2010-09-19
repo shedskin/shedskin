22a336102466f75a10ea91a934bc68c585b00a9f
Mark Dufour <mark.dufour@gmail.com>
2010-09-19 12:59:05 +0200

    add stuff

---
+++ combine/create_connections.py
@@ -0,0 +1,127 @@
+import random
+import sys
+
+my_file = open('connex','w')
+#layer_sizes = [[3,3,1],[3,3,2],[3,3,2],[3,3,1],[3,1,1]]
+layer_sizes = [[7,7,1],[7,7,2],[3,3,1],[3,1,1]]
+#layer_sizes = [[3,3,1],[3,3,2],[3,3,1],[3,1,1]]
+
+#synsumco = [0.06, 0.431, 0.445 ]
+synsumco = [0.06, 0.431]
+num_layers = len(layer_sizes)
+
+#write layer_sizes to file
+my_file.write(str(len(layer_sizes)))
+for layer in layer_sizes:
+    my_file.write('\n')
+    for elm in layer:
+        my_file.write(str(elm)+ ' ')
+my_file.write('\n')
+
+
+#write synsumco to file
+for syn_sum in synsumco:
+   my_file.write(str(syn_sum) + ' ' )
+my_file.write('\n')
+
+cell_type_ext = 0
+cell_type_in = 1
+
+x_out = 0.015
+in_top = 0.15
+
+total_x_out =0.0
+x_out_num = 0
+
+
+#basic way of connecting layer to layer
+def connect_layers(pre_layer,post_layer,pre_type,post_type,one_to_one,fast,plastic,stren):
+
+    fast = str(int(fast))
+    plastic = str(int(plastic))
+    total_strength = 0.0
+    num_connections = 0
+
+    for y in range(layer_sizes[pre_layer][0]):
+        for x in range(layer_sizes[pre_layer][1]):
+	    if one_to_one:
+		my_file.write(str(pre_type)+' '+str(pre_layer)+' '+str(y)+' ' + str(x) +' ' + str(post_type)+' '+str(post_layer) + ' '+str(y)+' ' + str(x) + ' ' + str(stren) + ' ' + fast + ' ' + plastic + '\n')
+	        total_strength = total_strength + stren
+	        num_connections = num_connections + 1
+	    else:
+		for y_post in range(layer_sizes[post_layer][0]):
+		    for x_post in range(layer_sizes[post_layer][1]):
+			my_file.write(str(pre_type)+' '+str(pre_layer)+' '+str(y)+' ' + str(x) +' ' + str(post_type)+' '+str(post_layer) + ' '+str(y_post)+' ' + str(x_post) + ' ' + str(stren) + ' ' + fast + ' ' + plastic + '\n')
+	                total_strength = total_strength + stren
+	                num_connections = num_connections + 1
+
+    return total_strength, num_connections
+
+#new setup
+#input to top
+#connect_layers(0,1,cell_type_ext,cell_type_ext,True,True,True,in_top)
+#connect_layers(0,1,cell_type_ext,cell_type_in,True,True,True,in_top)
+
+#top to top inhibition
+#connect_layers(1,1,cell_type_in,cell_type_ext,False,True,True,-in_top)
+
+#top to bottum
+#tstren,nconnect=connect_layers(1,2,cell_type_ext,cell_type_ext,False,True,True,in_top)
+#connect_layers(1,2,cell_type_ext,cell_type_in,False,True,True,in_top)
+#connect_layers(1,2,cell_type_in,cell_type_ext,False,True,True,-(tstren/nconnect))
+#connect_layers(1,2,cell_type_in,cell_type_in,False,True,True,-(tstren/nconnect))
+
+#bottum to output
+#tstren,nconnect = connect_layers(2,3,cell_type_ext,cell_type_ext,False,True,True,x_out)
+#print -tstren/nconnect,'in to out s', nconnect,'num ext to out', tstren,'total ext to out strength'
+#connect_layers(2,3,cell_type_in,cell_type_ext,False,True,True,-(tstren/nconnect))
+
+
+#old setup
+#input to top
+connect_layers(0,1,cell_type_ext,cell_type_ext,True,True,True,in_top)
+connect_layers(0,1,cell_type_ext,cell_type_in,True,True,True,in_top)
+	
+#top to output
+tstren,nconnect = connect_layers(1,2,cell_type_ext,cell_type_ext,False,True,True,x_out)
+connect_layers(1,2,cell_type_in,cell_type_ext,False,True,True,-tstren/nconnect)
+	
+
+#specific set ups for special connectivity
+for z in range(num_layers):
+    for y in range(layer_sizes[z][0]):
+        for x in range(layer_sizes[z][1]):
+
+            #this is emotion layer: reward and punish connect to hunger, hunger connects to all to inhibit
+            if z==num_layers-1:
+
+		#reward to hunger
+		if y ==0:
+                    my_file.write(str(cell_type_ext)+' '+str(z)+' '+str(y)+' ' + str(x) +' ' + str(cell_type_ext)+' '+str(z) + ' '+str(2)+' ' + str(x)+' '+ str(-.1)+' 0' + ' 0' +'\n')
+
+		#punish to hunger
+		if y == 1:
+		    my_file.write(str(cell_type_ext)+' '+str(z)+' '+str(y)+' ' + str(x) +' ' + str(cell_type_ext)+' '+str(z) + ' '+str(2)+' ' + str(x)+' '+ str(0.0025)+' 0' + ' 0' + '\n')
+
+		#hunger to others
+		if y == 2:
+		    for yi in range(layer_sizes[1][0]):
+			for xi in range(layer_sizes[1][1]):
+			    print 'create inhibition'
+			    my_file.write(str(cell_type_ext)+' '+str(z)+' '+str(y)+' ' + str(x) +' ' + str(cell_type_ext)+' '+str(1) + ' '+str(yi)+' ' + str(xi)+' '+ str(-0.0015)+' 0' + ' 0' +'\n')
+			    my_file.write(str(cell_type_ext)+' '+str(z)+' '+str(y)+' ' + str(x) +' ' + str(cell_type_in)+' '+str(1) + ' '+str(yi)+' ' + str(xi)+' '+ str(-0.0015)+' 0' + ' 0' +'\n')
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ combine/enviroment.py
@@ -0,0 +1,217 @@
+ #does not support multi-player
+
+import random
+#{
+import pygame
+#}
+
+class Enviroment:
+    def __init__(self,xsize,ysize,num_food,movie,food_type,dist_type):
+        #1 norm food
+        #2 creature
+        #0 blank
+        #3 horz food
+        #4 vert food
+        self.movie = movie
+        self.landscape = [ [0 for x in range(ysize)] for y in range(xsize)]
+        self.xsize = xsize
+        self.ysize = ysize
+        self.food_type = food_type
+        #self.face =pygame.image.load("smile.jpeg")
+        self.update_rects = []
+
+        #{
+        if self.movie:
+            pass
+            self.set_up_display()
+        #}
+
+        if dist_type == "hetero":
+            for i in range(num_food/2):
+                self.gen_vert_obj()
+
+            for i in range(num_food/2):
+                self.gen_horz_obj()
+
+        if dist_type == "homo":
+            for i in range(num_food):
+                self.gen_object(1)
+
+        self.critx, self.crity = self.gen_object(2)
+
+
+    def update_land(self,obj,x,y):
+        self.landscape[x][y] = obj
+#{
+        if self.movie:
+            if obj == 0:
+                self.screen.fill((100,0,0),self.rects[x][y])
+                self.update_rects.append(self.rects[x][y])
+            if obj == 1 or obj == 3 or obj ==4:
+                self.screen.fill((0,255,0),self.rects[x][y])
+                self.update_rects.append(self.rects[x][y])
+            if obj == 2:
+                #self.screen.blit(self.face,(x,y),(10,10,5,5))
+                self.screen.fill((0,0,255),self.rects[x][y])
+                self.update_rects.append(self.rects[x][y])
+  #}          
+
+    def gen_vert_obj(self):
+        while True:
+            x = random.randint(0, self.xsize - 1 )
+            y = random.randint(0,self.ysize-3)
+            if self.landscape[x][y] == 0 and self.landscape[x][y+1] == 0:
+                self.update_land(4,x,y)
+                self.update_land(4,x,y+1)
+                return x,y
+
+    def gen_horz_obj(self):
+        while True:
+            x = random.randint(0, self.xsize - 3 )
+            y = random.randint(0,self.ysize-1)
+            if self.landscape[x][y] == 0 and self.landscape[x+1][y] == 0:
+                self.update_land(3,x,y)
+                self.update_land(3,x+1,y)
+                return x,y
+
+
+    def gen_object(self,obj):
+        while True:
+            x = random.randint(0, self.xsize - 1 )
+            y = random.randint(0,self.ysize-1)
+            if self.landscape[x][y] == 0:
+                self.update_land(obj,x,y)
+                return x,y
+    
+    def wrap_calc(self,x,y):
+        while x >= self.xsize:
+            x = x- self.xsize
+        while x< 0:
+            x = x + self.xsize
+        while y >= self.ysize:
+            y = y- self.ysize
+        while y< 0:
+            y = y + self.ysize
+        return x,y
+
+
+    def teleport(self):
+	while True:
+	    new_x = random.randint(0,self.xsize-1)
+	    new_y = random.randint(0,self.ysize-1)
+	    if self.landscape[new_x][new_y] == 0:
+	        self.update_land(2,new_x,new_y)
+		self.update_land(0,self.critx,self.crity)
+		self.critx = new_x
+		self.crity = new_y
+		return
+
+    def move_crit(self,direction):
+        if direction=='north':
+            newcrit=(self.critx,self.crity-1)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+        if direction=='south':
+            newcrit=(self.critx,self.crity+1)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+        if direction=='east':
+            newcrit=(self.critx+1,self.crity)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+        if direction=='west':
+            newcrit=(self.critx-1,self.crity)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+
+	if direction=='north_west':
+            newcrit=(self.critx-1,self.crity-1)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+	if direction=='south_west':
+            newcrit=(self.critx-1,self.crity+1)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+	if direction=='north_east':
+            newcrit=(self.critx+1,self.crity-1)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+	if direction=='south_east':
+            newcrit=(self.critx+1,self.crity+1)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+	if direction=='null':
+            newcrit=(self.critx,self.crity)
+            newcrit=self.wrap_calc(newcrit[0],newcrit[1])
+
+
+        self.update_land(0,self.critx,self.crity)
+        self.critx = newcrit[0]
+        self.crity = newcrit[1]
+        if self.landscape[self.critx][self.crity]==1:
+            self.update_land(2,self.critx,self.crity)
+            self.gen_object(1)
+            return "bacon"
+
+        
+        if self.landscape[self.critx][self.crity]==3:
+            self.update_land(2,self.critx,self.crity)
+            if self.landscape[self.critx+1][self.crity] == 3:
+                self.update_land(0,self.critx+1,self.crity)
+            if self.landscape[self.critx-1][self.crity] == 3:
+                self.update_land(0,self.critx-1,self.crity)
+            self.gen_horz_obj()
+            if self.food_type == "horz":
+                return "bacon"
+            else:
+                return "grass"
+
+
+        if self.landscape[self.critx][self.crity]==4:
+            self.update_land(2,self.critx,self.crity)
+            if self.landscape[self.critx][self.crity+1] == 4:
+                self.update_land(0,self.critx,self.crity+1)
+            if self.landscape[self.critx][self.crity-1] == 4:
+                self.update_land(0,self.critx,self.crity-1)
+            self.gen_vert_obj()
+            if self.food_type == "vert":
+                return "bacon"
+            else:
+                return "grass"
+
+        self.update_land(2,self.critx,self.crity)
+        return "blank"
+
+        
+    def get_vision(self,xloc, yloc,x_range,y_range):
+        xloc = xloc - x_range
+        yloc = yloc - y_range
+        array = []
+        for i in range((x_range*2)+1):
+            array.append([])
+            for j in range((y_range*2)+1):
+                wxloc,wyloc = self.wrap_calc(xloc+i,yloc+j)
+                array[i].append(self.landscape[wxloc][wyloc])
+        return array
+
+#{
+    def set_up_display(self):
+         self.window = pygame.display.set_mode((1200, 1200)) 
+         self.screen = pygame.display.get_surface() 
+         self.rects = []
+         self.clock = pygame.time.Clock()
+         for i in range(self.xsize):
+            self.rects.append([])
+            for j in range(self.ysize):
+                 self.rects[i].append( (i*6,j*6,5,5))
+ 
+         for i in range(self.xsize):
+             for j in range(self.ysize):
+                 self.screen.fill((100,0,0),self.rects[i][j])
+                 self.update_rects.append(self.rects[i][j])
+
+
+
+    def display(self,extra_rects):
+         self.clock.tick(10000)
+         self.update_rects.extend(extra_rects)
+         pygame.display.update(self.update_rects)
+         self.update_rects = []
+#}
+
+
+    
+        
+        
