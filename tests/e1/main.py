99b2647ceeab39c4aca2d965f82f007fe754fa2e
Mark Dufour <mark.dufour@gmail.com>
2010-10-12 20:31:00 +0200

    change tests/e1 to test pickle improvement

--- tests/e1/main.py
+++ tests/e1/main.py
@@ -1,6 +1,8 @@
-#Matrix4
-import Matrix4
-v = Matrix4.Vector4(1,2,3,4)
-m = Matrix4.Matrix4([v,v,v,v])
-w = Matrix4.hoppa(v)
-print w.x, w.y, w.z, w.w
+#blah
+import blah as test
+import pickle
+
+blah = test.Blah(7, 'eight')
+s = pickle.dumps(blah)
+obj = pickle.loads(s)
+print obj.a, obj.b
