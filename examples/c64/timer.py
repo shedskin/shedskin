290ef9d11905bd3d651ea912b90a50b15733fbeb
Mark Dufour <mark.dufour@gmail.com>
2010-10-17 09:53:59 +0200

    remove unneeded stuff

--- examples/c64/timer.py
+++ examples/c64/timer.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python2
 # I, Danny Milosavljevic, hereby place this file into the public domain.
 
-#import gobject
+import gobject
 
 class Timer(object):
 	def fire_timer(self):
 		return True
 
 def timeout_add(timeout_ms, obj):
-	pass # FIXME return gobject.timeout_add(timeout_ms, fn)
-	return 42
+	return gobject.timeout_add(timeout_ms, obj.fire_timer)
 
 def timeout_remove(ID):
-	pass # FIXME
-	#return gobject.source_remove(ID)
+	return gobject.source_remove(ID)
