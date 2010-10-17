42b283fd44c275a7aeeb4f22acb2948b6e15baef
Mark Dufour <mark.dufour@gmail.com>
2010-10-17 10:27:24 +0200

    more retab

--- examples/c64/vic_ii.py
+++ examples/c64/vic_ii.py
@@ -57,130 +57,130 @@ A_COLOR_SPRITE_6 = 0x2D
 A_COLOR_SPRITE_7 = 0x2E
 
 class VIC_II(memory.Memory):
-	def __init__(self, MMU, CIA2, char_ROM):
-		self.B_can_write = True # in the instance because of ShedSkin
-		self.raster_counter = 0
-		self.MMU = MMU
-		self.char_ROM = char_ROM
-		self.CIA2 = CIA2
-		self.B_active = True
-		self.B_bitmap = False
-		self.B_clip_address = False # FIXME default?
-		self.B_MCM = False # FIXME default?
-		self.text_view = None
-		#self.set_control_1(0) # FIXME default.
-		#self.set_control_2(0) # FIXME default.
+    def __init__(self, MMU, CIA2, char_ROM):
+        self.B_can_write = True # in the instance because of ShedSkin
+        self.raster_counter = 0
+        self.MMU = MMU
+        self.char_ROM = char_ROM
+        self.CIA2 = CIA2
+        self.B_active = True
+        self.B_bitmap = False
+        self.B_clip_address = False # FIXME default?
+        self.B_MCM = False # FIXME default?
+        self.text_view = None
+        #self.set_control_1(0) # FIXME default.
+        #self.set_control_2(0) # FIXME default.
 
-	def increase_raster_position(self):
-		self.raster_counter += 5 # 1
-		if self.raster_counter >= 255: # FIXME how could this get > 255?
-			self.raster_counter = 0
-		return True
+    def increase_raster_position(self):
+        self.raster_counter += 5 # 1
+        if self.raster_counter >= 255: # FIXME how could this get > 255?
+            self.raster_counter = 0
+        return True
 
-	def unprepare(self):
-		self.text_view.unprepare()
+    def unprepare(self):
+        self.text_view.unprepare()
 
-	def set_control_1(self, value):
-		self.text_view.first_row = 51 if value & 8 else 55
-		self.text_view.last_row = 250 if value & 8 else 246
-		self.text_view.viewport_row = value & 7
-		value & 16 # DEN
-		self.B_bitmap = (value & 32) != 0 # BMM
-		self.B_clip_address = (value & 64) != 0 # ECM # bits 9 and 10 low.
-		value & 128 # RST8
-		self.text_view
+    def set_control_1(self, value):
+        self.text_view.first_row = 51 if value & 8 else 55
+        self.text_view.last_row = 250 if value & 8 else 246
+        self.text_view.viewport_row = value & 7
+        value & 16 # DEN
+        self.B_bitmap = (value & 32) != 0 # BMM
+        self.B_clip_address = (value & 64) != 0 # ECM # bits 9 and 10 low.
+        value & 128 # RST8
+        self.text_view
 
-	def set_control_2(self, value):
-		self.text_view.first_column = 24 if value & 8 else 31
-		self.text_view.last_column = 343 if value & 8 else 334
-		self.text_view.viewport_column = value & 7
-		self.B_MCM = (value & 16) != 0
+    def set_control_2(self, value):
+        self.text_view.first_column = 24 if value & 8 else 31
+        self.text_view.last_column = 343 if value & 8 else 334
+        self.text_view.viewport_column = value & 7
+        self.B_MCM = (value & 16) != 0
 
-	def set_memory_pointers(self, value):
-		value >>= 1
-		CB_13_12_11 = value & 0x7 # character bitmaps.
-		# TODO in bitmap mode, CB_13 only! (thus 2KiB/8KiB steps).
-		VM_13_12_11_10 = value >> 3 # video matrix (movable in 1KiB steps).
-		self.text_view.character_bitmaps_offset = (1 << 11) * CB_13_12_11
-		self.text_view.video_offset = (1 << 10) * VM_13_12_11_10
-		self.text_view.unprepare()
-		code_color = self.VIC_read_memory(0, 1) # ShedSkin
-		character_data = self.load_chunk(0, 8 * 256) # ShedSkin
+    def set_memory_pointers(self, value):
+        value >>= 1
+        CB_13_12_11 = value & 0x7 # character bitmaps.
+        # TODO in bitmap mode, CB_13 only! (thus 2KiB/8KiB steps).
+        VM_13_12_11_10 = value >> 3 # video matrix (movable in 1KiB steps).
+        self.text_view.character_bitmaps_offset = (1 << 11) * CB_13_12_11
+        self.text_view.video_offset = (1 << 10) * VM_13_12_11_10
+        self.text_view.unprepare()
+        code_color = self.VIC_read_memory(0, 1) # ShedSkin
+        character_data = self.load_chunk(0, 8 * 256) # ShedSkin
 
-	def load_chunk(self, offset, size):
-		#address = VIC_bank_offset + offset
-		return [self.VIC_read_memory(offset + i, 1) for i in range(size)]
+    def load_chunk(self, offset, size):
+        #address = VIC_bank_offset + offset
+        return [self.VIC_read_memory(offset + i, 1) for i in range(size)]
 
-	def repaint(self):
-		self.text_view.VIC_bank = self.CIA2.VIC_bank
-		self.text_view.mode = {
-			(False, False, False): "normal-text",
-			(False, False, True): "multicolor-text",
-			(False, True, False): "normal-bitmap",
-			(False, True, True): "multicolor-bitmap",
-			(True, False, False): "ECM-text",
-			(True, False, True): "invalid-1",
-			(True, True, False): "invalid-2",
-			(True, True, True): "invalid-3",
-		}[self.B_clip_address, self.B_bitmap, self.B_MCM]
+    def repaint(self):
+        self.text_view.VIC_bank = self.CIA2.VIC_bank
+        self.text_view.mode = {
+            (False, False, False): "normal-text",
+            (False, False, True): "multicolor-text",
+            (False, True, False): "normal-bitmap",
+            (False, True, True): "multicolor-bitmap",
+            (True, False, False): "ECM-text",
+            (True, False, True): "invalid-1",
+            (True, True, False): "invalid-2",
+            (True, True, True): "invalid-3",
+        }[self.B_clip_address, self.B_bitmap, self.B_MCM]
 
-		self.text_view.repaint()
+        self.text_view.repaint()
 
-	def read_color_RAM(self, address):
-		return self.MMU.read_memory(0xD800 + (address & 0x3FF))
+    def read_color_RAM(self, address):
+        return self.MMU.read_memory(0xD800 + (address & 0x3FF))
 
-	def VIC_read_memory(self, address, size = 1):
-		assert(size == 1)
-		if (self.CIA2.VIC_bank & 1) == 0: # have Char ROM
-			if address >= 0x1000 and address < 0x2000:
-				return self.char_ROM.read_memory(address - 0x1000, size) #| (self.read_color_RAM(address) << 8)
+    def VIC_read_memory(self, address, size = 1):
+        assert(size == 1)
+        if (self.CIA2.VIC_bank & 1) == 0: # have Char ROM
+            if address >= 0x1000 and address < 0x2000:
+                return self.char_ROM.read_memory(address - 0x1000, size) #| (self.read_color_RAM(address) << 8)
 
-		# Video_Matrix|Chargen|Sprite_Data_Pointers|Sprite_Data.
+        # Video_Matrix|Chargen|Sprite_Data_Pointers|Sprite_Data.
 
-		if self.B_clip_address:
-			address = address &~ (1 << 9) &~ (1 << 10) # FIXME does that also mappen with char_ROM?
+        if self.B_clip_address:
+            address = address &~ (1 << 9) &~ (1 << 10) # FIXME does that also mappen with char_ROM?
 
-		# FIXME return self. | (self.read_color_RAM(address) << 8)
-		VIC_bank_offset = self.CIA2.VIC_bank * 4096
-		return self.MMU.read_memory(address + VIC_bank_offset, size) | (self.read_color_RAM(address) << 8)
+        # FIXME return self. | (self.read_color_RAM(address) << 8)
+        VIC_bank_offset = self.CIA2.VIC_bank * 4096
+        return self.MMU.read_memory(address + VIC_bank_offset, size) | (self.read_color_RAM(address) << 8)
 
-	def set_background_color_0(self, value):
-		self.text_view.background_color_0 = value & 15
+    def set_background_color_0(self, value):
+        self.text_view.background_color_0 = value & 15
 
-	def set_border_color(self, value):
-		self.text_view.border_color = value & 15
+    def set_border_color(self, value):
+        self.text_view.border_color = value & 15
 
-	def read_memory(self, address, size = 1):
-		assert(size == 1)
-		address = address & 0x3F
-		# TODO The registers $d01e and $d01f are automatically cleared on reading.
-		slots = {
-			A_BORDER_COLOR: self.text_view.border_color,
-			A_BACKGROUND_COLOR_0: self.text_view.background_color_0,
-			A_RASTER_COUNTER: self.raster_counter,
-		}
-		return slots[address] if address in slots else 0xFF
+    def read_memory(self, address, size = 1):
+        assert(size == 1)
+        address = address & 0x3F
+        # TODO The registers $d01e and $d01f are automatically cleared on reading.
+        slots = {
+            A_BORDER_COLOR: self.text_view.border_color,
+            A_BACKGROUND_COLOR_0: self.text_view.background_color_0,
+            A_RASTER_COUNTER: self.raster_counter,
+        }
+        return slots[address] if address in slots else 0xFF
 
-	def write_memory(self, address, value, size):
-		assert(isinstance(value, int))
-		# TODO The registers $d01e and $d01f cannot be written.
-		address = address & 0x3F
-		value = (value)
-		# TODO 47 control registers.
-		# 34 for sprite control.
-		print("VIC-II $%X := %r" % (address, value))
-		#time.sleep(5)
-		if address == A_CONTROL_1:
-			return self.set_control_1(value)
-		elif address == A_CONTROL_2:
-			return self.set_control_2(value)
-		elif address == A_MEMORY_POINTERS:
-			return self.set_memory_pointers(value)
-		elif address == A_BORDER_COLOR:
-			return self.set_border_color(value)
-		elif address == A_BACKGROUND_COLOR_0:
-			return self.set_background_color_0(value)
-		#}.get(address) or ignore)(value)
+    def write_memory(self, address, value, size):
+        assert(isinstance(value, int))
+        # TODO The registers $d01e and $d01f cannot be written.
+        address = address & 0x3F
+        value = (value)
+        # TODO 47 control registers.
+        # 34 for sprite control.
+        print("VIC-II $%X := %r" % (address, value))
+        #time.sleep(5)
+        if address == A_CONTROL_1:
+            return self.set_control_1(value)
+        elif address == A_CONTROL_2:
+            return self.set_control_2(value)
+        elif address == A_MEMORY_POINTERS:
+            return self.set_memory_pointers(value)
+        elif address == A_BORDER_COLOR:
+            return self.set_border_color(value)
+        elif address == A_BACKGROUND_COLOR_0:
+            return self.set_background_color_0(value)
+        #}.get(address) or ignore)(value)
 
 """
 [$11]=$1B, [$16]=$8: hires text mode (global bg in $21).
