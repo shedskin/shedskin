9e4bca9fe2f4bb25ee29aeb819e24e39739efcf0
Mark Dufour <mark.dufour@gmail.com>
2010-10-24 12:05:41 +0200

    generalize a bit, replace crufty children_args

--- tests/194.py
+++ tests/194.py
@@ -11,8 +11,13 @@ digit_dict = {
               ,"9":{1:(4,)           ,2:()          ,3:()}
               ,"0":{1:()              ,2:(3,)       ,3:()}
 }
+for d in sorted(digit_dict):
+    d2 = digit_dict[d]
+    for e in sorted(d2):
+        print d, e, d2[e]
 
 l = [[7,8,9], [7.7,8.8,9.9]]
+    
 for ll in l:
     for lll in ll:
         print '%.2f' % lll
