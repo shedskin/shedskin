60ff2e91185641b5b48165bf377fd74243625609
Mark Dufour <mark.dufour@gmail.com>
2010-10-31 18:31:17 +0100

    add test for contained func ref warning

--- tests/errs/4.py
+++ tests/errs/4.py
@@ -3,4 +3,8 @@ try:
 except KeyboardInterrupt:
     print 'uhw'
 
+l = [lambda x,y: (y,x)]
+print l.pop()(1,2)
+
 #*WARNING* 4.py:3: system 'KeyboardInterrupt' is not caught
+#*WARNING* 4.py:6: 'list' instance containing function reference
