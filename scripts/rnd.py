124678b819333d0d55f3007ce206049358191d8c
Mark Dufour <mark.dufour@gmail.com>
2010-11-13 14:50:40 +0100

    optimizer for tx seed

---
+++ scripts/rnd.py
@@ -0,0 +1,21 @@
+import sys, os, random
+_, logfile, parts = sys.argv
+nr_time = {}
+for l in file(logfile):
+    if l.startswith('*** success'):
+        nr_time[int(l.split()[2])] = float(l.split()[3][1:-1])
+print nr_time
+parts = int(parts)
+best_time = None
+seed = 0
+while True:
+    random.seed(seed)
+    nrs = sorted(nr_time)
+    random.shuffle(nrs)
+    times = []
+    for part in range(parts):
+        times.append(sum([nr_time[nr] for nr in nrs[part::parts]]))
+    if best_time is None or max(times) < best_time:
+        best_time = max(times)
+        print 'best_time:', best_time, seed
+    seed += 1
