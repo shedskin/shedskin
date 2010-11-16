4b2d85a437804dc1edf0724b13c0a211a9bd8906
Mark Dufour <mark.dufour@gmail.com>
2010-11-16 15:51:28 +0100

    cleanup test

--- tests/40.py
+++ tests/40.py
@@ -1,8 +1,5 @@
+def hu(n, s=-1):
+    return [s]
 
-def hu(n, s=-1):                         # s: [int], n: [int]
-    return [s]                           # [list(int)]
-
-a = hu(10)                                   # [list(int)]
-#b = hu(10,'2')                               # [list(int,str)]
-c = [i for i in hu(10)]                      # [list(int)]
-
+a = hu(10)
+c = [i for i in hu(10)]
