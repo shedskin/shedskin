cc2c15f974ac6d7cc97f20741ed6c78b70d56913
Mark Dufour <mark.dufour@gmail.com>
2010-11-23 23:37:13 +0100

    test two more warnings

--- tests/errs/3.py
+++ tests/errs/3.py
@@ -3,5 +3,13 @@ import camera
 def hap(x):
     print x
 
+def hap2():
+    pass
+
+hoplist = [hap2]
+hoplist = [hop2]
+
 #*WARNING* camera.py:6: function runpix not called!
 #*WARNING* 3.py:3: function hap not called!
+#*WARNING* 3.py:9: 'list' instance containing function reference
+#*WARNING* 3.py:10: variable 'hop2' has no type
