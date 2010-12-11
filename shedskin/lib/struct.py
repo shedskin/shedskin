a789d45b3c0211c99ff5c63504d8ceba8ae22d36
Mark Dufour <mark.dufour@gmail.com>
2010-12-11 16:32:34 +0100

    partial struct model

---
+++ shedskin/lib/struct.py
@@ -0,0 +1,17 @@
+#class error(Exception): # XXX
+#    pass
+
+def pack(fmt, *vals):
+    pass
+
+def pack_into(fmt, buffer, offset, *vals):
+    pass
+
+def unpack(fmt, string, __elem):
+    return (__elem,)
+
+def unpack_from(fmt, buffer, offset=0):
+    pass
+
+def calcsize(fmt):
+    return 1
