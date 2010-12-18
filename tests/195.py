81922cda208bbc3dd448e5f62f71825197b62ed6
Mark Dufour <mark.dufour@gmail.com>
2010-12-18 16:25:15 +0100

    add some fun with insert as well

--- tests/195.py
+++ tests/195.py
@@ -1,9 +1,18 @@
 # !$!#$
 try:
-    [].pop(-1)
-except IndexError:
-    print 'kan niet poppen'
+    [1].pop(-2)
+except IndexError,e:
+    print e
+try:
+    [].pop(0)
+except IndexError,e:
+    print e
 try:
     [].remove(0)
-except ValueError:
-    print 'kan niet vinden'
+except ValueError,e:
+    print e
+l = []
+l.insert(4, 1)
+l.insert(-1, 2)
+l.insert(-10, 3)
+print l
