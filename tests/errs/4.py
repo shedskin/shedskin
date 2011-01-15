aa7486bdf128e57d9fe2a9cca7d939da932df99c
Mark Dufour <mark.dufour@gmail.com>
2011-01-15 11:25:25 +0100

    finally add warning for tuples > 2 and different types of elems

--- tests/errs/4.py
+++ tests/errs/4.py
@@ -6,5 +6,10 @@ except KeyboardInterrupt:
 l = [lambda x,y: (y,x)]
 print l.pop()(1,2)
 
+kwa = (1, None, [1])
+stack = ('hoi', frozenset([(0,0)]), 'ah')
+
 #*WARNING* 4.py:3: system 'KeyboardInterrupt' is not caught
 #*WARNING* 4.py:6: 'list' instance containing function reference
+#*WARNING* 4.py:9: tuple with length > 2 and different types of elements
+#*WARNING* 4.py:10: tuple with length > 2 and different types of elements
