46da0f7c3b1addcc0c25ef85ca9b6bf3b193de4d
Mark Dufour <mark.dufour@gmail.com>
2011-01-26 12:10:33 +0100

    increase leakage test coverage - found another leak

--- tests/leak/leak_main.py
+++ tests/leak/leak_main.py
@@ -14,3 +14,16 @@ while True:
 
     for q in leak.points:
         q.x
+
+    leak.test_list(range(100))
+    leak.test_list(None)
+    leak.test_list_nested([[1,2],None,[]])
+    leak.test_list_nested(None)
+    leak.test_dict({'hoi': 8.8, 'wa': 9.1})
+    leak.test_dict(None)
+    leak.test_set(set([1,2,3]))
+    leak.test_set(None)
+    leak.test_tuple((1,2,3))
+    leak.test_tuple(None)
+    leak.test_tuple2(('hoi', 8.9))
+    leak.test_tuple2(None)
