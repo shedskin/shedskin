1022a7a012ac60b561c3a26989ed84f8a92e0a88
mark dufour <mark.dufour@gmail.com>
2011-01-26 12:10:33 +0100

    increase leakage test coverage - found another leak

--- tests/leak/leak.py
+++ tests/leak/leak.py
@@ -20,9 +20,33 @@ def test_str(s):
 def test_none(n):
     return n
 
+def test_list(l):
+    return l
+
+def test_list_nested(l):
+    return l
+
+def test_dict(d):
+    return d
+
+def test_set(d):
+    return d
+
+def test_tuple(d):
+    return d
+
+def test_tuple2(d):
+    return d
+
 if __name__ == '__main__':
     p = Point()
     test_float(4.4)
     test_int(4)
     test_str('hoi')
     test_none(None)
+    test_list([1,2])
+    test_list_nested([[1,2]])
+    test_dict({'hoi': 8.8})
+    test_set(set([1,2,3]))
+    test_tuple((1,2,3))
+    test_tuple2(('hoi', 8.9))
