8166cd8a30d4162893410b79f8539e4236704941
Mark Dufour <mark.dufour@gmail.com>
2011-01-26 11:57:33 +0100

    more general fix: increase refcount when obtaining object through proxy

--- tests/leak.py
+++ tests/leak.py
@@ -3,6 +3,10 @@ class Point:
     def __init__(self):
         self.x = 4.4
         self.y = 7
+        self.s = 'wah'
+        self.n = None
+
+points = [Point() for x in range(10)]
 
 def test_float(x):
     return x
@@ -13,8 +17,12 @@ def test_int(x):
 def test_str(s):
     return s
 
+def test_none(n):
+    return n
+
 if __name__ == '__main__':
     p = Point()
     test_float(4.4)
     test_int(4)
     test_str('hoi')
+    test_none(None)
