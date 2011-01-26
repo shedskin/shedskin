c3cb8170e908353b59d426fbbd58c11f01806d08
Mark Dufour <mark.dufour@gmail.com>
2011-01-26 11:24:02 +0100

    add some more tests to that

--- tests/leak.py
+++ tests/leak.py
@@ -2,6 +2,19 @@
 class Point:
     def __init__(self):
         self.x = 4.4
+        self.y = 7
+
+def test_float(x):
+    return x
+
+def test_int(x):
+    return x
+
+def test_str(s):
+    return s
 
 if __name__ == '__main__':
     p = Point()
+    test_float(4.4)
+    test_int(4)
+    test_str('hoi')
