7ce5508286512fe86efa154acba1dd00f0931509
Mark Dufour <mark.dufour@gmail.com>
2011-03-18 09:11:22 +0100

    another exception hierarchy problem

--- tests/196.py
+++ tests/196.py
@@ -1,4 +1,4 @@
-# exception descendant constructor problem
+# exception hierarchy problem 1
 class BadError(Exception):
     pass
 
@@ -6,3 +6,15 @@ if __name__=='__main__':
     BadError()
     BadError("AOE")
 
+# exception hierarchy problem 2
+class MyBaseException:
+    def __init__(self, msg=None):
+        self.msg = msg
+class MyException(MyBaseException): pass
+class MyStandardError(MyException): pass
+class MyBadError(MyException):
+    pass
+
+if __name__=='__main__':
+    MyStandardError()
+    MyBadError()
