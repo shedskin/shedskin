d62700f0cafd0d95792ecb9d5a453839da3595c6
Mark Dufour <mark.dufour@gmail.com>
2011-03-19 11:55:23 +0100

    problem uncovered while looking into that

--- tests/196.py
+++ tests/196.py
@@ -1,4 +1,4 @@
-# exception descendant constructor problem
+# template not removed after iteration
 class BadError(Exception):
     pass
 
@@ -6,3 +6,15 @@ if __name__=='__main__':
     BadError()
     BadError("AOE")
 
+# crash in assign_needs_cast
+class MyBaseException:
+    def __init__(self, msg=None):
+        self.msg = msg
+class MyException(MyBaseException): pass
+class MyStandardError(MyException): pass
+class MyBadError(MyException):
+    pass
+
+if __name__=='__main__':
+    MyStandardError()
+    MyBadError()
