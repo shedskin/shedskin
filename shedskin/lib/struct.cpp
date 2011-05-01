fac5024d2a51b5df5590cca3b2d8dba586b699cb
srepmub <srepmub@akemi.(none)>
2011-05-01 10:22:43 +0200

    beginning of more serious struct implementation

---
+++ shedskin/lib/struct.cpp
@@ -0,0 +1,11 @@
+#include "struct.hpp"
+#include <stdio.h>
+
+namespace __struct__ {
+
+void __init() {
+
+}
+
+} // module namespace
+
--- shedskin/lib/struct.py
+++ shedskin/lib/struct.py
@@ -7,11 +7,11 @@ def pack(fmt, *vals):
 def pack_into(fmt, buffer, offset, *vals):
     pass
 
-def unpack(fmt, string, __elem):
-    return (__elem,)
-
-def unpack_from(fmt, buffer, offset=0):
-    pass
+#def unpack(fmt, string, __elem):
+#    return (__elem,)
+#
+#def unpack_from(fmt, buffer, offset=0):
+#    pass
 
 def calcsize(fmt):
     return 1
