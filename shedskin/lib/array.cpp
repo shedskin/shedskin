8f96120289c5cda9a785f0f837bc074f6e3764f2
Mark Dufour <mark.dufour@gmail.com>
2011-05-05 10:17:34 +0200

    array: stub out methods

--- shedskin/lib/array.cpp
+++ shedskin/lib/array.cpp
@@ -3,14 +3,9 @@
 namespace __array__ {
 
 str *__name__;
-
 void * default_0;
-
-/**
-class array
-*/
-
 class_ *cl_array;
+
 
 void __init() {
     __name__ = new str("array");
--- shedskin/lib/array.hpp
+++ shedskin/lib/array.hpp
@@ -13,22 +13,61 @@ extern class_ *cl_array;
 template <class T> class array : public pyseq<T> {
 public:
     __GC_VECTOR(T) units; /* XXX no pointers, so avoid GC */
+    str *typecode;
 
     array(str *typecode, pyiter<T> *it=NULL) {
         this->__class__ = cl_array;
         __init__(typecode, it);
     }
+
     void *__init__(str *typecode, pyiter<T> *arg);
     list<T> *tolist();
     str *tostring();
     void *extend(pyiter<T> *arg);
     void *fromlist(list<T> *l);
     void *fromstring(str *s);
-
     __ss_int __len__();
     T __getitem__(__ss_int i);
     str *__repr__();
 };
+
+template<class T> void *array<T>::__init__(str *typecode, pyiter<T> *it) {
+    this->typecode = typecode;
+    this->extend(it);
+    return NULL;
+}
+
+template<class T> list<T> *array<T>::tolist() {
+    return new list<T>();
+}
+
+template<class T> str *array<T>::tostring() {
+    return new str("beh");
+}
+
+template<class T> void *array<T>::extend(pyiter<T> *it) {
+    return NULL;
+}
+
+template<class T> void *array<T>::fromlist(list<T> *l) {
+    return NULL;
+}
+
+template<class T> void *array<T>::fromstring(str *s) {
+    return NULL;
+}
+
+template<class T> __ss_int array<T>::__len__() {
+    return NULL;
+}
+
+template<class T> T array<T>::__getitem__(__ss_int i) {
+    return NULL;
+}
+
+template<class T> str *array<T>::__repr__() {
+    return new str("array");
+}
 
 extern void * default_0;
 
