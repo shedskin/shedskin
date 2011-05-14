0fff799db4d9cfce73bca3a12ba8e20a20107812
Fahrzin Hemmati <fahhem@fahhem.com>
2011-05-14 15:20:59 +0200

    binascii: some more tests

--- tests/196.py
+++ tests/196.py
@@ -66,7 +66,39 @@ print '%.2f' % fla[1]
 
 # binascii
 import binascii
-b2a = binascii.b2a_uu('my guitar wants to kill your mama')
+str = 'my guitar wants to strum all night long'
+
+#b2a = binascii.b2a_qp(str)
+#print repr(b2a)
+#a2b = binascii.a2b_qp(b2a)
+#print repr(a2b)
+
+b2a = binascii.b2a_uu(str)
 print repr(b2a)
 a2b = binascii.a2b_uu(b2a)
+print repr(a2b)
+
+b2a = binascii.b2a_hex(str)
+print repr(b2a)
+a2b = binascii.a2b_hex(b2a)
+print repr(a2b)
+
+b2a = binascii.b2a_hqx(str)
+print repr(b2a)
+#a2b = binascii.a2b_hqx(b2a)
+#print repr(a2b)
+
+b2a = binascii.b2a_base64(str)
+print repr(b2a)
+a2b = binascii.a2b_base64(b2a)
+print repr(a2b)
+
+b2a = binascii.rlecode_hqx(str)
+print repr(b2a)
+a2b = binascii.rledecode_hqx(b2a)
+print repr(a2b)
+
+b2a = binascii.hexlify(str)
+print repr(b2a)
+a2b = binascii.unhexlify(b2a)
 print repr(a2b)
