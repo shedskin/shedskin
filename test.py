69908d15808d5060cd06d80557715c44fcc67743
Mark Dufour <mark.dufour@gmail.com>
2011-05-15 12:03:06 +0200

    array: copy, deepcopy

--- test.py
+++ test.py
@@ -1,16 +1,8 @@
 import array
+import copy
 arr = array.array('i', [3,2,1])
-print arr
-print arr.tolist(), repr(arr.tostring())
-print arr[0], arr[1], arr[2]
-print sorted(arr)
-arr2 = array.array('c')
-arr2.extend('hoei')
-print arr2, arr2.tolist(), arr2.tostring()
-print arr2[0]
-fla = array.array('d', (142344, 2384234))
-print fla.typecode, fla.itemsize
-print repr(fla.tostring()), ['%.2f' % flah for flah in fla.tolist()]
-print '%.2f' % fla[1]
-print 'hello, world!'
-
+c1 = copy.copy(arr)
+c1.append(4)
+c2 = copy.deepcopy(arr)
+c2.append(5)
+print c1, c2, arr
