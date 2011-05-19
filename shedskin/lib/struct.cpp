6a5490ea44a9616a7a7830e6f1b5ab0bd8182a20
Mark Dufour <m.dufour@knmp.nl>
2011-05-19 11:18:27 +0200

    struct: refactor int case

--- shedskin/lib/struct.cpp
+++ shedskin/lib/struct.cpp
@@ -5,6 +5,8 @@ namespace __struct__ {
 
 __GC_STRING ordering;
 
+char buffy[32];
+
 __ss_int unpack_one(str *s, __ss_int idx, __ss_int count, __ss_int endian) {
     unsigned int r = 0;
 
@@ -79,6 +81,65 @@ __ss_int calcsize(str *fmt) {
     return result;
 }
 
+int get_itemsize(char order, char c) {
+    if(order == '@') {
+        switch(c) {
+            case 'b': return sizeof(signed char);
+            case 'B': return sizeof(unsigned char);
+            case 'h': return sizeof(short); /* XXX */
+            case 'H': return sizeof(unsigned short);
+            case 'i': return sizeof(int);
+            case 'I': return sizeof(unsigned int);
+            case 'l': return sizeof(long);
+            case 'L': return sizeof(unsigned long);
+            case 'q': return sizeof(long long);
+            case 'Q': return sizeof(unsigned long long);
+        }
+    } else {
+        switch(c) {
+            case 'b': return 1;
+            case 'B': return 1;
+            case 'h': return 2;
+            case 'H': return 2;
+            case 'i': return 4;
+            case 'I': return 4;
+            case 'l': return 4;
+            case 'L': return 4;
+            case 'q': return 8;
+            case 'Q': return 8;
+        }
+    }
+}
+
+void fillbuf(char c, __ss_int t, char order, int itemsize) {
+    if(order == '@') {
+        switch(c) {
+            case 'b': *((signed char *)buffy) = t; break;
+            case 'B': *((unsigned char *)buffy) = t; break;
+            case 'h': *((short *)buffy) = t; break;
+            case 'H': *((unsigned short *)buffy) = t; break;
+            case 'i': *((int *)buffy) = t; break;
+            case 'I': *((unsigned int *)buffy) = t; break;
+            case 'l': *((long *)buffy) = t; break;
+            case 'L': *((unsigned long *)buffy) = t; break;
+            case 'q': *((long long *)buffy) = t; break;
+            case 'Q': *((unsigned long long *)buffy) = t; break;
+        }
+    } else {
+        if(order == '>' or order == '!') {
+            for(int i=itemsize-1; i>=0; i--) {
+                buffy[i] = (unsigned char)(t & 0xff);
+                t >>= 8;
+            }
+        } else {
+            for(unsigned int i=0; i<itemsize; i++) {
+                buffy[i] = (unsigned char)(t & 0xff);
+                t >>= 8;
+            }
+        }
+    }
+}
+
 str *pack(int n, str *fmt, ...) {
     pyobj *arg;
     va_list args;
@@ -86,6 +147,7 @@ str *pack(int n, str *fmt, ...) {
     str *result = new str();
     char order = '@';
     str *digits = new str();
+    int itemsize;
     for(unsigned int i=0, j=0; i<n; j++) {
         char c = fmt->unit[j];
         if(ordering.find(c) != -1) {
@@ -104,75 +166,21 @@ str *pack(int n, str *fmt, ...) {
         switch(c) {
             case 'b': 
             case 'B': 
-                for(unsigned int j=0; j<ndigits; j++) {
-                    arg = va_arg(args, pyobj *);
-                    if(arg->__class__ == cl_int_)
-                        result->unit += (char)((((int_ *)(arg))->unit) & 0xff);
-                }
-                break;
             case 'h': 
             case 'H': 
-                for(unsigned int j=0; j<ndigits; j++) {
-                    arg = va_arg(args, pyobj *);
-                    if(arg->__class__ == cl_int_) {
-                        if(order == '!' or order == '=') {
-                            result->unit += (char)(((((int_ *)(arg))->unit) >> 8) & 0xff);
-                            result->unit += (char)((((int_ *)(arg))->unit) & 0xff);
-                        } else {
-                            result->unit += (char)((((int_ *)(arg))->unit) & 0xff);
-                            result->unit += (char)(((((int_ *)(arg))->unit) >> 8) & 0xff);
-                        }
-                    }
-                }
-                break;
             case 'i':
             case 'I': 
             case 'l':
             case 'L':
-                for(unsigned int j=0; j<ndigits; j++) {
-                    arg = va_arg(args, pyobj *);
-                    if(arg->__class__ == cl_int_) {
-                        if(order == '!') {
-                            result->unit += (char)(((((int_ *)(arg))->unit) >> 24) & 0xff);
-                            result->unit += (char)(((((int_ *)(arg))->unit) >> 16) & 0xff);
-                            result->unit += (char)(((((int_ *)(arg))->unit) >> 8) & 0xff);
-                            result->unit += (char)((((int_ *)(arg))->unit) & 0xff);
-                        }
-                        else {
-                            result->unit += (char)((((int_ *)(arg))->unit) & 0xff);
-                            result->unit += (char)(((((int_ *)(arg))->unit) >> 8) & 0xff);
-                            result->unit += (char)(((((int_ *)(arg))->unit) >> 16) & 0xff);
-                            result->unit += (char)(((((int_ *)(arg))->unit) >> 24) & 0xff);
-                        }
-                    }
-                }
-                break;
             case 'q': 
             case 'Q':
+                itemsize = get_itemsize(order, c);
                 for(unsigned int j=0; j<ndigits; j++) {
                     arg = va_arg(args, pyobj *);
                     if(arg->__class__ == cl_int_) {
-                        long long value = ((int_ *)(arg))->unit;
-                        if(order == '!' or order == '=') {
-                            result->unit += (char)((value >> 56) & 0xff);
-                            result->unit += (char)((value >> 48) & 0xff);
-                            result->unit += (char)((value >> 40) & 0xff);
-                            result->unit += (char)((value >> 32) & 0xff);
-                            result->unit += (char)((value >> 24) & 0xff);
-                            result->unit += (char)((value >> 16) & 0xff);
-                            result->unit += (char)((value >> 8) & 0xff);
-                            result->unit += (char)((value >> 0) & 0xff);
-                        }
-                        else {
-                            result->unit += (char)((value >> 0) & 0xff);
-                            result->unit += (char)((value >> 8) & 0xff);
-                            result->unit += (char)((value >> 16) & 0xff);
-                            result->unit += (char)((value >> 24) & 0xff);
-                            result->unit += (char)((value >> 32) & 0xff);
-                            result->unit += (char)((value >> 40) & 0xff);
-                            result->unit += (char)((value >> 48) & 0xff);
-                            result->unit += (char)((value >> 56) & 0xff);
-                        }
+                        fillbuf(c, ((int_ *)arg)->unit, order, itemsize);
+                        for(unsigned int k=0; k<itemsize; k++)
+                            result->unit += buffy[k];
                     }
                 }
                 break;
