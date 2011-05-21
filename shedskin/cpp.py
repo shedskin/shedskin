19db303a1a40355157c874e21ea88f3f960866da
Mark Dufour <mark.dufour@gmail.com>
2011-05-21 16:03:47 +0200

    struct: improve struct.unpack usage warning

--- shedskin/cpp.py
+++ shedskin/cpp.py
@@ -1705,7 +1705,7 @@ class generateVisitor(ASTVisitor):
         if self.library_func(funcs, 'itertools', None, 'izip_longest'):
             error("default fillvalue for 'izip_longest' becomes 0 for integers", node, warning=True, mv=getmv())
         if self.library_func(funcs, 'struct', None, 'unpack'):
-            error("unsupported use of struct.unpack result", node, warning=True, mv=getmv())
+            error("struct.unpack should be used as follows: 'a, = struct.unpack(..)'", node, warning=True, mv=getmv())
         if self.library_func(funcs, 'array', 'array', '__init__'):
             if not node.args or not isinstance(node.args[0], Const) or node.args[0].value not in 'cbBhHiIlLfd':
                 error("non-constant or unsupported type code", node, warning=True, mv=getmv())
