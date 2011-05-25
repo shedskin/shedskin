faaf871bcca4c779c5a0118af7918f9846c61dab
Mark Dufour <mark.dufour@gmail.com>
2011-05-25 10:44:29 +0200

    setup.py: bump to 0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='shedskin',
-      version='0.7.1',
+      version='0.8',
       description='Shed Skin is an experimental compiler, that can translate pure, but implicitly statically typed Python programs into optimized C++. It can generate stand-alone programs or extension modules that can be imported and used in larger Python programs.',
       url='http://code.google.com/p/shedskin/',
       scripts=['scripts/shedskin'],
