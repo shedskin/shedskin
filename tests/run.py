be122743f4a1595c23b6cb6505cd2c38b39777ff
Mark Dufour <mark.dufour@gmail.com>
2011-05-25 09:19:14 +0200

    tests: fix -e, -x options of test.py again

--- tests/run.py
+++ tests/run.py
@@ -2,9 +2,9 @@
 import traceback, sys, os, time, subprocess, glob, os.path
 
 if os.path.exists('../shedskin/__init__.py'):
-    SS = 'python ../shedskin/__init__.py'
+    SS = '../shedskin/__init__.py'
 else:
-    SS = 'python ../../shedskin/__init__.py'
+    SS = '../../shedskin/__init__.py'
 
 def usage():
     print "'-l': give individual test numbers"
@@ -80,11 +80,11 @@ def run_test(test_nr, failures, msvc, options):
     t0 = time.time()
     try:
         if msvc: 
-            assert os.system('%s -v %d' % (SS, test_nr)) == 0
+            assert os.system('python %s -v %d' % (SS, test_nr)) == 0
         elif 'n' in options:
-            assert os.system('%s -e -m Makefile.%d %d' % (SS, test_nr, test_nr)) == 0
+            assert os.system('python %s -e -m Makefile.%d %d' % (SS, test_nr, test_nr)) == 0
         else:
-            assert os.system('%s -m Makefile.%d %d' % (SS, test_nr, test_nr)) == 0
+            assert os.system('python %s -m Makefile.%d %d' % (SS, test_nr, test_nr)) == 0
         if msvc:
             assert os.system('nmake /C /S clean') == 0
             assert os.system('nmake /C /S') == 0
@@ -115,7 +115,7 @@ def extmod_tests(args, options):
         os.chdir('e%d' % test)
         try:
             extmod = file('main.py').next()[1:].strip()
-            assert os.system('%s -e %s' % (SS, extmod)) == 0
+            assert os.system('python ../%s -e %s' % (SS, extmod)) == 0
             assert os.system('make') == 0
             native_output = get_output('python main.py')
             if sys.platform == 'win32': ext = '.pyd'
@@ -146,7 +146,7 @@ def error_tests(args, options):
             for line in file('%d.py' % test):
                 if line.startswith('#*'):
                     checks.append(line[1:].strip())
-            output = get_output('%s %d 2>&1' % (SS, test)).splitlines()
+            output = get_output('python ../%s %d 2>&1' % (SS, test)).splitlines()
             assert not [l for l in output if 'Traceback' in l]
             for check in checks:
                 assert [l for l in output if l.startswith(check)]
