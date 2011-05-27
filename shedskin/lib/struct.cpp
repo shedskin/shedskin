8927164163147eb0df95a06b376797b52d5a0e7c
Mark Dufour <m.dufour@knmp.nl>
2011-05-27 14:59:08 +0200

    struct: 64-bit C structure alignment

--- shedskin/lib/struct.cpp
+++ shedskin/lib/struct.cpp
@@ -55,10 +55,12 @@ int get_itemsize(char order, char c) {
 }
 
 int padding(char o, int pos, int itemsize) {
+    if(sizeof(void *) == 4) {
 #ifndef WIN32
-    if(itemsize == 8)
-        itemsize = 4;
+        if(itemsize == 8)
+            itemsize = 4;
 #endif
+    }
     if(o == '@' and pos % itemsize)
         return itemsize - (pos % itemsize);
     return 0;
