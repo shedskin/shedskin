8cb87939d9b9622a6cde52c7f55ecfb2fc37c4b1
Thomas Spura <tomspur@fedoraproject.org>
2011-05-29 19:06:23 +0200

    add python setup.py test and include the files in tarball
    Signed-off-by: Thomas Spura <tomspur@fedoraproject.org>

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Command
+import os, subprocess
+
+class run_tests(Command):
+    description = "run testsuite"
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+        ss_dir = os.path.abspath(__file__).split(os.path.sep)[:-1]
+        ss_dir.append('tests')
+        self.tests_dir = os.path.sep.join(ss_dir)
+    def run(self):
+        os.chdir(self.tests_dir)
+        subprocess.call('./run.py -r', shell=True)
+        os.chdir(self.cwd)
 
 setup(name='shedskin',
       version='0.8',
       description='Shed Skin is an experimental compiler, that can translate pure, but implicitly statically typed Python programs into optimized C++. It can generate stand-alone programs or extension modules that can be imported and used in larger Python programs.',
       url='http://code.google.com/p/shedskin/',
       scripts=['scripts/shedskin'],
+      cmdclass={'test':run_tests},
       packages=['shedskin'],
       package_data={'shedskin': ['lib/*.cpp', 'lib/*.hpp', 'lib/*.py', 'lib/os/*.cpp', 'lib/os/*.hpp', 'lib/os/*.py', 'FLAGS*', 'illegal']},
      )
