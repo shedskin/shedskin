186299ec9e7ac82b774e19b8fdeb2a444cb870b2
Mark Dufour <mark.dufour@gmail.com>
2011-06-03 18:14:57 +0200

    dist.py: pull stuff right of git, add tests and examples

--- scripts/dist.py
+++ scripts/dist.py
@@ -8,20 +8,12 @@ os.system('mkdir %s' % release)
 for file in ['doc/LICENSE', 'doc/README.html', 'setup.py']:
     os.system('cp %s %s' % (file, release))
 
-os.system('mkdir %s/shedskin' % release)
-os.system('mkdir %s/shedskin/lib' % release)
-os.system('mkdir %s/shedskin/lib/os' % release)
+for directory in ['shedskin', 'tests', 'examples']:
+    os.system('rm %s -R' % directory)
+    os.system('git checkout %s' % directory)
+    os.system('cp -R %s %s' % (directory, release))
+
 os.system('mkdir %s/scripts' % release)
-
-os.system('rm shedskin/lib/struct* shedskin/lib/serial* shedskin/lib/array* shedskin/lib/hashlib*')
-
-os.system('cp shedskin/lib/*.py %s/shedskin/lib' % release)
-os.system('cp shedskin/lib/*.?pp %s/shedskin/lib' % release)
-os.system('cp shedskin/lib/os/*.py %s/shedskin/lib/os' % release)
-os.system('cp shedskin/lib/os/*.?pp %s/shedskin/lib/os' % release)
-os.system('cp shedskin/*.py %s/shedskin' % release)
-os.system('cp shedskin/FLAGS %s/shedskin' % release)
-os.system('cp shedskin/illegal %s/shedskin' % release)
 os.system('cp scripts/shedskin %s/scripts' % release)
 
 os.system("echo print \\'hello, world!\\' > %s/test.py" % release)
