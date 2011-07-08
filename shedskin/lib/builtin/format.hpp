4648de2bf4a38999308112cc7acb8411cce8c931
Mark Dufour <mark.dufour@gmail.com>
2011-07-08 15:43:42 +0200

    separate out a few more files

---
+++ shedskin/lib/builtin/format.hpp
@@ -0,0 +1,37 @@
+/* string formatting */
+
+int __fmtpos(str *fmt);
+int __fmtpos2(str *fmt);
+void __modfill(str **fmt, pyobj *t, str **s);
+str *mod_to_c2(pyobj *t);
+int_ *mod_to_int(pyobj *t);
+float_ *mod_to_float(pyobj *t);
+
+template<class T> str *__modtuple(str *fmt, tuple2<T,T> *t) {
+    list<pyobj *> *vals = new list<pyobj *>();
+    for(int i=0;i<len(t);i++)
+        vals->append(___box(t->__getitem__(i)));
+    return __mod4(fmt, vals);
+}
+
+template<class A, class B> str *__modtuple(str *fmt, tuple2<A,B> *t) {
+    list<pyobj *> *vals = new list<pyobj *>(2, ___box(t->__getfirst__()), ___box(t->__getsecond__()));
+    return __mod4(fmt, vals);
+}
+
+template<class T> str *__moddict(str *v, dict<str *, T> *d) {
+    str *const_6 = new str(")");
+    int i, pos, pos2;
+    list<str *> *names = (new list<str *>());
+
+    while((pos = __fmtpos2(v)) != -1) {
+        pos2 = v->find(const_6, pos);
+        names->append(v->__slice__(3, (pos+2), pos2, 0));
+        v = (v->__slice__(2, 0, (pos+1), 0))->__add__(v->__slice__(1, (pos2+1), 0, 0));
+    }
+
+    list<pyobj *> *vals = new list<pyobj *>();
+    for(i=0;i<len(names);i++)
+        vals->append(___box(d->__getitem__(names->__getitem__(i))));
+    return __mod4(v, vals);
+}
---
+++ shedskin/lib/builtin/math.hpp
@@ -0,0 +1,191 @@
+/* pow */
+
+template<class A, class B> double __power(A a, B b);
+template<> inline double __power(__ss_int a, double b) { return pow(a,b); }
+template<> inline double __power(double a, __ss_int b) { 
+    if(b==2) return a*a;
+    else if(b==3) return a*a*a;
+    else return pow(a,b); 
+}
+
+complex *__power(complex *a, complex *b);
+complex *__power(complex *a, __ss_int b);
+complex *__power(complex *a, double b);
+
+template<class A> A __power(A a, A b);
+template<> inline double __power(double a, double b) { return pow(a,b); }
+
+template<> inline __ss_int __power(__ss_int a, __ss_int b) {
+    switch(b) {
+        case 2: return a*a;
+        case 3: return a*a*a;
+        case 4: return a*a*a*a;
+        case 5: return a*a*a*a*a;
+        case 6: return a*a*a*a*a*a;
+        case 7: return a*a*a*a*a*a*a;
+        case 8: return a*a*a*a*a*a*a*a;
+        case 9: return a*a*a*a*a*a*a*a*a;
+        case 10: return a*a*a*a*a*a*a*a*a*a;
+    }
+    __ss_int res, tmp;
+
+    res = 1;
+    tmp = a;
+
+    while((b>0)) {
+        if ((b%2)) {
+            res = (res*tmp);
+        }
+        tmp = (tmp*tmp);
+        b = (b/2);
+    }
+    return res;
+}
+
+#ifdef __SS_LONG
+inline __ss_int __power(__ss_int a, __ss_int b, __ss_int c) {
+    __ss_int res, tmp;
+
+    res = 1;
+    tmp = a;
+
+    while((b>0)) {
+        if ((b%2)) {
+            res = ((res*tmp)%c);
+        }
+        tmp = ((tmp*tmp)%c);
+        b = (b/2);
+    }
+    return res;
+}
+#endif
+
+inline int __power(int a, int b, int c) {
+    int res, tmp;
+
+    res = 1;
+    tmp = a;
+
+    while((b>0)) {
+        if ((b%2)) {
+            res = ((res*tmp)%c);
+        }
+        tmp = ((tmp*tmp)%c);
+        b = (b/2);
+    }
+    return res;
+}
+
+/* division */
+
+template<class A, class B> double __divs(A a, B b);
+template<> inline double __divs(__ss_int a, double b) { return (double)a/b; }
+template<> inline double __divs(double a, __ss_int b) { return a/((double)b); }
+
+template<class A> A __divs(A a, A b);
+template<> inline double __divs(double a, double b) { return a/b; }
+#ifdef __SS_LONG
+template<> inline __ss_int __divs(__ss_int a, __ss_int b) {
+    if(a<0 && b>0) return (a-b+1)/b;
+    else if(b<0 && a>0) return (a-b-1)/b;
+    else return a/b;
+}
+#endif
+template<> inline int __divs(int a, int b) {
+    if(a<0 && b>0) return (a-b+1)/b;
+    else if(b<0 && a>0) return (a-b-1)/b;
+    else return a/b;
+}
+
+template<class A, class B> double __floordiv(A a, B b);
+template<> inline double __floordiv(__ss_int a, double b) { return floor((double)a/b); }
+template<> inline double __floordiv(double a, __ss_int b) { return floor(a/((double)b)); }
+
+template<class A> inline A __floordiv(A a, A b) { return a->__floordiv__(b); }
+template<> inline double __floordiv(double a, double b) { return floor(a/b); }
+
+#ifdef __SS_LONG /* XXX */
+template<> inline __ss_int __floordiv(__ss_int a, __ss_int b) { return (__ss_int)floor((double)a/b); } /* XXX */
+#endif
+template<> inline int __floordiv(int a, int b) { return (int)floor((double)a/b); } /* XXX */
+
+/* modulo */
+
+template<class A> A __mods(A a, A b);
+#ifdef __SS_LONG /* XXX */
+template<> inline __ss_int __mods(__ss_int a, __ss_int b) {
+    int m = a%b;
+    if((m<0 && b>0)||(m>0 && b<0)) m+=b;
+    return m;
+}
+#endif
+template<> inline int __mods(int a, int b) {
+    int m = a%b;
+    if((m<0 && b>0)||(m>0 && b<0)) m+=b;
+    return m;
+}
+template<> inline double __mods(double a, double b) {
+    double f = fmod(a,b);
+    if((f<0 && b>0)||(f>0 && b<0)) f+=b;
+    return f;
+}
+
+template<class A, class B> double __mods(A a, B b);
+#ifdef __SS_LONG
+template<> inline double __mods(__ss_int a, double b) { return __mods((double)a, b); }
+template<> inline double __mods(double a, __ss_int b) { return __mods(a, (double)b); }
+#endif
+template<> inline double __mods(int a, double b) { return __mods((double)a, b); }
+template<> inline double __mods(double a, int b) { return __mods(a, (double)b); }
+
+/* divmod */
+
+template<class A> inline tuple2<A, A> *divmod(A a, A b) { return a->__divmod__(b); }
+template<> inline tuple2<double, double> *divmod(double a, double b) {
+    return new tuple2<double, double>(2, __floordiv(a,b), __mods(a,b));
+}
+#ifdef __SS_LONG
+template<> inline tuple2<__ss_int, __ss_int> *divmod(__ss_int a, __ss_int b) {
+    return new tuple2<__ss_int, __ss_int>(2, __floordiv(a,b), __mods(a,b));
+}
+#endif
+template<> inline tuple2<int, int> *divmod(int a, int b) {
+    return new tuple2<int, int>(2, __floordiv(a,b), __mods(a,b));
+}
+template<class A, class B> tuple2<double, double> *divmod(A a, B b);
+#ifdef __SS_LONG
+template<> inline tuple2<double, double> *divmod(double a, __ss_int b) { return divmod(a, (double)b); }
+template<> inline tuple2<double, double> *divmod(__ss_int a, double b) { return divmod((double)a, b); }
+#endif
+template<> inline tuple2<double, double> *divmod(double a, int b) { return divmod(a, (double)b); }
+template<> inline tuple2<double, double> *divmod(int a, double b) { return divmod((double)a, b); }
+
+tuple2<complex *, complex *> *divmod(complex *a, double b);
+tuple2<complex *, complex *> *divmod(complex *a, __ss_int b);
+
+/* complex */
+
+template<class T> complex::complex(T t) {
+    __class__ = cl_complex;
+    real = __float(t);
+    imag = 0;
+}
+
+/* add */
+
+template<class T> inline T __add(T a, T b) { return a->__add__(b); }
+#ifdef __SS_LONG
+template<> inline __ss_int __add(__ss_int a, __ss_int b) { return a + b; }
+#endif
+template<> inline int __add(int a, int b) { return a + b; }
+template<> inline double __add(double a, double b) { return a + b; }
+
+/* reverse */
+
+template<class U> U __add2(double a, U b) { return b->__add__(a); }
+template<class U> U __sub2(double a, U b) { return b->__rsub__(a); }
+template<class T> T __mul2(__ss_int n, T a) { return a->__mul__(n); }
+template<class T> T __mul2(__ss_bool n, T a) { return a->__mul__(n.value); }
+template<class T> T __mul2(double n, T a) { return a->__mul__(n); }
+template<class T> T __div2(__ss_int n, T a) { return a->__rdiv__(n); }
+template<class T> T __div2(double n, T a) { return a->__rdiv__(n); }
