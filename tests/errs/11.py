374e2e2e9a8b3477c51dbe16ff867451ac1e94fb
Mark Dufour <mark.dufour@gmail.com>
2011-08-13 14:03:29 +0200

    almost equal now

--- tests/errs/11.py
+++ tests/errs/11.py
@@ -3,7 +3,13 @@ P = []
 [1] if True else P
 print [1] == P
 
+P2 = [[1],P]
+
+S = [None]
+S2 = [['s'],S]
 
 #*WARNING* 11.py:3: incompatible types
 #*WARNING* 11.py:4: incompatible types
+#*WARNING* 11.py:6: incompatible types
+#*WARNING* 11.py:9: incompatible types
 
