711e8d957f76e3b1cdc579d8da79c4924fb25333
Mark Dufour <mark.dufour@gmail.com>
2011-09-03 13:23:30 +0200

    remove docs, update dist script for 0.9

--- scripts/dist.py
+++ scripts/dist.py
@@ -5,16 +5,19 @@ release = 'shedskin-%s' % sys.argv[1]
 os.system('rm -R shedskin-%s' % sys.argv[1])
 os.system('mkdir %s' % release)
 
-for file in ['doc/LICENSE', 'doc/README.html', 'setup.py']:
+for file in ['LICENSE', 'README', 'setup.py']:
     os.system('cp %s %s' % (file, release))
 
-for directory in ['shedskin', 'tests', 'examples']:
+for directory in ['shedskin', 'tests']:
     os.system('rm %s -R' % directory)
     os.system('git checkout %s' % directory)
     os.system('cp -R %s %s' % (directory, release))
 
 os.system('mkdir %s/scripts' % release)
 os.system('cp scripts/shedskin %s/scripts' % release)
+
+os.system('mkdir %s/doc' % release)
+os.system('cp doc/shedskin.rst %s/doc' % release)
 
 os.system("echo print \\'hello, world!\\' > %s/test.py" % release)
 
