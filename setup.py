20f12938845d374be4d59d096f1a9570e473a5f2
Thomas Spura <thomas.spura@gmail.com>
2011-09-16 17:08:09 +0200

    Build libshedskin

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Command
+from distutils.core import setup, Command, Extension
 import os
+import glob
 
 class run_tests(Command):
     description = "run testsuite"
@@ -18,6 +19,11 @@ class run_tests(Command):
         os.system('./run.py')
         os.chdir(self.cwd)
 
+def src(globbing):
+    globbing = "shedskin" + os.path.sep + globbing
+    files = glob.glob(globbing)
+    return files
+
 setup(name='shedskin',
       version='0.9',
       description='Shed Skin is an experimental compiler, that can translate pure, but implicitly statically typed Python programs into optimized C++. It can generate stand-alone programs or extension modules that can be imported and used in larger Python programs.',
@@ -26,4 +32,5 @@ setup(name='shedskin',
       cmdclass={'test':run_tests},
       packages=['shedskin'],
       package_data={'shedskin': ['lib/*.cpp', 'lib/*.hpp', 'lib/builtin/*.cpp', 'lib/builtin/*.hpp', 'lib/*.py', 'lib/os/*.cpp', 'lib/os/*.hpp', 'lib/os/*.py', 'FLAGS*', 'illegal']},
+      ext_modules=[Extension('libshedskin', src('lib/*.cpp') + src('lib/builtin/*.cpp') + src('lib/os/*.cpp'), include_dirs=["shedskin" + os.path.sep + "lib"])],
      )
