e4addb79d58544cbc9ef831b6fc1edbc99e85e1c
Mark Dufour <mark.dufour@gmail.com>
2012-05-06 16:15:13 +0200

    rubik2: remove visualization. seems to work well for different shuffles now

--- examples/rubik2.py
+++ examples/rubik2.py
@@ -1,23 +1,7 @@
 import random
 random.seed(1)
 
-faces = U, R, F, D, L, B = range(6)
-facenames = ["U", "R", "F", "D", "L", "B"]
-
-facelets = U1, U2, U3, U4, U5, U6, U7, U8, U9, R1, R2, R3, R4, R5, R6, R7, R8, R9, F1, F2, F3, F4, F5, F6, F7, F8, F9, D1, D2, D3, D4, D5, D6, D7, D8, D9, L1, L2, L3, L4, L5, L6, L7, L8, L9, B1, B2, B3, B4, B5, B6, B7, B8, B9 = range(54)
-
-facelet_color = ["G"] * 9 + ["O"] * 9 + ["W"] * 9 + ["B"] * 9 + ["R"] * 9 + ["Y"] * 9
-
-facelet_turn = [
-    [U3,U6,U9,U2,U5,U8,U1,U4,U7,F1,F2,F3,R4,R5,R6,R7,R8,R9,L1,L2,L3,F4,F5,F6,F7,F8,F9,D1,D2,D3,D4,D5,D6,D7,D8,D9,B1,B2,B3,L4,L5,L6,L7,L8,L9,R1,R2,R3,B4,B5,B6,B7,B8,B9],
-    [U1,U2,B7,U4,U5,B4,U7,U8,B1,R3,R6,R9,R2,R5,R8,R1,R4,R7,F1,F2,U3,F4,F5,U6,F7,F8,U9,D1,D2,F3,D4,D5,F6,D7,D8,F9,L1,L2,L3,L4,L5,L6,L7,L8,L9,D9,B2,B3,D6,B5,B6,D3,B8,B9],
-    [U1,U2,U3,U4,U5,U6,R1,R4,R7,D3,R2,R3,D2,R5,R6,D1,R8,R9,F3,F6,F9,F2,F5,F8,F1,F4,F7,L3,L6,L9,D4,D5,D6,D7,D8,D9,L1,L2,U9,L4,L5,U8,L7,L8,U7,B1,B2,B3,B4,B5,B6,B7,B8,B9],
-    [U1,U2,U3,U4,U5,U6,U7,U8,U9,R1,R2,R3,R4,R5,R6,B7,B8,B9,F1,F2,F3,F4,F5,F6,R7,R8,R9,D3,D6,D9,D2,D5,D8,D1,D4,D7,L1,L2,L3,L4,L5,L6,F7,F8,F9,B1,B2,B3,B4,B5,B6,L7,L8,L9],
-    [F1,U2,U3,F4,U5,U6,F7,U8,U9,R1,R2,R3,R4,R5,R6,R7,R8,R9,D1,F2,F3,D4,F5,F6,D7,F8,F9,B9,D2,D3,B6,D5,D6,B3,D8,D9,L3,L6,L9,L2,L5,L8,L1,L4,L7,B1,B2,U7,B4,B5,U4,B7,B8,U1],
-    [L7,L4,L1,U4,U5,U6,U7,U8,U9,R1,R2,U1,R4,R5,U2,R7,R8,U3,F1,F2,F3,F4,F5,F6,F7,F8,F9,D1,D2,D3,D4,D5,D6,R9,R6,R3,D7,L2,L3,D8,L5,L6,D9,L8,L9,B3,B6,B9,B2,B5,B8,B1,B4,B7],
-]
-
-facemap = [0, 3, 2, 5, 4, 1]
+facenames = ["U", "D", "F", "B", "L", "R"]
 
 affected_cubies = [
   [  0,  1,  2,  3,  0,  1,  2,  3 ],   # U
@@ -28,58 +12,31 @@ affected_cubies = [
   [  1,  8,  5, 10,  1,  0,  4,  7 ],   # R
 ]
 
-def row(state, j):
-    return ' '.join([facelet_color[state[i]] for i in range(j, j+3)])
-
-def visual(state):
-   for i in range(3):
-       print 8*' '+row(state, U*9+3*i)
-   for i in range(3):
-       print ' '+row(state, L*9+3*i)+'  '+row(state, F*9+3*i)+'  '+row(state, R*9+3*i)+'  '+row(state, B*9+3*i)
-   for i in range(3):
-       print 8*' '+row(state, D*9+3*i)
+applicable_moves = [262143, 259263, 74943, 74898]
 
 def move_str(move):
-    return facenames[facemap[move/3]]+{1: '', 2: '2', 3: "'"}[move%3+1]
+    return facenames[move/3]+{1: '', 2: '2', 3: "'"}[move%3+1]
 
-def apply_move(move, state, state2):
-    turns = move % 3 + 1;
-    face = move / 3;
+def apply_move(move, state):
+    turns = move % 3 + 1
+    face = move / 3
 
-    face2 = facemap[face]
+    newstate = state[:]
     for turn in range(turns):
-        newstate = 54*[0]
-        for i in range(54):
-            newstate[facelet_turn[face2][i]] = state[i]
-        state = newstate
-
-    newstate2 = state2[:]
-    for turn in range(turns):
-        oldstate = newstate2[:]
+        oldstate = newstate[:]
         for i in range(8):
             isCorner = int(i > 3)
             target = affected_cubies[face][i] + isCorner*12
             killer = affected_cubies[face][(i-3) if (i&3)==3 else i+1] + isCorner*12
             orientationDelta = int(1<face<4) if i<4 else (0 if face<2 else 2 - (i&1))
-            newstate2[target] = oldstate[killer]
-            newstate2[target+20] = oldstate[killer+20] + orientationDelta
+            newstate[target] = oldstate[killer]
+            newstate[target+20] = oldstate[killer+20] + orientationDelta
             if turn == turns-1:
-                newstate2[target+20] %= 2 + isCorner
+                newstate[target+20] %= 2 + isCorner
 
-    return newstate, newstate2
+    return newstate
 
-class cube_state:
-    def __init__(self, state, state2, route):
-        self.state = state
-        self.state2 = state2
-        self.route = route
-
-    def apply_move(self, move):
-        state, state2 = apply_move(move, self.state, self.state2)
-        return cube_state(state, state2, self.route+[move])
-
-def get_id(cube_state, phase):
-    state = cube_state.state2
+def get_id(state, phase):
     if phase == 0:
         return tuple(state[20:32])
 
@@ -100,77 +57,82 @@ def get_id(cube_state, phase):
                 result[2] ^= int(state[i] > state[j])
         return tuple(result)
 
-    return tuple(cube_state.state2)
+    return tuple(state)
 
-goal_state = range(20)+20*[0]
+class cube_state:
+    def __init__(self, state, route=None):
+        self.state = state
+        self.route = route
+
+    def id_(self, phase): # XXX inline
+        return get_id(self.state, phase)
+
+    def apply_move(self, move):
+        state = apply_move(move, self.state)
+        return cube_state(state, self.route+[move])
 
 print 'start'
-state = range(54)
-visual(state)
+end_state = range(20)+20*[0]
+start_state = end_state[:]
+print start_state
 
-state2 = range(20)+20*[0]
-#print state2, state2[20:32]
+#for move in [2,15,13,4,8,8,11,14,1,0,0,12,9,0,13,17,0,7,15]:
+#    start_state = apply_move(move, start_state)
+#print 'hum', start_state
+#stop
 
 print 'randomize'
-for x in range(2):
+for x in range(50):
     move = random.randrange(0,18)
-    print move_str(move)
-    state, state2 = apply_move(move, state, state2)
-visual(state)
-
-applicable_moves = [262143, 262098, 259218, 74898]
-
-#for phase in range(4):
-#    for move in range(18):
-#        if applicable_moves[phase] & (1<<move):
-#            print 'ok', phase, move, move_str(move)
-#solve
+    print move, move_str(move)
+    start_state = apply_move(move, start_state)
+print start_state
+print 'start id', get_id(start_state, 0)
 
 print 'solve'
-state_ids = set()
-states = [cube_state(state, state2, [])]
+
+state = cube_state(start_state, [])
+goal_state = cube_state(end_state, [])
 
 for phase in range(4):
-    cs = states[0]
     print 'PHASE', phase
-    phase_ok = False
-    depth = 0
-    goal_id = get_id(cube_state(None, goal_state, []), phase)
-    current_id = get_id(states[0], phase)
-#    print 'current_id', current_id
-    if current_id == goal_id:
-#        print 'al ok'
-        continue
+    current_id = state.id_(phase)
+    print 'current_id', current_id
+    goal_id = goal_state.id_(phase)
+    print 'goal_id', goal_id
 
+    states = [state]
+    state_ids = set([current_id])
+
+    if current_id == goal_id:
+      print 'phase skip!'
+      continue
+
+    depth = 0
+    phase_ok = False
     while not phase_ok:
-#        print 'DEPTH', depth
-        new_states = []
-        for cstate in states:
+        print 'DEPTH', depth
+        next_states = []
+        for cur_state in states:
             for move in range(18):
                 if applicable_moves[phase] & (1<<move):
-                    new_state = cstate.apply_move(move)
-                    id_ = get_id(new_state, phase)
-#                    print 'move', move_str(move), id_
-                    if id_ == goal_id:
-#                        print 'gevonden!', move_str(move), id_
-                        for m in new_state.route:
-                            print m, move_str(m)
-                        for m in new_state.route:
-                            cs = cs.apply_move(m)
-                        visual(cs.state)
+                    next_state = cur_state.apply_move(move)
+                    next_id = next_state.id_(phase)
+                    if next_id == goal_id:
+                        print 'gevonden!', next_state.route, ','.join([move_str(m) for m in next_state.route])
                         phase_ok = True
+                        state = next_state
                         break
-                    elif id_ not in state_ids:
-                        state_ids.add(id_)
-                        new_states.append(new_state)
+                    if next_id not in state_ids:
+                        state_ids.add(next_id)
+                        next_states.append(next_state)
             if phase_ok:
                 break
+        if phase_ok:
+            break
+
         depth += 1
-#        print 'new states at depth %d: %d' % (depth, len(new_states))
-        states = new_states
+        states = next_states
+        print 'next states', len(next_states)
 
-    state_ids = set()
-    states = [cs]
-
-visual(cs.state)
-print 'used moves:', len(new_state.route)
+print 'used moves:', len(state.route)
