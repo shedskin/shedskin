e6e5a8b4bcba6f0e6dfc1cf7bf30528785feaa88
Administrator <srepmub@ubuntu.ubuntu-domain>
2012-09-22 10:53:00 +0800

    fix time crashes for default args (reported by mark perry miller)

--- tests/200.py
+++ tests/200.py
@@ -5,3 +5,8 @@ f = os.popen("ls", "r")
 rFDs, wFDs, xFDs = select.select((), [], set(), 0)
 rFDs, wFDs, xFDs = select.select([f.fileno()], set(), [], 20)
 print len(rFDs), len(wFDs), len(xFDs)
+
+#time crashes without args
+import time
+print time.asctime()[:10]
+print str(time.localtime())[:50]
