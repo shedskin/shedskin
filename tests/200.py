bde4d675e27252b0e49f34de9c864a05a00b1b76
Mark Dufour <mark.dufour@gmail.com>
2012-12-19 17:26:01 +0100

    inheritance and generator expression problem (triggered by piotr tarsa)

--- tests/200.py
+++ tests/200.py
@@ -16,3 +16,21 @@ print 'hello\0world'
 print repr('hello\0world')
 print repr('woef%swaf' % 'waaa\0wa')
 print repr('woef%swaf%s!' % ('waaa\0wa\0wa', '\0haaap'))
+
+#inheritance, generator expression
+import array
+
+class Common(object):
+    CostScale = 7
+    #StateTable = FsmGenerator().stateTable
+
+    def __init__(self, inputStream, outputStream, options):
+        lzpLowCount = 10
+        self.lzpLow = array.array("H", (0xffb5 for _ in xrange(lzpLowCount)))
+
+class Decoder(Common):
+    def __init__(self, inputStream, outputStream, options):
+        Common.__init__(self, inputStream, outputStream, options)
+
+d = Decoder(None, None, None)
+print d.lzpLow
