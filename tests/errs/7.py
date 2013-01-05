5269ad51373f148fdc0028c82241f958497f7e4e
Mark Dufour <mark.dufour@gmail.com>
2013-01-05 21:27:01 +0100

    more warnings for broken python code

--- tests/errs/7.py
+++ tests/errs/7.py
@@ -39,6 +39,31 @@ while a:
     print a.pop().frut
     print a.pop().fledder()
 
+class woef:
+    hoep = 1
+
+    def __init__(self):
+        self.MMU2 = 18
+        wa = self.MMU.ole_hop(21)
+        wo = self.MMU2.boe
+        ugh = self.MMU2
+        we = self.MMU3
+
+    def roep(self):
+        print 'roep'
+
+    @property
+    def roep2(self):
+        return 1
+
+woef()
+
+flut = 1
+flutattr = flut.attr
+
+flut2 = woef()
+flut2attr = flut2.attr
+
 #*WARNING* 7.py: variable 'l' has dynamic (sub)type
 #*WARNING* 7.py: variable 'r' has dynamic (sub)type
 #*WARNING* 7.py:5: variable 'snuif' has no type
@@ -49,4 +74,8 @@ while a:
 #*WARNING* 7.py:32: expression has no type
 #*WARNING* 7.py:39: expression has no type
 #*WARNING* 7.py:40: expression has no type
-
+#*WARNING* 7.py:47: expression has no type
+#*WARNING* 7.py:48: expression has no type
+#*WARNING* 7.py:50: expression has no type
+#*WARNING* 7.py:62: expression has no type
+#*WARNING* 7.py:65: expression has no type
