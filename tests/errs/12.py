4422811517d39ee75e6f2bd6772d51c506d8db6b
Mark Dufour <mark.dufour@gmail.com>
2013-04-29 14:53:18 +0200

    include function references in dynamic typing warnings

--- tests/errs/12.py
+++ tests/errs/12.py
@@ -7,5 +7,21 @@ a = A()
 a.b = 'woef'
 del a.b
 
+azo = 1
+azo = lambda x: -x
+
+def blah(b):
+    pass
+
+blah(1)
+blah(blah)
+
+#def hoep(a,b):
+#    pass
+#
+#if False:
+#    hoep(1)
 #*WARNING* 12.py:2: 'del' has no effect without refcounting
 #*WARNING* 12.py:8: 'del' has no effect without refcounting
+#*WARNING* 12.py: Variable 'azo' has dynamic (sub)type
+#*WARNING* 12.py: Variable (Function blah, 'b') has dynamic (sub)type
