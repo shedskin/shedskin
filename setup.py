ee7f968ef438330ad1fd30c6f9a85fd13ef25ca7
Fahrzin Hemmati <fahhem@gmail.com>
2015-05-04 19:25:50 -0700

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@
 from distutils.core import setup, Command
 import os
 
+with open('README.rst') as readme:
+    description = [
+        line.strip() for line in readme.readlines()
+        if line.startswith('Shed Skin is an')][0]
+
+
 class run_tests(Command):
     description = "run testsuite"
     user_options = []
@@ -18,12 +24,17 @@ class run_tests(Command):
         os.system('./run.py')
         os.chdir(self.cwd)
 
-setup(name='shedskin',
-      version='0.9.4',
-      description='Shed Skin is an experimental compiler, that can translate pure, but implicitly statically typed Python programs into optimized C++. It can generate stand-alone programs or extension modules that can be imported and used in larger Python programs.',
-      url='http://code.google.com/p/shedskin/',
-      scripts=['scripts/shedskin'],
-      cmdclass={'test':run_tests},
-      packages=['shedskin'],
-      package_data={'shedskin': ['lib/*.cpp', 'lib/*.hpp', 'lib/builtin/*.cpp', 'lib/builtin/*.hpp', 'lib/*.py', 'lib/os/*.cpp', 'lib/os/*.hpp', 'lib/os/*.py', 'FLAGS*', 'illegal']},
-     )
+setup(
+    name='shedskin',
+    version='0.9.4',
+    description=description,
+    url='http://code.google.com/p/shedskin/',
+    scripts=['scripts/shedskin'],
+    cmdclass={'test': run_tests},
+    packages=['shedskin'],
+    package_data={
+        'shedskin': [
+            'lib/*.cpp', 'lib/*.hpp', 'lib/builtin/*.cpp', 'lib/builtin/*.hpp',
+            'lib/*.py', 'lib/os/*.cpp', 'lib/os/*.hpp', 'lib/os/*.py',
+            'FLAGS*', 'illegal']},
+)
