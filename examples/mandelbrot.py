a264c99b34a6c1a81d6403e42a51049837011cbf
folkert van heusden <mail@vanheusden.com>
2022-10-25 14:40:18 +0200

    2to3 mandelbrot.py

--- examples/mandelbrot.py
+++ examples/mandelbrot.py
@@ -5,9 +5,9 @@
 
 def mandelbrot(max_iterations=1000):
     bailout = 16
-    for y in xrange(-39, 39):
+    for y in range(-39, 39):
         line = []
-        for x in xrange(-39, 39):
+        for x in range(-39, 39):
             cr = y/40.0 - 0.5
             ci = x/40.0
             zi = 0.0
@@ -26,7 +26,7 @@ def mandelbrot(max_iterations=1000):
                 if i > max_iterations:
                     line.append("#")
                     break
-        print "".join(line)
+        print(''.join(line))
 
 for x in range(10):
     mandelbrot()
