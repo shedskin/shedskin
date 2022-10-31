cc47b680eaf360105f34c5785128862b7107d1b6
Mark Dufour <mark.dufour@gmail.com>
2022-10-31 18:00:35 +0100

    make unicode test from sunfish in 206

--- tests/206.py
+++ tests/206.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 
 # check bounds after wrapping..
@@ -358,3 +359,29 @@ arr2.insert(0, '-')
 arr2.fromlist(['a', 'b'])
 print(arr2, arr2.tolist(), arr2.tostring())
 print(arr2[0])
+
+
+initial = (
+    '         \n'  #   0 -  9
+    '         \n'  #  10 - 19
+    ' rnbqkbnr\n'  #  20 - 29
+    ' pppppppp\n'  #  30 - 39
+    ' ........\n'  #  40 - 49
+    ' ........\n'  #  50 - 59
+    ' ........\n'  #  60 - 69
+    ' ........\n'  #  70 - 79
+    ' PPPPPPPP\n'  #  80 - 89
+    ' RNBQKBNR\n'  #  90 - 99
+    '         \n'  # 100 -109
+    '         \n'  # 110 -119
+)
+
+def print_pos(board):
+    print()
+    uni_pieces = {'R':u'♜', 'N':u'♞', 'B':u'♝', 'Q':u'♛', 'K':u'♚', 'P':u'♟',
+                  'r':u'♖', 'n':u'♘', 'b':u'♗', 'q':u'♕', 'k':u'♔', 'p':u'♙', '.':u'·'}
+
+    for k in sorted(uni_pieces):
+        print(k, uni_pieces[k])
+
+print_pos(initial)
