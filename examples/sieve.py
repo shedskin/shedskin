0d2d3ccedbe2e2e6acc40730702910c77a87f4eb
Mark Dufour <mark.dufour@stcorp.nl>
2022-11-01 09:48:55 +0100

    2to3 examples/sieve.py

--- examples/sieve.py
+++ examples/sieve.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from math import sqrt, ceil
 from sys import argv
 
@@ -12,14 +14,14 @@ def sieveOfAtkin(end):
     sieve = [False] * (lng + 1)
 
     x_max, x2, xd = int(sqrt((end-1)/4.0)), 0, 4
-    for xd in xrange(4, 8*x_max + 2, 8):
+    for xd in range(4, 8*x_max + 2, 8):
         x2 += xd
         y_max = int(sqrt(end-x2))
         n, n_diff = x2 + y_max*y_max, (y_max << 1) - 1
         if not (n & 1):
             n -= n_diff
             n_diff -= 2
-        for d in xrange((n_diff - 1) << 1, -1, -8):
+        for d in range((n_diff - 1) << 1, -1, -8):
             m = n % 12
             if m == 1 or m == 5:
                 m = n >> 1
@@ -27,26 +29,26 @@ def sieveOfAtkin(end):
             n -= d
 
     x_max, x2, xd = int(sqrt((end-1) / 3.0)), 0, 3
-    for xd in xrange(3, 6 * x_max + 2, 6):
+    for xd in range(3, 6 * x_max + 2, 6):
         x2 += xd
         y_max = int(sqrt(end-x2))
         n, n_diff = x2 + y_max*y_max, (y_max << 1) - 1
         if not(n & 1):
             n -= n_diff
             n_diff -= 2
-        for d in xrange((n_diff - 1) << 1, -1, -8):
+        for d in range((n_diff - 1) << 1, -1, -8):
             if n % 12 == 7:
                 m = n >> 1
                 sieve[m] = not sieve[m]
             n -= d
 
     x_max, y_min, x2, xd = int((2 + sqrt(4-8*(1-end)))/4), -1, 0, 3
-    for x in xrange(1, x_max + 1):
+    for x in range(1, x_max + 1):
         x2 += xd
         xd += 6
         if x2 >= end: y_min = (((int(ceil(sqrt(x2 - end))) - 1) << 1) - 2) << 1
         n, n_diff = ((x*x + x) << 1) - 1, (((x-1) << 1) - 2) << 1
-        for d in xrange(n_diff, y_min, -8):
+        for d in range(n_diff, y_min, -8):
             if n % 12 == 11:
                 m = n >> 1
                 sieve[m] = not sieve[m]
@@ -56,18 +58,18 @@ def sieveOfAtkin(end):
     if end <= 3:
         return primes[:max(0,end-2)]
 
-    for n in xrange(5 >> 1, (int(sqrt(end))+1) >> 1):
+    for n in range(5 >> 1, (int(sqrt(end))+1) >> 1):
         if sieve[n]:
             primes.append((n << 1) + 1)
             aux = (n << 1) + 1
             aux *= aux
-            for k in xrange(aux, end, 2 * aux):
+            for k in range(aux, end, 2 * aux):
                 sieve[k >> 1] = False
 
     s  = int(sqrt(end)) + 1
     if s  % 2 == 0:
         s += 1
-    primes.extend([i for i in xrange(s, end, 2) if sieve[i >> 1]])
+    primes.extend([i for i in range(s, end, 2) if sieve[i >> 1]])
 
     return primes
 
@@ -79,7 +81,7 @@ def sieveOfEratostenes(n):
     # http://groups.google.com/group/comp.lang.python/msg/f1f10ced88c68c2d
     if n <= 2:
         return []
-    sieve = range(3, n, 2)
+    sieve = list(range(3, n, 2))
     top = len(sieve)
     for si in sieve:
         if si:
@@ -93,16 +95,16 @@ if __name__ == "__main__":
     # The Sieve of Atkin is supposed to be faster for big n.
 
     n = 10000000 #int(argv[1])
-    print "n:", n
+    print("n:", n)
 
     #if argv[2] == "1":
-    print "Sieve of Atkin"
+    print("Sieve of Atkin")
     r = sieveOfAtkin(n)
-    print len(r)
+    print(len(r))
     #else:
-    print "Sieve of Eratostenes"
+    print("Sieve of Eratostenes")
     r = sieveOfEratostenes(n)
-    print len(r)
+    print(len(r))
 
     #if argv[3] == "1":
     #    print r
