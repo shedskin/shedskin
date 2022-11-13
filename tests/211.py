011ddb70631d329d86ba8248361aa366b12530ed
Mark Dufour <mark.dufour@gmail.com>
2022-11-13 11:02:18 +0100

    ConfigParser -> configparser
    also separate out related tests to 216

--- tests/211.py
+++ tests/211.py
@@ -1,6 +1,5 @@
-from __future__ import print_function
-
 # binascii
+
 import binascii
 str = b'my guitar wants to strum all night long'
 
---
+++ tests/216.py
@@ -0,0 +1,41 @@
+# configparser # XXX readfp
+
+import configparser
+
+config = configparser.ConfigParser(defaults={'aha': 'hah'})
+
+config.read("testdata/test.conf")
+
+print(config.getint('ematter', 'pages'), config.getfloat('ematter', 'pages'))
+print(int(config.getboolean('ematter', 'hop')))
+
+print(int(config.has_section('ematteu')))
+config.add_section('meuk')
+config.set('meuk', 'submeuk1', 'oi')
+config.set('meuk', 'submeuk2', 'bwah')
+if config.has_section('meuk') and config.has_option('meuk', 'submeuk1'):
+    config.remove_option('meuk', 'submeuk1')
+config.add_section('bagger')
+config.remove_section('bagger')
+
+# dump entire config file
+for section in sorted(config.sections()):
+    print(section)
+    for option in sorted(config.options(section)):
+        print(" ", option, "=", config.get(section, option))
+
+print(config.get('ematter', 'pages', vars={'var': 'blah'}))
+
+fl = open('testdata/test.ini', 'w')
+config.write(fl)
+fl.close()
+print(sorted(open('testdata/test.ini').readlines()))
+
+print(config.defaults().items())
+print(sorted(config.items('ematter', vars={'var': 'blah'})))
+
+rcp = configparser.RawConfigParser()
+rcp.read(["testdata/test.conf"])
+
+print(rcp.get('ematter', 'pages')) #, vars={'var': 'blah'})
+print(sorted(rcp.items('ematter')))
