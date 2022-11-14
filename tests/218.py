5dd50c411b6a71d2905c23763896d262203a411f
Mark Dufour <mark.dufour@stcorp.nl>
2022-11-14 13:03:51 +0100

    more file stuff merging to 218

--- tests/218.py
+++ tests/218.py
@@ -1,7 +1,5 @@
 # files
 
-print(next(open('run.py')).strip())
-
 # open('U')
 # MAC
 with open('cr.txt', 'w') as f1:
@@ -45,3 +43,31 @@ with open('crlf.txt', 'rU') as f1:
     for line in f1:
         print('%r' % line,)
 print('===')
+
+# next
+print(next(open('run.py')).strip())
+
+# cStringIO.StringIO, file.seek
+import cStringIO, sys
+
+sio = cStringIO.StringIO(open('testdata/hopsakee').read())
+print(sio.readlines())
+
+sio = cStringIO.StringIO('blaat')
+sio.seek(-3, 2)
+print(sio.read())
+
+sio = cStringIO.StringIO()
+print(sio.tell())
+sio.write('hallo\njoh')
+print(sio.tell())
+sio.seek(0, 0)
+print(sio.tell())
+print(sio.readlines())
+print(sio.tell())
+sio.seek(0, 0)
+print(sio.tell())
+sio.write('hoi')
+print(sio.tell())
+print(sio.readlines())
+print(sio.tell())
