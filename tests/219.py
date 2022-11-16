6a479522a172d9366ef5b91c675c6b4eb9a6aa9b
Mark Dufour <mark.dufour@gmail.com>
2022-11-16 20:58:48 +0100

    make test 219 test python3 string module

--- tests/219.py
+++ tests/219.py
@@ -1,31 +1,14 @@
-# string.* -> str.*?
+# string; what about str.*?
 
 import string
-print(string.join(['hello', 'world!']), string.join(['hello', 'world!'], '_'))
-# string.maketrans
-import string
-si = 'abcde'
-trans = string.maketrans('abc', 'xyz')
-print(si.translate(trans))
 
-# --- str.translate problem
-import string
-atable = string.maketrans("bc", "ef")
-print('abcdeg'.translate(atable, "cde"))
-gtable = string.maketrans("", "")
-word = 'aachen\n'
-key = word.translate(gtable, "a\n")
-print('word', repr(word))
-
-# --- string.{capitalize, capwords, swapcase, center, atoi, atol, atof}
-print(string.capitalize('hoi'), ' hoi'.capitalize())
-print(string.capwords('yo   momma')+'!'+string.capwords(' yo momma ')+'!'+string.capwords(' yo momma ', 'mm')+'!')
-allchars = ''.join([chr(cx) for cx in range(256)])
-print(repr(allchars.swapcase()), repr(string.swapcase(allchars)))
-print(string.center('hoi', 10), string.center('hoi', 10, 'u'))
-print('hoi'.center(10, 'u'))
-for i in range(10):
-    print('!'+'hoi'.center(i)+'!')
-print(string.atoi('+0x10', 0), string.atol('-100l', 0), string.atof('-1.234'))
-
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.capwords('ik haat smurven'))
+print(string.digits)
+print(string.hexdigits)
+print(string.octdigits)
+print(repr(string.printable))
+print(string.punctuation)
 print(repr(string.whitespace))
