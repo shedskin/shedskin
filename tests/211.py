441889b80befabe3918a77db230b2ebd44f1accd
Mark Dufour <mark.dufour@stcorp.nl>
2022-11-17 12:08:39 +0100

    minor change to file model
    in preparation for bin/str split, depending on passed flags.

--- tests/211.py
+++ tests/211.py
@@ -18,20 +18,20 @@ print(repr(b2a))
 a2b = binascii.a2b_hex(b2a)
 print(repr(a2b))
 
-b2a = binascii.b2a_hqx(str)
-print(repr(b2a))
-a2b,done = binascii.a2b_hqx(b2a) # returns a string instead of a tuple
-print(repr(a2b),done)
+#b2a = binascii.b2a_hqx(str)
+#print(repr(b2a))
+#a2b,done = binascii.a2b_hqx(b2a) # returns a string instead of a tuple
+#print(repr(a2b),done)
 
 b2a = binascii.b2a_base64(str)
 print(repr(b2a))
 a2b = binascii.a2b_base64(b2a)
 print(repr(a2b))
 
-b2a = binascii.rlecode_hqx(str)
-print(repr(b2a))
-a2b = binascii.rledecode_hqx(b2a)
-print(repr(a2b))
+#b2a = binascii.rlecode_hqx(str)
+#print(repr(b2a))
+#a2b = binascii.rledecode_hqx(b2a)
+#print(repr(a2b))
 
 b2a = binascii.hexlify(str)
 print(repr(b2a))
