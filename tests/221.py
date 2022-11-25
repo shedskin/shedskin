60759586e4999bcd34dc1943b0c7e2ae77dec7f5
Mark Dufour <mark.dufour@gmail.com>
2022-11-25 20:35:25 +0100

    list str/bytes/bytearray methods to test

---
+++ tests/221.py
@@ -0,0 +1,7 @@
+# str/bytes/bytearray
+
+# str: 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
+
+# str ^ bytes: {'casefold', 'isidentifier', 'fromhex', 'format', 'isprintable', 'encode', 'hex', 'decode', 'isdecimal', 'format_map', 'isnumeric'}
+
+# bytes ^ bytearray: {'__delitem__', 'pop', 'reverse', '__imul__', 'remove', 'append', '__getnewargs__', '__iadd__', 'copy', '__alloc__', 'insert', '__setitem__', 'clear', 'extend'}
