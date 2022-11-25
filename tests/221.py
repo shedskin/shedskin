eabe9e238324ce1e01c7956a5805f35597f4f3cd
Mark Dufour <mark.dufour@gmail.com>
2022-11-25 21:01:18 +0100

    add next tests to work on

--- tests/221.py
+++ tests/221.py
@@ -2,18 +2,30 @@
 
 # str
 
-# 'capitalize', 'casefold', 'center', 'count', 'expandtabs', 'find', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
+# 'capitalize', 'casefold', 'center', 'count', 'expandtabs', 'find', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isspace', 'istitle', 'isupper', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
 
 print('bla'.endswith('la'))
+print('-'.join(['a', 'b', 'c']))
+#print('bla'.isprintable(), chr(200).isprintable())
 
 # bytes
 
-# str ^ bytes: {'casefold', 'isidentifier', 'fromhex', 'format', 'isprintable', 'hex', 'decode', 'isdecimal', 'isnumeric'}
+# {'fromhex', 'hex'}
 
 print(b'bla'.endswith(b'la'))
+print(b'-'.join([b'a', b'b', b'c']))
+#print(b'bla'.hex())
 
-# bytes ^ bytearray: {'__delitem__', 'pop', 'reverse', '__imul__', 'remove', 'append', '__iadd__', 'copy', 'insert', '__setitem__', 'extend'}
+# bytes ^ bytearray: {'__delitem__', 'pop', 'reverse', '__imul__', 'remove', '__iadd__', 'copy', 'insert', 'extend'}
 
 ba = bytearray(b'bla')
 ba.clear()
 print(ba)
+
+ba = bytearray(b'bla')
+ba[1] = 80
+print(ba)
+
+#ba = bytearray(b'bla')
+#ba.append(81)
+#print(ba)
