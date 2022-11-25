bc29b1f5c25c5656d45411eb349d439d707b98ab
Mark Dufour <mark.dufour@gmail.com>
2022-11-25 20:53:01 +0100

    test endswith, clear

--- tests/221.py
+++ tests/221.py
@@ -1,7 +1,19 @@
 # str/bytes/bytearray
 
-# str: 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
+# str
 
-# str ^ bytes: {'casefold', 'isidentifier', 'fromhex', 'format', 'isprintable', 'encode', 'hex', 'decode', 'isdecimal', 'format_map', 'isnumeric'}
+# 'capitalize', 'casefold', 'center', 'count', 'expandtabs', 'find', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
 
-# bytes ^ bytearray: {'__delitem__', 'pop', 'reverse', '__imul__', 'remove', 'append', '__getnewargs__', '__iadd__', 'copy', '__alloc__', 'insert', '__setitem__', 'clear', 'extend'}
+print('bla'.endswith('la'))
+
+# bytes
+
+# str ^ bytes: {'casefold', 'isidentifier', 'fromhex', 'format', 'isprintable', 'hex', 'decode', 'isdecimal', 'isnumeric'}
+
+print(b'bla'.endswith(b'la'))
+
+# bytes ^ bytearray: {'__delitem__', 'pop', 'reverse', '__imul__', 'remove', 'append', '__iadd__', 'copy', 'insert', '__setitem__', 'extend'}
+
+ba = bytearray(b'bla')
+ba.clear()
+print(ba)
