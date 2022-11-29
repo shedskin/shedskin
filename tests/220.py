10b46ed56621677d1f35e7eea960d44d3ba5174c
Mark Dufour <mark.dufour@gmail.com>
2022-11-29 22:34:03 +0100

    move generator/next tests to 222
    final tests for next release?

--- tests/220.py
+++ tests/220.py
@@ -32,39 +32,6 @@ print(bytes(h))
 print(h/h)
 print(h//h)
 
-# next
-f = open('220.py')
-for x in range(5):
-    print(next(f))
-print('extra', f.__next__())
-
-file_iter = iter(f)
-print('iter', next(file_iter))
-
-# TODO next second arg
-
-# generators
-
-# reversed(range)
-#print(reversed(range(10,20,2)))
-
-# now iterators: dict_items, dict_keys, dict_values, like reversed, enumerate, itertools.imap?
-#print({1:2}.items())
-
-# different length args to map
-#def hoppa2(a, b):
-#    if b: return a+b
-#    return a+'X'
-#print(list(map(hoppa2, 'banaan', 'aap')))
-#
-#def hoppa3(a, b):
-#    if b: return a+b
-#    return a
-#print(list(map(hoppa3, range(8), range(4))))
-#
-#print(list(map(max, ['a','bc'], ['d'], ['e'])))
-#
-#print(list(map(set, [[1]])))
 
 # TODO os.popen2 disappeared? check
 #os.popen2 improvement
