56b7a741b1f66a31c3da873a33a5dba847b8c461
Mark Dufour <mark.dufour@gmail.com>
2022-12-02 19:37:35 +0100

    remove itertools.{imap, ifilter, izip}
    as these no longer exist in python3..

--- shedskin/lib/itertools.py
+++ shedskin/lib/itertools.py
@@ -33,9 +33,6 @@ def dropwhile(predicate, iterable):
 def groupby(iterable, key = lambda x: x):
     yield key(iter(iterable).__next__()), iter(iterable)
 
-def ifilter(predicate, iterable):
-    yield __pred_elem(predicate, iterable)
-
 def ifilterfalse(predicate, iterable):
     yield __pred_elem(predicate, iterable)
 
@@ -46,29 +43,11 @@ def islice(iterable, start, stop = -1, step = -1):
     'Known limitations: cannot distinguish between 0 and None for the stop argument'
     yield iter(iterable).__next__()
 
-def imap(function, *iterables):
-    'Known limitations: no more than 5 iterables are supported'
-    yield function(*iter(iterables).__next__())
-def __imap3(function, iter1, iter2):
-    yield function(iter(iter1).__next__(), iter(iter2).__next__())
-def __imap4(function, iter1, iter2, iter3):
-    yield function(iter(iter1).__next__(), iter(iter2).__next__(), iter(iter3).__next__())
-def __imap5(function, iter1, iter2, iter3, iter4):
-    yield function(iter(iter1).__next__(), iter(iter2).__next__(), iter(iter3).__next__(), iter(iter4).__next__())
-def __imap6(function, iter1, iter2, iter3, iter4, iter5):
-    yield function(iter(iter1).__next__(), iter(iter2).__next__(), iter(iter3).__next__(), iter(iter4).__next__(), iter(iter5).__next__())
-
 #def starmap(function, iterable):
 #    yield func(*iterable[0])
 
 def tee(iterable, n = 2):
     return iter(iterable), iter(iterable)
-
-def izip(*iterables):
-    'Known limitations: iterables must all be of the same type if they are more than two'
-    yield iter(iterables).__next__(),
-def __izip2(iterable1, iterable2):
-    yield iter(iterable1).__next__(), iter(iterable2).__next__()
 
 def izip_longest(__kw_fillvalue=None, *iterables):
     'Known limitations: iterables must all be of the same type, cannot distinguish between 0 and None for the return value'
