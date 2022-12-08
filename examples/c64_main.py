e1a30722d8087850f0c21f5a724e30fc7b2a7b1a
Mark Dufour <mark.dufour@gmail.com>
2022-12-08 22:33:56 +0100

    fix c64 example
    (well except for the extern issue on OSX)
    
    removed key-handling models before - add in again.

--- examples/c64_main.py
+++ examples/c64_main.py
@@ -6,9 +6,8 @@ from gi.repository import Gtk, Gdk, GLib, GdkPixbuf, GLib
 
 import sys
 import time
-import c64
-print(c64)
 from c64 import c64, screens
+print(c64)
 from c64.symbols import S_A, S_X, S_Y, S_SP, S_PC
 from optparse import OptionParser
 
