0d4a16099639b0e5a3a6fd57125b982b7db072b0
Mark Dufour <mark.dufour@gmail.com>
2022-12-08 16:30:46 +0100

    avoid crash in str binding
    (when converting non-ascii to str, interpret as latin1)

--- shedskin/lib/builtin/str.cpp
+++ shedskin/lib/builtin/str.cpp
@@ -850,9 +850,10 @@ str::str(PyObject *p) : hash(-1) {
 }
 
 PyObject *str::__to_py__() {
+//    return PyBytes_FromStringAndSize("bla", 3);
     // return PyString_FromStringAndSize(c_str(), size());
     // return PyBytes_FromStringAndSize(c_str(), size());
-    return PyUnicode_FromStringAndSize(c_str(), size());
+    return PyUnicode_DecodeLatin1(c_str(), size(), "");
 }
 #endif
 
