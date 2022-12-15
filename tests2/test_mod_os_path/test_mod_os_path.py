8d5b18273a550d1562bb806cf2e3255d2a430566
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-12-15 14:55:21 +0300

    improved str tests

--- tests2/test_mod_os_path/test_mod_os_path.py
+++ tests2/test_mod_os_path/test_mod_os_path.py
@@ -1,5 +1,6 @@
 import os.path
 from os.path import *
+import os
 
 def test_os_path_join():
     assert os.path.join("heuk") == 'heuk'
@@ -14,14 +15,16 @@ def test_os_path():
     assert splitdrive("hoei/woei") == ('', 'hoei/woei')
     assert basename("hoei/woei") == 'woei'
     assert dirname("hoei/woei") == 'hoei'
-    assert exists("testdata")
-    assert lexists("testdata")
-    assert isdir("testdata")
-    assert not isfile("testdata")
-    # assert getsize('test_hello.py') == 23
-    # assert getatime("test_hello.py") > 1 # dummy: cannot test for time
-    # assert getctime("test_hello.py") > 1 # dummy: cannot test for time
-    # assert getmtime("test_hello.py") > 1 # dummy: cannot test for time
+
+    assert exists("../testdata")
+    assert lexists("../testdata")
+    assert isdir("../testdata")
+    assert not isfile("../testdata")
+
+    assert getsize("../test_hello.py") == 23
+    assert getatime("../test_hello.py") > 1 # dummy: cannot test for time
+    assert getctime("../test_hello.py") > 1 # dummy: cannot test for time
+    assert getmtime("../test_hello.py") > 1 # dummy: cannot test for time
 
 
 
--- tests2/test_type_str.py
+++ tests2/test_type_str.py
@@ -1,24 +1,219 @@
+# 221.py
+# TODO hex(bytes_per_sep), bytes.fromhex
+# TODO maketrans, translate
+
+# str
+
+def test_capitalize():
+    assert 'bla bla'.capitalize() == 'Bla bla'
+
+def test_casefold():
+    assert 'BLA'.casefold()
+
+def test_center():
+    assert 'bla'.center(10) == '   bla    '
+    assert 'bla'.center(10, '-') == '---bla----'
+
+def test_count():
+    assert 'blaa'.count('a') == 2
+    assert 'blaabla'.count('aa') == 1
+
+def test_encode(): pass
+
+def test_endswith():
+    assert 'bla'.endswith('la')
+    assert not 'bla'.endswith('xx')
+
+def test_expandtabs():
+    assert 'bla\tbla'.expandtabs() == 'bla     bla'
+
+def test_find():
+    assert 'bla'.find('la') == 1
+    assert 'bla'.find('ba') == -1
+
+def test_format(): pass
+
+def test_format_map(): pass
+
+def test_index():
+    assert 'bla'.index('la') == 1
+    assert 'bla'.index('bl') == 0
+
+def test_isalnum():
+    assert 'bla'.isalnum()
+    assert '123'.isalnum()
+    assert '12a'.isalnum()
+    assert not ''.isalnum()
+
+def test_isalpha():
+    assert 'bla'.isalpha()
+    assert not '123'.isalpha()
+    assert not ''.isalpha()
+
+def test_isascii():
+    assert 'bla'.isascii()
+    assert not '\xf0'.isascii()
+    assert ''.isascii()
+
+def test_isdecimal():
+    assert not 'bla'.isdecimal()
+    assert '123'.isdecimal()
+    assert not ''.isdecimal()
+
+def test_isdigit():
+    assert not 'bla'.isdigit()
+    assert '123'.isdigit()
+    assert not ''.isdigit()
+
+def test_isidentifier():
+    assert 'Bla_'.isidentifier()
+    assert not '9bla'.isidentifier()
+    assert not ''.isidentifier()
+
+def test_islower():
+    assert not 'BLA'.islower()
+    assert 'bla'.islower()
+    assert not ''.islower()
+
+def test_isnumeric():
+    assert not 'bla'.isnumeric()
+    assert '123'.isnumeric()
+    assert not ''.isnumeric()
+
+def test_isprintable():
+    assert 'bla'.isprintable()
+    assert chr(200).isprintable()
+    assert ''.isprintable()
+
+def test_isspace():
+    assert not 'bla'.isspace()
+    assert '   '.isspace()
+    assert not ''.isspace()
+
+def test_istitle():
+    assert not 'bla'.istitle()
+    assert 'Bla'.istitle()
+    assert not ''.istitle()
+    assert "This Is A Title".istitle()
+    assert not "This is not a title".istitle()
+
+def test_isupper():
+    assert 'BLA'.isupper()
+    assert not 'bla'.isupper()
+    assert not ''.isupper()
+
+def test_join():
+    assert '-'.join(['a', 'b', 'c']) == "a-b-c"
+
+def test_ljust():
+    assert 'bla'.ljust(8) == 'bla     '
+    assert 'bla'.ljust(6, '+') == 'bla+++'
+
+def test_lower():
+    assert 'BLA'.lower() == 'bla'
+
+def test_lstrip():
+    assert ' bla'.lstrip() == 'bla'
+
+def test_maketrans(): pass
+
+def test_partition():
+    assert "a and b and c".partition("and") == ('a ', 'and', ' b and c')
+    assert 'aa-bb-cc'.partition('-') ==  ('aa', '-', 'bb-cc')
+
+def test_removeprefix(): pass
+
+def test_removesuffix(): pass
+
+def test_replace():
+    assert 'bla'.replace('la', 'bla') == 'bbla'
+
+def test_rfind():
+    assert 'bla'.rfind('la') == 1
+    assert 'bla'.rfind('ba') == -1
+
+def test_rindex():
+    assert 'bla'.rindex('la') == 1
+    assert 'bla'.rindex('bl') == 0
+
+def test_rjust():
+    assert 'bla'.rjust(8) == '     bla'
+    assert 'bla'.rjust(6, '-') ==  '---bla'
+
+def test_rpartition():
+    assert "a and b and c".rpartition("and") == ('a and b ', 'and', ' c')
+    assert 'aa-bb-cc'.rpartition('-')
+
+def test_rsplit():
+    assert 'bla'.rsplit('l') == ['b', 'a']
+    assert 'b l a'.rsplit() == ['b', 'l', 'a']
+    assert 'haajaaja'.rsplit('aa') == ['h', 'j', 'ja']
+
+def test_rstrip():
+    assert 'bla'.rstrip('a') == 'bl'
+
+def test_split():
+    assert 'bla'.split('l') == ['b', 'a']
+    assert 'b l a'.split() == ['b', 'l', 'a']
+    assert 'haajaaja'.split('aa') == ['h', 'j', 'ja']
+    assert "hoei hoei".split() == ['hoei', 'hoei']
+    assert "hoei hoei\\n".split() == ['hoei', 'hoei\\n']
+
+def test_splitlines():
+    assert "ab\ncd\r\nef\rghi\n".splitlines() == ['ab', 'cd', 'ef', 'ghi']
+    assert "ab\ncd\r\nef\rghi\n".splitlines(1) == ['ab\n', 'cd\r\n', 'ef\r', 'ghi\n']
+
+def test_startswith():
+    assert 'bla'.startswith('bla')
+    assert not 'bla'.startswith('xx')
+
+def test_strip():
+    assert 'bla  '.strip() == 'bla'
+    assert '**bla**'.strip('*') == 'bla'
+
+def test_swapcase():
+    assert 'bLa'.swapcase() == 'BlA'
+
+def test_title():
+    assert 'bla bla'.title() == 'Bla Bla'
+
+def test_translate(): pass
+
+def test_upper():
+    assert 'bla'.upper() == 'BLA'
+
+def test_zfill():
+    assert 'bla'.zfill(10) == '0000000bla'
+
 def heuk(x):
-    return
+    return    
+
+def test_str_cmp():
+    assert "hoei\\n" != "hoei\n"
+    assert 'aap' < 'blup'
+    assert 'aap' <= 'blup'
+    assert 'bla' != 'blup'
+    assert 'bla' == 'bla'
+    assert 'bla' != 'blup'
+    assert 'bla'*2 == 'blabla'
+    assert 'bla'*3 == 'blablabla'
+    assert 'bla'+'bla' == 'blabla'
+    assert 'bla'+'bla' == 'blabla'
+    assert 'bla'[1:] == 'la'
+    assert 'bla'[1:] == 'la'
+    assert 'bla'[1] == 'l'
+    assert 'bla'[1] == 'l'
+    assert 'bla'[::-1] == 'alb'
+    assert 'bla'[::-1] == 'alb'
+    assert 'blup' >= 'aap'
+    assert 2*'bla' == 'blabla'
+    assert 3*'bla' == 'blablabla'
+    assert list('aap') == ['a', 'a', 'p']
+    assert list(sorted(['blup', 'aap'])) == ['aap', 'blup']
 
 
 def test_str_concat():
     assert "x" + "x" + "x" == "xxx"
-
-def test_str_methods():
-    # missing string methods
-    assert "ab\ncd\r\nef\rghi\n".splitlines() == ['ab', 'cd', 'ef', 'ghi']
-    assert "ab\ncd\r\nef\rghi\n".splitlines(1) == ['ab\n', 'cd\r\n', 'ef\r', 'ghi\n']
-    assert "This Is A Title".istitle()
-    assert not "This is not a title".istitle()
-    assert "a and b and c".partition("and") == ('a ', 'and', ' b and c')
-    assert "a and b and c".rpartition("and") == ('a and b ', 'and', ' c')
-
-    ah = "hatsie flatsie pots"
-    assert ah == " ".join(ah.split())
-    assert "hoei hoei".split() == ['hoei', 'hoei']
-    assert "hoei hoei\\n".split() == ['hoei', 'hoei\\n']
-    assert "hoei\\n" != "hoei\n"
 
 
 def test_str_overload():
@@ -30,7 +225,54 @@ def test_str_overload():
 
 
 if __name__ == '__main__':
-    test_str_methods()
+    test_str_cmp()
     test_str_concat()
     test_str_overload()
 
+    test_capitalize()
+    test_casefold()
+    test_center()
+    test_count()
+    test_encode()
+    test_endswith()
+    test_expandtabs()
+    test_find()
+    test_format()
+    test_format_map()
+    test_index()
+    test_isalnum()
+    test_isalpha()
+    test_isascii()
+    test_isdecimal()
+    test_isdigit()
+    test_isidentifier()
+    test_islower()
+    test_isnumeric()
+    test_isprintable()
+    test_isspace()
+    test_istitle()
+    test_isupper()
+    test_join()
+    test_ljust()
+    test_lower()
+    test_lstrip()
+    test_maketrans()
+    test_partition()
+    test_removeprefix()
+    test_removesuffix()
+    test_replace()
+    test_rfind()
+    test_rindex()
+    test_rjust()
+    test_rpartition()
+    test_rsplit()
+    test_rstrip()
+    test_split()
+    test_splitlines()
+    test_startswith()
+    test_strip()
+    test_swapcase()
+    test_title()
+    test_translate()
+    test_upper()
+    test_zfill()
