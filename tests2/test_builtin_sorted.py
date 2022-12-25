4636266d3d3d7dc4a47f9df69ddf56e9948e1ece
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-12-25 11:21:17 +0400

    fix and more tests

--- tests2/test_builtin_sorted.py
+++ tests2/test_builtin_sorted.py
@@ -42,7 +42,6 @@ a_sorted = [
 def test_sorted1():
     assert list(sorted(a)) == a_sorted
 
-
 def test_sorted2():
     b = [[3, 2], [1, 3]]
     assert list(sorted(b)) == [[1, 3], [3, 2]]
@@ -51,12 +50,16 @@ def test_sorted3():
     c = ["b", "c", "aa"]
     assert list(sorted(c)) == ['aa', 'b', 'c']
 
+def test_reversed():
+    assert [z for z in reversed(range(10))] == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    assert list(reversed(['a','c','b'])) == ['b', 'c', 'a']
 
 
 def test_all():
     test_sorted1()
     test_sorted2()
     test_sorted3()
+    test_reversed()
 
 if __name__ == '__main__':
     test_all()
---
+++ tests2/test_builtins_functional.py
@@ -0,0 +1,39 @@
+
+
+def test_filter():
+    assert list(filter(lambda c: c > "a", "abaaac")) == ['b', 'c']
+
+def test_reversed():
+    assert list(reversed(range(10, 20, 2))) == [18, 16, 14, 12, 10]
+
+def test_enumerate():
+    assert list(enumerate("bun")) == [(0, 'b'), (1, 'u'), (2, 'n')]
+
+def test_range():
+    assert list(range(11, 4, -2)) == [11, 9, 7, 5]
+    assert len(range(8, 20)) == 12
+    assert range(8, 20)[5] == 13
+    assert range(8, 20)[-2] == 18
+
+def test_zip():
+    assert list(zip()) == []
+    assert list(zip([1,2])) == [(1,), (2,)]
+    assert list(zip([1, 2], [3, 4])) == [(1, 3), (2, 4)]
+    assert list(list(zip([1, 2], [3, 4], [5, 6]))) == [(1, 3, 5), (2, 4, 6)]
+
+def test_map():
+    assert list(map(lambda a: 2 * a, [1, 2, 3])) == [2, 4, 6]
+    assert list(map(lambda a, b: a * b, [1, 2, 3], [4, 5])) == [4, 10]
+    assert list(map(lambda a, b, c: a + b + c, [1, 2, 3], [3, 4, 5], [5, 4, 3])) == [9, 10, 11]
+
+def test_all():
+    test_filter()
+    test_reversed()
+    test_enumerate()
+    test_range()
+    test_zip()
+    test_map()
+
+if __name__ == '__main__':
+    test_all()
+
