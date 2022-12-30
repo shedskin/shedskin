c64ab507a8cfff6ac7c2167308983dbfa96671de
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2022-12-30 10:28:43 +0400

    rest of support modules

---
+++ tests2/test_mod_io/test_mod_io.py
@@ -0,0 +1,47 @@
+import io
+import os
+
+
+def test_io_from_file():
+    if not os.path.exists("testdata"):
+        testdata = "../testdata"
+    else:
+        testdata = "testdata"
+
+    datafile = os.path.join(testdata, "test_io.txt")
+
+    with open(datafile, "rb") as f:
+        sio = io.BytesIO(f.read())
+        lines = sio.readlines()
+        assert len(lines) == 6
+
+def test_io_read_from_binary_string():
+        sio = io.BytesIO(b"blaat")
+        assert sio.seek(-3, 2) == 2
+        sio.read() == b'aat'
+
+def test_io_write_to_binary_string():
+        sio = io.BytesIO()
+        assert sio.tell() == 0
+        assert sio.write(b"hallo\njoh") == 9
+        assert sio.tell() == 9
+        assert sio.seek(0, 0) == 0
+        assert sio.tell() == 0
+        assert sio.readlines() == [b'hallo\n', b'joh']
+        assert sio.tell() == 9
+        assert sio.seek(0, 0) == 0
+        assert sio.tell() == 0
+        assert sio.write(b"hoi") == 3
+        assert sio.tell() == 3
+        assert sio.readlines() == [b'lo\n', b'joh']
+        assert sio.tell() == 9
+
+def test_all():
+    assert True
+    # test_io_from_file()
+    # test_io_read_from_binary_string()
+    # test_io_write_to_binary_string()
+
+
+if __name__ == '__main__':
+    test_all() 
---
+++ tests2/test_mod_socket/test_mod_socket.py
@@ -0,0 +1,11 @@
+import socket
+
+
+def test_socket():
+    assert True
+
+def test_all():
+    test_socket()
+
+if __name__ == '__main__':
+    test_all() 
