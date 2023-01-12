627b1ec3dfe75cbd8ec18c608ad481a0a1f98d3c
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2023-01-12 08:15:23 +0000

    fixed minilight

---
+++ examples2/_scripts/get_deps.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+def get_tree(path, options=None):
+    parentdir = os.path.dirname(__file__)
+    results = []
+    # endings = ['.cpp', '.hpp']
+    endings = ['.py']
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if any(f.endswith(e) for e in endings):
+                name = os.path.splitext(f)[0]
+                results.append(os.path.join(root, name))
+    return sorted(results)
+
+
+
+def commandline():
+    parser = argparse.ArgumentParser(
+        prog = 'get_tree',
+        description = 'get tree of translated depedencies (.hpp, .cpp)')
+    arg = opt = parser.add_argument
+    arg('path', help='path to root')
+
+    args = parser.parse_args()
+    if args.path:
+        tree = get_tree(args.path, args)
+        for elem in tree:
+            print(elem)
+
+commandline()
