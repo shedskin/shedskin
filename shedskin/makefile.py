c070886b4e686f81761319954472c8e6f0fe2522
Mark Dufour <mark.dufour@gmail.com>
2023-03-19 12:02:16 +0100

    Revert "sys.platform under mingw gives 'cygwin', so check for this"
    This reverts commit 8614a1b4e41c51bb448bbd0187b9c2e93599d86d.

--- shedskin/makefile.py
+++ shedskin/makefile.py
@@ -108,7 +108,7 @@ def generate_makefile(gx):
         flags = "/etc/shedskin/FLAGS"
     elif gx.msvc:
         flags = gx.shedskin_flags / "FLAGS.msvc"
-    elif sys.platform == "cygwin":
+    elif sys.platform == "win32":
         flags = gx.shedskin_flags / "FLAGS.mingw"
     elif sys.platform == "darwin":
         flags = gx.shedskin_flags / "FLAGS.osx"
