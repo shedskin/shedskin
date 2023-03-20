df0708904f57d8ec75853539c3e7ea56482a4bfa
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2023-03-20 09:01:03 +0300

    added custom cmake options to test

--- shedskin/cmake.py
+++ shedskin/cmake.py
@@ -573,7 +573,7 @@ class CMakeBuilder:
         """process shedskin program with cmake"""
         start_time = time.time()
 
-        cfg_options = []
+        cfg_options = [] if not self.options.cfg else [f'-D{opt}' for opt in self.options.cfg]
         bld_options = []
         tst_options = []
 
