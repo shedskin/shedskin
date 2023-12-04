c56a690588c419afc59de7c353d41ce77e72d47f
Mark Dufour <mark.dufour@gmail.com>
2023-12-04 18:53:04 +0100

    inline out-of-bounds exception in __wrap
    making indexing a lot faster, at least on my system

--- shedskin/lib/builtin.hpp
+++ shedskin/lib/builtin.hpp
@@ -1425,7 +1425,7 @@ template<class T> static inline int __wrap(T a, __ss_int i) {
 #endif
 #ifndef __SS_NOBOUNDS
         if(unlikely(i<0 || i>= l))
-            __throw_index_out_of_range();
+            throw new IndexError(new str("index out of range")); // inlined for performance
 #endif
     return i;
 }
