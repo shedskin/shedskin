d8704354ee43f31acd3e0856093313ff55960a20
Mark Dufour <mark.dufour@gmail.com>
2023-12-14 17:10:27 +0100

    gs example port to pygame 1/2

--- examples/gs/gs_main.py
+++ examples/gs/gs_main.py
@@ -7,15 +7,12 @@ import array
 import sys
 import time
 
-import gi
-gi.require_version("Gtk", "3.0")
-
-from gi.repository import Gtk, Gdk, GdkPixbuf
+import pygame
 
 from Gh0stenstein import world_manager
 print(world_manager)
 
-WIDTH, HEIGHT = 1024, 768
+WIDTH, HEIGHT = 800, 600
 
 worldMap = [
     [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 4, 6, 4, 4, 6, 4, 6, 4, 4, 4, 6, 4],
@@ -71,100 +68,71 @@ sprite_positions = [
     (10.5, 15.8, 9),
 ]
 
-class EventBox(Gtk.EventBox):
-    def __init__(self, view):
-        Gtk.EventBox.__init__(self)
-        self.view = view
-        self.props.can_focus = True
-        self.pressed = {}
-        self.connect("key-press-event", self.handle_key_press)
-        self.connect("key-release-event", self.handle_key_release)
-
-    def handle_key_press(self, widget, event):
-        keyval = Gdk.keyval_name(event.keyval)
-        if keyval == 'q':
-            sys.exit()
-        self.pressed[keyval] = True
-
-    def handle_key_release(self, widget, event):
-        self.pressed[Gdk.keyval_name(event.keyval)] = False
-
-
-class View(object):
-    def __init__(self):
-        self.frame_time = 0.0
-
-        self.wm = world_manager.WorldManager(WIDTH, HEIGHT, worldMap, sprite_positions, 22, 11.5, -1, 0, 0, .66)
-
-        load_image(self.wm, 0, 'Gh0stenstein/pics/background.png', WIDTH, HEIGHT)
-
-        load_image(self.wm, 1, 'Gh0stenstein/pics/walls/eagle.png', 64, 64)
-        load_image(self.wm, 2, 'Gh0stenstein/pics/walls/redbrick.png', 64, 64)
-        load_image(self.wm, 3, 'Gh0stenstein/pics/walls/purplestone.png', 64, 64)
-        load_image(self.wm, 4, 'Gh0stenstein/pics/walls/greystone.png', 64, 64)
-        load_image(self.wm, 5, 'Gh0stenstein/pics/walls/bluestone.png', 64, 64)
-        load_image(self.wm, 6, 'Gh0stenstein/pics/walls/mossy.png', 64, 64)
-        load_image(self.wm, 7, 'Gh0stenstein/pics/walls/wood.png', 64, 64)
-        load_image(self.wm, 8, 'Gh0stenstein/pics/walls/colorstone.png', 64, 64)
-
-        load_image(self.wm, 9, 'Gh0stenstein/pics/items/GoldBar.png', 64, 64)
-        load_image(self.wm, 10, 'Gh0stenstein/pics/items/PlantInPot.png', 64, 64)
-        load_image(self.wm, 11, 'Gh0stenstein/pics/items/BlueOverheadLight.png', 64, 64)
-
-        self.window = Gtk.Window()
-        self.event_box = EventBox(self)
-        self.drawing_area = Gtk.DrawingArea()
-        self.drawing_area.connect("draw", self.repaint)
-        self.drawing_area.set_size_request(WIDTH, HEIGHT)
-        self.drawing_area.show()
-        self.event_box.show()
-        box = Gtk.HBox()
-        self.event_box.add(self.drawing_area)
-        box.pack_start(self.event_box, False, False, 0)
-        box.show()
-        self.window.add(box)
-        self.window.show_all()
-
-        self.times = 0
-        self.t0 = 0
-
-    def repaint(self, drawarea, pCr):
-        t0 = time.time()
-
-        for key, val in self.event_box.pressed.items():
-            if val:
-                move_speed = self.frame_time * 6.0  # the constant value is in squares / second
-                rot_speed = self.frame_time * 2.0
-                self.wm.move(key, move_speed, rot_speed)
-        t0 = time.time()
-        self.wm.draw()
-        s = self.wm.dump()
-
-        self.pixbuf = GdkPixbuf.Pixbuf.new_from_data(s, GdkPixbuf.Colorspace.RGB, True, 8, WIDTH, HEIGHT, WIDTH * 4)  # TODO optimize!!
-
-        Gdk.cairo_set_source_pixbuf(pCr, self.pixbuf, 5, 5)
-        pCr.paint()
-
-        self.frame_time = time.time() - t0
-        self.times += 1
-        if self.times == 10:
-            print('FPS: %.2f' % (1 / ((time.time() - self.t0) / 10)))
-            self.times = 0
-            self.t0 = time.time()
-
-        self.drawing_area.queue_draw()
 
 def load_image(wm, pos, filename, w, h):
-    pb = GdkPixbuf.Pixbuf.new_from_file(filename)
-    pb = pb.add_alpha(False, 0,0,0)
-    pb = pb.scale_simple(w, h, GdkPixbuf.InterpType.NEAREST)
-    pixels = pb.get_pixels()
-    arr = array.array('I', pixels)
+    surf = pygame.image.load(filename)
+    surf = pygame.transform.scale(surf, (w, h))
+
+    img = pygame.image.tostring(surf, 'RGBA')
+
+    arr = array.array('I')
+    arr.frombytes(img)
     wm.load_image(pos, arr.tolist(), w, h)
 
+
 def main():
-    View()
-    Gtk.main()
+    screen = (WIDTH, HEIGHT)
+    pygame.init()
+    surface = pygame.display.set_mode(screen)
+    drawsurf = pygame.Surface(screen).convert()
+    drawsurf.set_colorkey((0, 0, 0))
+
+    wm = world_manager.WorldManager(WIDTH, HEIGHT, worldMap, sprite_positions, 22, 11.5, -1, 0, 0, .66)
+
+    load_image(wm, 0, 'Gh0stenstein/pics/background.png', WIDTH, HEIGHT)
+    load_image(wm, 1, 'Gh0stenstein/pics/walls/eagle.png', 64, 64)
+    load_image(wm, 2, 'Gh0stenstein/pics/walls/redbrick.png', 64, 64)
+    load_image(wm, 3, 'Gh0stenstein/pics/walls/purplestone.png', 64, 64)
+    load_image(wm, 4, 'Gh0stenstein/pics/walls/greystone.png', 64, 64)
+    load_image(wm, 5, 'Gh0stenstein/pics/walls/bluestone.png', 64, 64)
+    load_image(wm, 6, 'Gh0stenstein/pics/walls/mossy.png', 64, 64)
+    load_image(wm, 7, 'Gh0stenstein/pics/walls/wood.png', 64, 64)
+    load_image(wm, 8, 'Gh0stenstein/pics/walls/colorstone.png', 64, 64)
+    load_image(wm, 9, 'Gh0stenstein/pics/items/GoldBar.png', 64, 64)
+    load_image(wm, 10, 'Gh0stenstein/pics/items/PlantInPot.png', 64, 64)
+    load_image(wm, 11, 'Gh0stenstein/pics/items/BlueOverheadLight.png', 64, 64)
+
+    clock = pygame.time.Clock()
+    frame_count = 0
+
+    ingame = True
+    while ingame:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_q]:
+            ingame = False
+
+        # quit?
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                ingame = False
+
+        # render!
+        t0 = time.time()
+
+        wm.draw()
+
+        img = pygame.image.frombuffer(wm.dump(), (WIDTH, HEIGHT), 'RGBX')
+        surface.blit(img, (0, 0))
+        pygame.display.flip()
+
+        clock.tick(60)
+
+        delta = (time.time()-t0)
+
+        if frame_count % 10 == 0:
+            print('FPS %.2f' % (1/delta))
+        frame_count += 1
 
 
 if __name__ == '__main__':
