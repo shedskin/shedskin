3209c3cbc47950062b76d334693c138e34800687
Mark Dufour <mark.dufour@gmail.com>
2023-12-15 12:52:15 +0100

    mandelbrot2: port to pygame

--- examples/mandelbrot2/mandelbrot2_main.py
+++ examples/mandelbrot2/mandelbrot2_main.py
@@ -1,99 +1,74 @@
 # interactive mandelbrot program
 # copyright Tony Veijalainen, tony.veijalainen@gmail.com
 
-try:
-    import tkinter as tk
-except ImportError:
-    import tkinter as tk
-
 import os
-from PIL import Image, ImageTk
 
 import mandelbrot2
 print(mandelbrot2)
 from mandelbrot2 import mandel_file
 
-main_file = 'm-1 0i_3.5_240.bmp'
-class MandelbrotTk(tk.Tk):
-    def __init__(self, width=640, height=480, image_file=main_file):
-        tk.Tk.__init__(self)
-        self.canvas = tk.Canvas(width=width, height=height)
-        self.geometry("%sx%s+100+100" % (width,height+40))
-        self.canvas.pack()
-        self.item = None
-        
-        self.label = tk.Label(self.canvas, font=('courier', 10))
-        self.parameters_from_fn(image_file)
-        if not os.path.isfile(image_file):
-            self.image_file = mandel_file(self.cx, self.cy, self.fsize, self.max_iterations)
-            print(('Mainfile %s generated' % self.image_file))
-        else:
-            self.image_file = image_file           
-        self.label.pack(side=tk.BOTTOM)
+import pygame
 
-        self.canvas.image = self.load_image()
-        self.x, self.y, self.size = None, None, (0,)
-        self.bind("<Button-2>", self.recalculate)
-        # Linux
-        self.bind("<Button-1>", self.zoom_in)
-        self.bind("<Button-3>", self.zoom_out)
-        # Windows
-        self.bind("<MouseWheel>", self.mouse_wheel)
+WIDTH, HEIGHT = 1024, 768
 
-    def recalculate(self, event):
-        print('recalculating')
-        self.image_file = mandel_file(self.cx, self.cy, self.fsize, self.max_iterations)
-        self.canvas.image = self.load_image()
-        self.update_label()
-        
-    def parameters_from_fn(self, fn):
-        # extract parameters from name
-        ipos = fn.find('i')
-        self.cx, self.cy = list(map(float, fn[1:ipos].split()))
-        self.fsize, rest = fn[ipos+2:].split('_', 1)
-        self.fsize = float(self.fsize)
-        self.max_iterations = int(rest.split('.',1)[0])
-        self.update_label()
 
-    def load_image(self):   
-        self.canvas.delete('m')
-        photo = Image.open(self.image_file)
-        im = ImageTk.PhotoImage(photo)
-        self.canvas.create_image(0, 0, image=im, anchor='nw', tag='m')
-        self.canvas.pack(fill=tk.BOTH, expand=tk.YES)
-        self.max_iterations = min(max(self.max_iterations, 256), 10000)
-        self.update_label()
-        self.step = self.fsize / max(im.width(), im.height())
-        return im
+def main():
+    screen = (WIDTH, HEIGHT)
+    pygame.init()
+    surface = pygame.display.set_mode(screen)
+    drawsurf = pygame.Surface(screen).convert()
+    drawsurf.set_colorkey((0, 0, 0))
 
-    def update_label(self):
-        self.label['text'] = ('cx: %g, cy: %g, fsize: %g, max_iterations: %i'  %
-              (self.cx, self.cy, self.fsize, self.max_iterations))
-        
-    def zoom_in(self, event):
-        print('ZOOM in')
-        xshift, yshift = event.x - self.canvas.image.width() // 2, self.canvas.image.height() // 2 - event.y
-        self.fsize /= 4.0
-        self.cx, self.cy = xshift * self.step + self.cx, yshift * self.step + self.cy
-        self.image_file = mandel_file(self.cx, self.cy, self.fsize, self.max_iterations)
-        self.canvas.image = self.load_image()
+    # get params from file name
+    fn = 'm-1 0i_3.5_240.bmp'
+    ipos = fn.find('i')
+    cx, cy = list(map(float, fn[1:ipos].split()))
+    fsize, rest = fn[ipos+2:].split('_', 1)
+    fsize = float(fsize)
+    max_iterations = int(rest.split('.',1)[0])
+    step = fsize / max(WIDTH, HEIGHT)
 
-    def zoom_out(self, event):
-        print('zoom out')
-        xshift, yshift = event.x - self.canvas.image.width() // 2, self.canvas.image.height() // 2 - event.y
-        self.fsize *= 4.0
-        self.cx, self.cy =  xshift * self.step + self.cx, yshift * self.step + self.cy
-        self.image_file = mandel_file(self.cx, self.cy, self.fsize, self.max_iterations)
-        self.canvas.image = self.load_image()
+    def render(bmp):
+        img = pygame.image.load(bmp)
+        surface.blit(img, (0, 0))
+        pygame.display.flip()
 
-    def mouse_wheel(self, event):
-        """respond to Linux or Windows wheel event"""
-        if event.num == 5 or event.delta == -120:
-            self.max_iterations -= 120
-        if event.num == 4 or event.delta == 120:
-            self.max_iterations += 120
-        self.update_label()
+    # render initial image
+    render(mandel_file(cx, cy, fsize, max_iterations, WIDTH, HEIGHT))
+
+    clock = pygame.time.Clock()
+
+    ingame = True
+    while ingame:
+        # quit?
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_q]:
+            ingame = False
+
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                ingame = False
+
+            # check mouse
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                xshift, yshift = event.pos[0] - WIDTH // 2, HEIGHT // 2 - event.pos[1]
+                cx, cy = xshift * step + cx, yshift * step + cy
+
+                # zoom in/out?
+                if event.button == 1:
+                    print('zoom in')
+                    fsize /= 2.0
+
+                elif event.button == 3:
+                    print('zoom out')
+                    fsize *= 2.0
+
+                step = fsize / max(WIDTH, HEIGHT)
+
+                # render updated image
+                render(mandel_file(cx, cy, fsize, max_iterations, WIDTH, HEIGHT))
+
+        clock.tick(60)
 
 if __name__ == '__main__':
-    mand = MandelbrotTk()
-    mand.mainloop()
+    main()
