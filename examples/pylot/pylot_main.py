db0e292969d72633606b5b12a168f6fc1019ad88
Mark Dufour <mark.dufour@gmail.com>
2023-12-16 16:21:20 +0100

    pylot: further minimizing before switch to pygame

--- examples/pylot/pylot_main.py
+++ examples/pylot/pylot_main.py
@@ -17,24 +17,27 @@
 # You should have received a copy of the GNU General Public License
 # along with Foobar.  If not, see <http://www.gnu.org/licenses/>.import math
 
-#TODO:
-#   Softer lighting, by supersampling light sources
-#   Texture/bump/normal mapping.  Mipmaps for the textures, at least.
-#   Hierarchies with transforms--probably should wait until we see what a real
-#     file format looks like.
-#   Bounding boxes for hierarchies and complex shapes
-
+import random
 import time
+
+import pygame
+
+
+
 from tkinter import *
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageTk
-import random
 import pylot
 from pylot.Pool import ThreadedQueueProcessor
 from pylot.Utils import *
 from pylot import SimpleGeometry
 print(SimpleGeometry.__file__)
+
+WIDTH = 320
+BLOCKS_WIDE = 20
+BLOCKS_TALL = 20
+
 
 class CameraHandler(object):
   def __init__(self, camera):
@@ -50,31 +53,11 @@ class App(object):
   def __init__(self, root, viewports):
     self.root = root
     self.viewports = viewports
-#    self.topButtonFrame = Frame(root)
-#    self.topButtonFrame.pack(side=TOP)
-#    self.botButtonFrame = Frame(root)
-#    self.botButtonFrame.pack(side=BOTTOM)
-#    self.button = Button(self.topButtonFrame, text="Quit",
-#                         command=self.shutdown)
-#    self.button.pack(side=LEFT)
-#
-#    self.button = Button(self.topButtonFrame, text="Save")
-#    self.button.pack(side=LEFT)
-#
-#    self.button = Button(self.botButtonFrame, text="Process",
-#                         command=self.process)
-#    self.button.pack(side=LEFT)
-#
-#    self.button = Button(self.botButtonFrame, text="Stop")
-#    self.button.pack(side=LEFT)
     self.labels = []
     for v in viewports:
       label = Label(root)
       self.labels.append(label)
       label.pack(side=LEFT)
-      def clickHandler(v):
-        return lambda event: v.handleClick(event)
-      label.bind("<Button-1>", clickHandler(v))
       v.setLabel(label)
       v.setApp(self)
     root.bind("<Destroy>", lambda x : self.shutdown())
@@ -83,20 +66,12 @@ class App(object):
     for v in self.viewports:
       v.process()
 
-  def stop(self):
-    for v in self.viewports:
-      v.stop()
-
   def shutdown(self):
     for v in self.viewports:
       v.shutdown()
     self.viewports = []
     self.root.quit()
     self.root.update()
-
-  def save(self):
-    for i in range(len(self.viewports)):
-      self.viewports[i].save("test%d.png" % i)
 
 
 class Viewport(object):
@@ -122,10 +97,6 @@ class Viewport(object):
       self.processor.terminate()
       self.processor = None
 
-  def handleClick(self, event):
-    if self.count and not self.jobs:
-      self.stop()
-
   def getBlock(self, i, j, BLOCKS_WIDE, BLOCKS_TALL):
     cols = self.camera.cols
     rows = self.camera.rows
@@ -135,13 +106,12 @@ class Viewport(object):
              int(float(rows) / BLOCKS_TALL * (j + 1))))
 
   def initProcessor(self):
-    self.processor = ThreadedQueueProcessor(CameraHandler(self.camera), 4,
+    self.processor = ThreadedQueueProcessor(CameraHandler(self.camera), 8,
                                             use_processes=True)
     self.image = Image.new("RGBA", (self.camera.cols, self.camera.rows))
     self.draw = ImageDraw.Draw(self.image)
 
   def process(self):
-    print("Called process on this viewport.")
     try:
       jobs = []
       if self.jobs:
@@ -154,8 +124,6 @@ class Viewport(object):
         else:
           self.draw.rectangle(((0, 0), (self.camera.cols, self.camera.rows)),
                               fill="grey")
-        BLOCKS_WIDE = 20
-        BLOCKS_TALL = 20
         jobs = []
         for i in range(BLOCKS_WIDE):
           for j in range(BLOCKS_TALL):
@@ -176,10 +144,6 @@ class Viewport(object):
     w = xMax - x
     h = yMax - y
     return Image.frombytes('RGB', (w, h), pixels)
-
-  def stop(self):
-    if not self.jobs:
-      self.jobs = self.processor.clear()
 
   def refreshImage(self):
     self.imageTk = ImageTk.PhotoImage(self.image)
@@ -208,7 +172,7 @@ class Viewport(object):
 root = Tk()
 root.title("Pylot")
 
-geometry = SimpleGeometry.getGeometry(size=512, which=2)
+geometry = SimpleGeometry.getGeometry(size=WIDTH, which=2)
 world = SimpleGeometry.getWorld(geometry)
 cameras = [SimpleGeometry.getCamera(world)] #,
 viewports = [Viewport(c) for c in cameras]
