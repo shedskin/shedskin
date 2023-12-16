0cc569de01425c45f5bf4360e14b2633c0ed6428
Mark Dufour <mark.dufour@gmail.com>
2023-12-16 18:49:02 +0100

    pylot: finish conversion to pygame

--- examples/pylot/pylot_main.py
+++ examples/pylot/pylot_main.py
@@ -22,21 +22,15 @@ import time
 
 import pygame
 
-
-
-from tkinter import *
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageTk
 import pylot
 from pylot.Pool import ThreadedQueueProcessor
-from pylot.Utils import *
+#from pylot.Utils import *
 from pylot import SimpleGeometry
 print(SimpleGeometry.__file__)
 
-WIDTH = 320
-BLOCKS_WIDE = 20
-BLOCKS_TALL = 20
+WIDTH = 640
+BLOCKS_WIDE = 40
+BLOCKS_TALL = 40
 
 
 class CameraHandler(object):
@@ -45,141 +39,85 @@ class CameraHandler(object):
 
   def handle(self, job):
     realJob, debugFlag = job
-    set_debug(debugFlag)
     return self.camera.runPixelRange(realJob)
 
-QUEUE_CHECK_TIME = 250 # ms
-class App(object):
-  def __init__(self, root, viewports):
-    self.root = root
-    self.viewports = viewports
-    self.labels = []
-    for v in viewports:
-      label = Label(root)
-      self.labels.append(label)
-      label.pack(side=LEFT)
-      v.setLabel(label)
-      v.setApp(self)
-    root.bind("<Destroy>", lambda x : self.shutdown())
 
-  def process(self):
-    for v in self.viewports:
-      v.process()
-
-  def shutdown(self):
-    for v in self.viewports:
-      v.shutdown()
-    self.viewports = []
-    self.root.quit()
-    self.root.update()
-
-
-class Viewport(object):
-  def __init__(self, camera):
-    self.processor = None
-    self.camera = camera
-    self.jobs = None
-    self.image = None
-    self.label = None
-    self.app = None
-    self.count = 0
-
-  def setLabel(self, label):
-    assert not self.label
-    self.label = label
-
-  def setApp(self, app):
-    assert not self.app
-    self.app = app
-
-  def shutdown(self):
-    if self.processor:
-      self.processor.terminate()
-      self.processor = None
-
-  def getBlock(self, i, j, BLOCKS_WIDE, BLOCKS_TALL):
-    cols = self.camera.cols
-    rows = self.camera.rows
+def getBlock(i, j, BLOCKS_WIDE, BLOCKS_TALL):
+    cols = WIDTH
+    rows = WIDTH
     return ((int(float(cols) / BLOCKS_WIDE * i),
              int(float(cols) / BLOCKS_WIDE * (i + 1))),
             (int(float(rows) / BLOCKS_TALL * j),
              int(float(rows) / BLOCKS_TALL * (j + 1))))
 
-  def initProcessor(self):
-    self.processor = ThreadedQueueProcessor(CameraHandler(self.camera), 8,
-                                            use_processes=True)
-    self.image = Image.new("RGBA", (self.camera.cols, self.camera.rows))
-    self.draw = ImageDraw.Draw(self.image)
 
-  def process(self):
-    try:
-      jobs = []
-      if self.jobs:
-        jobs = self.jobs
-        self.jobs = None
-      elif not self.count:
-        self.startTime = time.time()
-        if not self.processor:
-          self.initProcessor()
-        else:
-          self.draw.rectangle(((0, 0), (self.camera.cols, self.camera.rows)),
-                              fill="grey")
-        jobs = []
-        for i in range(BLOCKS_WIDE):
-          for j in range(BLOCKS_TALL):
-            jobs.append((self.getBlock(i, j, BLOCKS_WIDE, BLOCKS_TALL), False))
-        random.shuffle(jobs)
-        self.count = BLOCKS_WIDE * BLOCKS_TALL
-      for j in jobs:
-        self.processor.put(j)
-      if jobs:
-        self.app.root.after(QUEUE_CHECK_TIME, self.checkQueue)
-    except:
-      if self.processor:
-        self.processor.terminate()
-      raise
-
-  def imageFromBlock(self, r, pixels):
+def imageFromBlock(r, pixels):
     (x, xMax), (y, yMax) = r
     w = xMax - x
     h = yMax - y
     return Image.frombytes('RGB', (w, h), pixels)
 
-  def refreshImage(self):
-    self.imageTk = ImageTk.PhotoImage(self.image)
-    self.label.config(image=self.imageTk)
 
-  def checkQueue(self):
-    gotData = False
-    if self.processor:
-      block = self.processor.get(False)
-      while block:
-        gotData = True
-        self.count -= 1
-        assert self.count >= 0
-        r, pixels = block
-        image_temp = self.imageFromBlock(r, pixels)
-        (x, _), (y, _) = r
-        self.image.paste(image_temp, (x, y))
-        block = self.processor.get(False)
-      if gotData:
-        self.refreshImage()
-        if not self.count:
-          print("That took %.3f seconds." % (time.time() - self.startTime))
-      self.app.root.after(QUEUE_CHECK_TIME, self.checkQueue)
+def main():
+    screen = (WIDTH, WIDTH)
+    pygame.init()
+    surface = pygame.display.set_mode(screen)
+    drawsurf = pygame.Surface(screen).convert()
+    drawsurf.set_colorkey((0, 0, 0))
+
+    geometry = SimpleGeometry.getGeometry(size=WIDTH, which=2)
+    world = SimpleGeometry.getWorld(geometry)
+    camera = SimpleGeometry.getCamera(world)
+
+    processor = ThreadedQueueProcessor(CameraHandler(camera), 8, use_processes=True)
+
+    jobs = []
+    for i in range(BLOCKS_WIDE):
+        for j in range(BLOCKS_TALL):
+            jobs.append((getBlock(i, j, BLOCKS_WIDE, BLOCKS_TALL), False))
+    random.shuffle(jobs)
+
+    startTime = time.time()
+
+    for j in jobs:
+        processor.put(j)
+
+    count = BLOCKS_WIDE * BLOCKS_TALL
+
+    clock = pygame.time.Clock()
+
+    ingame = True
+    while ingame:
+        # quit?
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_q]:
+            ingame = False
+        for event in pygame.event.get():
+            if event.type is pygame.QUIT:
+                ingame = False
+
+        gotData = False
+        block = processor.get(False)
+        while block:
+            gotData = True
+            count -= 1
+            r, pixels = block
+            (x, _), (y, _) = r
+
+            img = pygame.image.frombuffer(pixels, (WIDTH // BLOCKS_WIDE, WIDTH // BLOCKS_TALL), 'RGB')
+            surface.blit(img, (x, y))
+            pygame.display.flip()
+
+            if count == 0:
+                print("That took %.3f seconds." % (time.time() - startTime))
+                break
+
+            block = processor.get(False)
+
+        clock.tick(60)
+
+    processor.terminate()
 
 
-root = Tk()
-root.title("Pylot")
-
-geometry = SimpleGeometry.getGeometry(size=WIDTH, which=2)
-world = SimpleGeometry.getWorld(geometry)
-cameras = [SimpleGeometry.getCamera(world)] #,
-viewports = [Viewport(c) for c in cameras]
-app = App(root, viewports)
-
-try:
-    app.process()
-    root.mainloop()
-finally:
-    app.shutdown()
+if __name__ == '__main__':
+    main()
