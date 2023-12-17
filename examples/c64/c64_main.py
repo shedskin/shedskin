c00e912af1b00d06a45715eb4a8e55830af451ba
Mark Dufour <mark.dufour@gmail.com>
2023-12-17 13:18:34 +0100

    c64: handle keys again, looks good

--- examples/c64/c64_main.py
+++ examples/c64/c64_main.py
@@ -8,67 +8,7 @@ print(c64)
 from c64.symbols import S_A, S_X, S_Y, S_SP, S_PC
 from optparse import OptionParser
 
-
-'''
-        keyboard_matrix = self.C64.CIA1.get_keyboard_matrix()
-        self.hardware_keycodes = {} # keyval_name -> keycode
-        self.keycode_names = {}
-        self.keymap = Gdk.Keymap.get_default()
-        self.screen_count = 0
-        alternatives = { # C64_name: GDK_name
-            "grave": "numbersign", # German
-            "LeftArrow": "Escape",
-            "pound": "F9",
-            "/": "F8", # actually overwritten below :P
-            "=": "F7", # eep.
-            ";": "F6", # eep.
-        }
-        for row in keyboard_matrix:
-            for cell in row:
-                #print(cell)
-                entries = self.keymap.get_entries_for_keyval(Gdk.keyval_from_name(alternatives.get(cell) or cell) or ord(cell))
-                if entries is None:
-                    entries = self.keymap.get_entries_for_keyval(Gdk.keyval_from_name(alternatives[cell]))
-                    #print(cell)
-                assert(entries)
-                if entries[1]:
-                    hardware_keycode = entries[1][0].keycode
-                    self.hardware_keycodes[cell] = hardware_keycode
-                    self.keycode_names[hardware_keycode] = cell # for the C64, that is.
-        self.hardware_keycodes["/"] = 20 # FIXME remove this.
-        self.keycode_names[20] = "/" # FIXME remove this.
-
-    def handle_key_press(self, keycode):
-        n = self.keycode_names.get(keycode)
-        if n:
-            return self.C64.CIA1.handle_key_press(n)
-
-    def handle_key_release(self, keycode):
-        n = self.keycode_names.get(keycode)
-        if n:
-            return self.C64.CIA1.handle_key_release(n)
-
-        self.screen = c64.VIC.screen
-        native_pixbuf = self.screen.get_rendered_pixbuf()
-        if native_pixbuf != 0:
-            self.pixbuf = GObject.PyGObjectCPAI().pygobject_new(native_pixbuf)
-            self.B_create_flip_pixbuf = False
-        else:
-            self.pixbuf = None
-            self.B_create_flip_pixbuf = True
-
-    def repaint(self, drawarea, pCr):
-        self.C64.fire_timer()
-
-        s = self.screen.pixbuf_obj.get_rendered_image()
-        self.pixbuf = GdkPixbuf.Pixbuf.new_from_data(s, GdkPixbuf.Colorspace.RGB, True, 8, screens.WIDTH, screens.HEIGHT, screens.WIDTH * 4)  # TODO optimize!!
-        self.pixbuf = self.pixbuf.scale_simple(2*screens.WIDTH, 2*screens.HEIGHT, GdkPixbuf.InterpType.NEAREST)
-
-        Gdk.cairo_set_source_pixbuf(pCr, self.pixbuf, 5, 5)
-        pCr.paint()
-
-        self.drawing_area.queue_draw()
-'''
+SCALE = 2
 
 def main():
     parser = OptionParser()
@@ -87,20 +27,36 @@ def main():
     for i in range(50000): # boot a little first
         c_64.iterate()
 
-    screen = (screens.WIDTH, screens.HEIGHT)
+    WIDTH = screens.WIDTH*SCALE
+    HEIGHT = screens.HEIGHT*SCALE
+
     pygame.init()
+    screen = (WIDTH, HEIGHT)
     surface = pygame.display.set_mode(screen)
     drawsurf = pygame.Surface(screen).convert()
     drawsurf.set_colorkey((0, 0, 0))
 
-
     clock = pygame.time.Clock()
 
+    n = 0
     ingame = True
     while ingame:
         c_64.fire_timer()
         s = c64_screen.pixbuf_obj.get_rendered_image()
-        img = pygame.image.frombuffer(s, screen, 'RGBX')
+        img = pygame.image.frombuffer(s, (screens.WIDTH, screens.HEIGHT), 'RGBX')
+        img = pygame.transform.scale(img, (WIDTH, HEIGHT))
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                name = pygame.key.name(event.key).title()
+                c_64.CIA1.handle_key_press(name)
+
+            elif event.type == pygame.KEYUP:
+                name = pygame.key.name(event.key).title()
+                c_64.CIA1.handle_key_release(name)
+
+            elif event.type == pygame.QUIT:
+                ingame = False
 
         surface.blit(img, (0, 0))
         pygame.display.flip()
