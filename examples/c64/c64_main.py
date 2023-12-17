ac9039161f8930448656d5f99bec70d73a4d2f33
Mark Dufour <mark.dufour@gmail.com>
2023-12-17 12:53:08 +0100

    c64: rewrite/simplify to pygame, part 1/3

--- examples/c64/c64_main.py
+++ examples/c64/c64_main.py
@@ -1,50 +1,15 @@
 #!/usr/bin/env python2
 # I, Danny Milosavljevic, hereby place this file into the public domain.
-import gi
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, GdkPixbuf
+
+import pygame
 
 from c64 import c64, screens
 print(c64)
 from c64.symbols import S_A, S_X, S_Y, S_SP, S_PC
 from optparse import OptionParser
 
-def unpack_unsigned(value):
-    return value
 
-def to_signed_byte(value):
-    return value if value < 0x80 else -(256 - value)
-
-class StatusDialog(Gtk.Dialog):
-    def __init__(self, *args, **kwargs):
-        Gtk.Dialog.__init__(self, *args, **kwargs)
-        self.size_group = Gtk.SizeGroup(Gtk.SIZE_GROUP_HORIZONTAL)
-        self.add_button(Gtk.STOCK_CLOSE, Gtk.RESPONSE_CLOSE)
-        self.controls = {}
-        for ID in [S_A, S_X, S_Y, S_SP, S_PC]:
-            self.add_line(ID)
-
-    def add_line(self, ID):
-        box = Gtk.HBox()
-        label = Gtk.Label(chr(ID))
-        self.size_group.add_widget(label)
-        control = Gtk.Label()
-        box.pack_start(label, False, False, 0)
-        box.pack_start(control, True, True, 0)
-        self.vbox.pack_start(box, False, False, 0)
-        self.controls[ID] = control
-        return control
-
-    def set_value(self, ID, value):
-        v = unpack_unsigned(value)
-        text = "$%04X=%r=%r" % (v, v, to_signed_byte(value) if ID != S_PC else value)
-        self.controls[ID].set_text(text)
-
-class Controls(Gtk.VBox):
-    def __init__(self, c64):
-        Gtk.VBox.__init__(self)
-        self.C64 = c64
-        self.status_dialog = None
+'''
         keyboard_matrix = self.C64.CIA1.get_keyboard_matrix()
         self.hardware_keycodes = {} # keyval_name -> keycode
         self.keycode_names = {}
@@ -73,35 +38,6 @@ class Controls(Gtk.VBox):
         self.hardware_keycodes["/"] = 20 # FIXME remove this.
         self.keycode_names[20] = "/" # FIXME remove this.
 
-        status_button = Gtk.Button(label="_Status")
-        status_button.connect("clicked", self.show_status)
-        pause_button = Gtk.Button(label="_Pause")
-        pause_button.connect("clicked", self.pause_CPU)
-        read_memory_button = Gtk.Button(label="_Read Memory...")
-        read_memory_button.connect("clicked", self.dump_memory)
-        toggle_disassembly_button = Gtk.Button(label="_Toggle Disassembly")
-        toggle_disassembly_button.connect("clicked", self.toggle_disassembly)
-        self.pack_start(status_button, False, False ,0)
-        self.pack_start(pause_button, False, False ,0)
-        self.pack_start(read_memory_button, False, False ,0)
-        self.pack_start(toggle_disassembly_button, False, False ,0)
-        self.show_all()
-
-    def show_status(self, *args, **kwargs):
-        pass
-
-    def pause_CPU(self, widget, *args, **kwargs):
-        pass
-
-    def toggle_disassembly(self, *args, **kwargs):
-        pass
-
-    def dump_memory(self, *args, **kwargs):
-        pass
-
-    def update_status(self):
-        pass
-
     def handle_key_press(self, keycode):
         n = self.keycode_names.get(keycode)
         if n:
@@ -112,31 +48,6 @@ class Controls(Gtk.VBox):
         if n:
             return self.C64.CIA1.handle_key_release(n)
 
-# TODO 3 bit row counter.
-
-class EventBox(Gtk.EventBox):
-    def __init__(self, controls):
-        Gtk.EventBox.__init__(self)
-        self.controls = controls
-        self.props.can_focus = True
-        #self.pressed_keys = set()
-        #self.keymap = Gdk.keymap_get_default()
-        self.connect("key-press-event", self.handle_key_press)
-        self.connect("key-release-event", self.handle_key_release)
-        self.connect("button-press-event", self.handle_button_press)
-    def handle_button_press(self, widget, event):
-        self.grab_focus()
-        return False
-    def handle_key_press(self, widget, event):
-        return self.controls.handle_key_press(event.hardware_keycode)
-    def handle_key_release(self, widget, event):
-        # hardware_keycode
-        return self.controls.handle_key_release(event.hardware_keycode)
-        #self.pressed_keys.discard(event.keycode)
-
-class View(object): # graphical part.
-    def __init__(self, c64, controls):
-        self.C64 = c64
         self.screen = c64.VIC.screen
         native_pixbuf = self.screen.get_rendered_pixbuf()
         if native_pixbuf != 0:
@@ -145,22 +56,6 @@ class View(object): # graphical part.
         else:
             self.pixbuf = None
             self.B_create_flip_pixbuf = True
-        #self.pixbuf = Gdk.Pixbuf(Gdk.COLORSPACE_RGB, True, 8, screens.WIDTH, screens.HEIGHT)
-        #self.pixbuf.fill(0x000000FF)
-        self.window = Gtk.Window()
-        self.event_box = EventBox(controls)
-        self.drawing_area = Gtk.DrawingArea()
-        self.drawing_area.connect("draw", self.repaint)
-        self.drawing_area.set_size_request(screens.WIDTH*2, screens.HEIGHT*2) # FIXME make configurable.
-        self.drawing_area.show()
-        self.event_box.show()
-        box = Gtk.HBox()
-        self.event_box.add(self.drawing_area)
-        box.pack_start(self.event_box, False, False, 0)
-        box.pack_start(controls, False, False, 0)
-        box.show()
-        self.window.add(box)
-        self.window.show_all()
 
     def repaint(self, drawarea, pCr):
         self.C64.fire_timer()
@@ -173,26 +68,45 @@ class View(object): # graphical part.
         pCr.paint()
 
         self.drawing_area.queue_draw()
+'''
 
 def main():
     parser = OptionParser()
     parser.add_option("-t", "--tape", dest="tape",help="load from T64 tape image", metavar="TAPE")
     parser.add_option("-p", "--prg", dest="prg", help="load from PRG file", metavar="PRG")
     (options, args) = parser.parse_args()
+
     c_64 = c64.C64()
+    c64_screen = c_64.VIC.screen
+
     if options.tape:
         c_64.set_tape_image_name(options.tape.encode(), b"T64")
-        #c_64.set_tape_loader(loaders.t64.Loader.parse(open(options.tape, "rb"), options.tape))
     elif options.prg:
         c_64.set_tape_image_name(options.prg.encode(), b"PRG")
-        #c_64.set_tape_loader(loaders.t64.Loader.parse(open(options.prg, "rb"), options.prg))
+
     for i in range(50000): # boot a little first
         c_64.iterate()
-    controls = Controls(c_64)
-    assert(isinstance(controls, Gtk.VBox))
-    graphics_view = View(c_64, controls)
-    controls.graphics_view = graphics_view
-    Gtk.main()
+
+    screen = (screens.WIDTH, screens.HEIGHT)
+    pygame.init()
+    surface = pygame.display.set_mode(screen)
+    drawsurf = pygame.Surface(screen).convert()
+    drawsurf.set_colorkey((0, 0, 0))
+
+
+    clock = pygame.time.Clock()
+
+    ingame = True
+    while ingame:
+        c_64.fire_timer()
+        s = c64_screen.pixbuf_obj.get_rendered_image()
+        img = pygame.image.frombuffer(s, screen, 'RGBX')
+
+        surface.blit(img, (0, 0))
+        pygame.display.flip()
+
+        clock.tick(60)
+
 
 if __name__ == '__main__':
     main()
