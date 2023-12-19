85cb79531cd2bb60bb796b9f235440676552cb1a
Mark Dufour <mark.dufour@gmail.com>
2023-12-19 09:57:40 +0100

    removed too much unboxing

--- shedskin/cpp.py
+++ shedskin/cpp.py
@@ -2663,6 +2663,21 @@ class GenerateVisitor(ast_utils.BaseNodeVisitor):
                 cast = True
                 self.append("((void *)(")
 
+            if (
+                print_function
+                and not formal.name.startswith("__kw_")
+                and not formal.name == 'offset'
+            ):
+                types = [t[0].ident for t in self.mergeinh[arg]]
+                if (
+                    "float_" in types
+                    or "int_" in types
+                    or "bool_" in types
+                    or "complex" in types
+                ):
+                    cast = True
+                    self.append("___box((")
+
             if arg in target.mv.defaults:
                 if self.mergeinh[arg] == set([(python.def_class(self.gx, "bool_"), 0)]):
                     self.append(str(arg.value))
