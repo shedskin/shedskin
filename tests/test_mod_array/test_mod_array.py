fe7a91810ba9fe661225db10c20c3b864121067b
Mark Dufour <mark.dufour@gmail.com>
2024-01-22 13:19:54 +0100

    start filling in test_mod_array

--- tests/test_mod_array/test_mod_array.py
+++ tests/test_mod_array/test_mod_array.py
@@ -28,9 +28,9 @@ def test_file():
 
 
 def test_list():
-    pass
-    # tolist
-    # fromlist
+    arr = array.array('i', [1, 2])
+    arr.fromlist([3, 4, 5])
+    assert arr.tolist() == [1, 2, 3, 4, 5]
 
 
 def test_bytes():
@@ -40,24 +40,26 @@ def test_bytes():
 
 
 def test_sequence_immutable():
-    pass
-    # __getitem__
-    # count
-    # index
-    # __contains__
-    # __iter__
-    # __slice__
-    # __mul__, __add__
-    # __len__
+    arr = array.array('i', range(5, 15))
+    assert arr[3] == 8
+    assert arr.index(8) == 3
+    assert arr.count(6) == 1
+    assert 14 in arr
+    assert 15 not in arr
+    assert len(arr) == 10
+    assert arr + arr == array.array('i', 2 * list(range(5, 15)))
+    assert arr * 2 == 2 * arr
+    assert list(3 * arr) == 3 * list(range(5, 15))
 
-#    arr = array.array("B")
-#    arr.extend(list(range(20)))
-#    assert arr == array.array('B', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
-#    assert arr[:] == array.array('B', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
-#    assert arr[-7:] == array.array('B', [13, 14, 15, 16, 17, 18, 19])
-#    assert arr[-7::2] == array.array('B', [13, 15, 17, 19])
-#    assert arr[:8:3] == array.array('B', [0, 3, 6])
-#    assert arr[15:1:-2] == array.array('B', [15, 13, 11, 9, 7, 5, 3])
+    # slicing
+    arr = array.array("B")
+    arr.extend(list(range(20)))
+    assert arr == array.array('B', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+    assert arr[:] == array.array('B', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19])
+    assert arr[-7:] == array.array('B', [13, 14, 15, 16, 17, 18, 19])
+    assert arr[-7::2] == array.array('B', [13, 15, 17, 19])
+    assert arr[:8:3] == array.array('B', [0, 3, 6])
+    assert arr[15:1:-2] == array.array('B', [15, 13, 11, 9, 7, 5, 3])
 
 
 def test_sequence_mutable():
