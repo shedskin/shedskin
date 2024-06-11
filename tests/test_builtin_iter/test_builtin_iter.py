e7b53a01a328a4c540b28a811d98ed3b9555d1ec
Mark Dufour <mark.dufour@gmail.com>
2024-06-11 23:09:25 +0200

    test_builtin_iter: do not depend on dict iteration order

--- tests/test_builtin_iter/test_builtin_iter.py
+++ tests/test_builtin_iter/test_builtin_iter.py
@@ -30,12 +30,13 @@ def iter_(x):
     return x.__iter__()
 
 
-
 b = [1, 2, 3]
+
 
 def ranged_iter(n):
     for i in range(n):
         yield i
+
 
 def test_iter1():
     list_it = iter(b)
@@ -43,10 +44,11 @@ def test_iter1():
     assert next(list_it) == 2
     assert next(list_it) == 3
 
+
 def test_iter2():
     assert list(iter(b)) == [1, 2, 3]
     assert [y for y in "stroop"] == ['s', 't', 'r', 'o', 'o', 'p']
-    assert [n for n in {1: "1", 2: "2", 3: "3"}] == [1,2,3]
+    assert sorted([n for n in {1: "1", 2: "2", 3: "3"}]) == [1,2,3]
     assert [z for z in [[1], [2], [3]]] == [[1], [2], [3]]
     assert sorted([m for m in set([1.0, 2.0, 3.0])]) == [1.0, 2.0, 3.0]
 
@@ -60,6 +62,7 @@ def test_file_iter():
         testdata = "../../testdata"
     datafile = os.path.join(testdata, 'hoppa')
     assert [l for l in open(datafile)] == ['hop\n', 'hop\n', 'hoppa!\n']
+
 
 def stop_iter(n, mode):
     if mode == 1:
@@ -75,11 +78,13 @@ def stop_iter(n, mode):
         return 'stopped'
     return 'not-stopped'
 
+
 def test_stop_iter():
     assert stop_iter(3, mode=1) == 'stopped'
     assert stop_iter(3, mode=2) == 'stopped'
     assert stop_iter(4, mode=1) == 'not-stopped'
     assert stop_iter(4, mode=2) == 'not-stopped'
+
 
 def test_class_iter():
     i = iter_(Container())
@@ -90,11 +95,13 @@ def test_class_iter():
         pass
     assert y == 2
 
+
 def test_all():
     test_iter1()
     test_iter2()
     test_file_iter()
     test_stop_iter()
+
 
 if __name__ == '__main__':
     test_all()
