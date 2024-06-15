2fe058d15c81eeb3ba514b0e67dca9bc73ccd178
Mark Dufour <mark.dufour@gmail.com>
2024-06-15 19:56:02 +0200

    use std::hash for __ss_float

--- shedskin/lib/builtin/hash.hpp
+++ shedskin/lib/builtin/hash.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2005-2011 Mark Dufour and contributors; License Expat (See LICENSE) */
+/* Copyright 2005-2024 Mark Dufour and contributors; License Expat (See LICENSE) */
 
 /* hashing */
 
@@ -16,18 +16,7 @@ template<> inline long hasher(__ss_int a) { return (a==-1)?-2:a; }
 template<> inline long hasher(int a) { return (a==-1)?-2:a; }
 template<> inline long hasher(__ss_bool a) { return a.value; }
 template<> inline long hasher(void *a) { return (intptr_t)a; }
-template<> inline long hasher(__ss_float v) {
-    long hipart, x; /* modified from CPython */
-    int expo;
-    v = frexp(v, &expo);
-    v *= 2147483648.0; /* 2**31 */
-    hipart = (long)v;   /* take the top 32 bits */
-    v = (v - (__ss_float)hipart) * 2147483648.0; /* get the next 32 bits */
-    x = hipart + (long)v + (expo << 15);
-    if (x== -1)
-        x = -2;
-    return x;
-}
+template<> inline long hasher(__ss_float v) { return std::hash<__ss_float>{}(v); }
 
 template<class T> class ss_hash {
     public:
