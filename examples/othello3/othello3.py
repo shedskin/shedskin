516fde254e213ee8bc6b38e19238be699672ea1a
Mark Dufour <m.dufour@knmp.nl>
2024-11-07 19:17:41 +0100

    othello3 remove unused 'product' func

--- examples/othello3/othello3.py
+++ examples/othello3/othello3.py
@@ -1,22 +1,6 @@
 import collections
 import time
 
-def product(iterables):  # shedskin: avoid itertools.product splat operator
-    result = [[]]
-    for pool in iterables:
-        result = [x+[y] for x in result for y in pool]
-    return result
-
-def str_base(number, base):  # base-3 helper
-    digits = []
-    while number > 0:
-        number, digit = divmod(number, base)
-        digits.append(str(digit))
-    return ''.join(digits)
-
-def str_state(s):
-    return str_base(s, 3).ljust(8, '0')
-
 BLACK, WHITE = 1, -1
 
 class Line:
@@ -128,6 +112,16 @@ state_43 = 3280
 state_44 = 3280
 state_45 = 3280
 
+def str_base(number, base):  # base-3 helper
+    digits = []
+    while number > 0:
+        number, digit = divmod(number, base)
+        digits.append(str(digit))
+    return ''.join(digits)
+
+def str_state(s):
+    return str_base(s, 3).ljust(8, '0')
+
 def states():
     return [
         state_0, state_1, state_2, state_3, state_4, state_5, state_6, state_7, state_8, state_9, state_10, state_11, state_12, state_13, state_14, state_15, state_16, state_17, state_18, state_19, state_20, state_21, state_22, state_23, state_24, state_25, state_26, state_27, state_28, state_29, state_30, state_31, state_32, state_33, state_34, state_35, state_36, state_37, state_38, state_39, state_40, state_41, state_42, state_43, state_44, state_45
@@ -142,6 +136,7 @@ for l, line in enumerate(lines):
         pos = (pos[0]+line.dx, pos[1]+line.dy)
 
 
+# textual board representation
 def get_board(line_from, line_to):
     board = [['.' for i in range(8)] for j in range(8)]
     for i in range(8):
@@ -157,6 +152,7 @@ def calc_pos(l, j):
     return (line.start[0]+j*line.dx, line.start[1]+j*line.dy)
 
 
+# for each line state, idx and turn, determine flipped discs
 def state_flips(s, idx, turn):
     flips = []
     s2 = str_state(s)
@@ -177,8 +173,6 @@ def state_flips(s, idx, turn):
                     flips2.append(j)
     return flips
 
-
-# for each line state, idx and turn, determine flipped discs
 flippers_x = {}
 flippers_o = {}
 for s in range(3**8):
@@ -187,6 +181,7 @@ for s in range(3**8):
         flippers_o[s, idx] = state_flips(s, idx, '0')
 
 
+# check that all line states match
 def check_board():
     a = get_board(0, 8)
     print(a)
