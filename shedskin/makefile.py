38b77d940dcdc08a1a5fd91d9b67c3299148fa5a
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-11-15 08:29:27 +0300

    more makefile refactoring

--- shedskin/makefile.py
+++ shedskin/makefile.py
@@ -32,9 +32,9 @@ if TYPE_CHECKING:
 
 PLATFORM = platform.system()
 
+
 def env_var(name: str) -> str:
     return "$(%s)" % name
-
 
 def check_output(cmd: str) -> Optional[str]:
     """Run a command and return its output, or None if command not found"""
@@ -248,6 +248,21 @@ class MakefileGenerator:
         """Reverse sorted list of .hpp files"""
         return sorted([fn + ".hpp" for fn in self.filenames], reverse=True)
 
+    @property
+    def target_name(self) -> str:
+        """Get the target executable/library name"""
+        if self.gx.pyextension_product: 
+            return f"{self.gx.main_module.ident}{self.py.extension_suffix}"
+        else:
+            return self.gx.main_module.ident
+
+    @property
+    def makefile_path(self) -> str:
+        """Get the Makefile output path"""
+        if self.gx.outputdir:
+            return os.path.join(self.gx.outputdir, self.gx.makefile_name)
+        return self.gx.makefile_name
+
     def write(self, text: Optional[str] = None) -> None:
         """Write a line to the Makefile"""
         if not text:
@@ -280,6 +295,23 @@ class MakefileGenerator:
         else:
             return check_output("brew --prefix")
 
+    def generate(self) -> None:
+        """Generate the Makefile"""
+        if self.gx.nomakefile:
+            return
+            
+        self._setup_platform()
+        self._add_user_dirs()
+        
+        self.writer = MakefileWriter(self.makefile_path)
+        
+        self._write_variables()
+        self._write_targets()
+        self._write_clean()
+        self._write_phony()
+        
+        self.writer.close()
+
     def _setup_platform(self) -> None:
         """Configure platform-specific settings"""
         if PLATFORM == "Windows":
@@ -292,25 +324,24 @@ class MakefileGenerator:
         
     def _setup_unix(self) -> None:
         """Configure Unix-like platform settings"""
-        self.add_include_dirs(
-            self.py.include_dir,
-            self.py.config_h_dir,
-        )
+        if self.py.include_dir != self.py.config_h_dir:
+            self.add_include_dirs(
+                "$(PY_INCLUDE)",
+                self.py.config_h_dir,
+            )
+        else:
+            self.add_include_dirs("$(PY_INCLUDE)")
         if PLATFORM == "Darwin":
             self.add_ldflags(self.py.base_cflags)
         else:
-            self._setup_unix_ldflags()
-            
-    def _setup_unix_ldflags(self) -> None:
-        """Configure Unix-like linker flags"""
-        self.add_ldflags(
-            self.py.libs,
-            self.py.syslibs,
-            self.py.linklib,
-        )
-        if not self.py.is_shared:
-            self.add_link_dirs(self.py.libpl)
-           
+            self.add_ldflags(
+                self.py.libs,
+                self.py.syslibs,
+                self.py.linklib,
+            )
+            if not self.py.is_shared:
+                self.add_link_dirs(self.py.libpl)
+    
     def _add_user_dirs(self) -> None:
         """Add user-specified include and link directories"""
         if self.gx.options.include_dirs:
@@ -319,140 +350,188 @@ class MakefileGenerator:
         if self.gx.options.link_dirs:
             for link_dir in self.gx.options.link_dirs:
                 self.add_link_dirs(link_dir)
-
-    def _get_target_name(self) -> str:
-        """Get the target executable/library name"""
-        return f"{self.gx.main_module.ident}{self.py.extension_suffix}"
-        
-    def _get_makefile_path(self) -> str:
-        """Get the Makefile output path"""
-        if self.gx.outputdir:
-            return os.path.join(self.gx.outputdir, self.gx.makefile_name)
-        return self.gx.makefile_name
     
     def _write_variables(self) -> None:
         """Write variables to the Makefile"""
         self.write("SHEDSKIN_LIBDIR=%s" % self.shedskin_libdirs[-1])
+        self.write("PY_INCLUDE=%s" % self.py.include_dir)
+        if PLATFORM == "Darwin":
+            if self.homebrew_prefix():
+                self.write("HOMEBREW_PREFIX=%s" % self.homebrew_prefix())
+                self.write("HOMEBREW_INCLUDE=%s/include" % self.homebrew_prefix())
+                self.write("HOMEBREW_LIB=%s/lib" % self.homebrew_prefix())
+        self._write_flags()
+        self._write_cpp_files()
+        
+    def _write_cpp_files(self) -> None:
+        """Write C++ source and header file lists"""
         cppfiles_str = " \\\n\t".join(self.cppfiles)
         hppfiles_str = " \\\n\t".join(self.hppfiles)
-
-        # import flags
-        if self.gx.flags:
-            flags = self.gx.flags
-        elif os.path.isfile("FLAGS"):
-            flags = pathlib.Path("FLAGS")
-        elif os.path.isfile("/etc/shedskin/FLAGS"):
-            flags = pathlib.Path("/etc/shedskin/FLAGS")
-        elif PLATFORM == "Windows":
-            flags = self.gx.shedskin_flags / "FLAGS.mingw"
-        elif PLATFORM == "Darwin":
-            BREW_PREFIX = self.homebrew_prefix()
-            if self.homebrew_prefix():
-                self.add_include_dirs(f"{BREW_PREFIX}/include")
-                self.add_link_dirs(f"{BREW_PREFIX}/lib")
-            flags = self.gx.shedskin_flags / "FLAGS.osx"
-        else:
-            flags = self.gx.shedskin_flags / "FLAGS"
-
-        line = ""
+        self.write("CPPFILES=%s\n" % cppfiles_str)
+        self.write("HPPFILES=%s\n" % hppfiles_str)
+        
+    def _write_flags(self) -> None:
+        """Write compiler and linker flags"""
+        flags = self._get_flags_file()
         includes = " ".join(self.includes)
         ldflags = " ".join(self.ldflags)
+        
         for line in open(flags):
             line = line[:-1]
-
             variable = line[: line.find("=")].strip().rstrip("?")
-
+            
             if variable == "CXXFLAGS":
-                line += " -I. -I%s" % env_var("SHEDSKIN_LIBDIR")
-                line += "".join(" -I" + libdir for libdir in self.shedskin_libdirs[:-1])
-                line += " " + includes
-                if PLATFORM == "Darwin" and os.path.isdir("/usr/local/include"):
-                    line += " -I/usr/local/include"  # XXX
-                if PLATFORM == "Darwin" and os.path.isdir("/opt/local/include"):
-                    line += " -I/opt/local/include"  # XXX
-                if not self.gx.wrap_around_check:
-                    line += " -D__SS_NOWRAP"
-                if not self.gx.bounds_checking:
-                    line += " -D__SS_NOBOUNDS"
-                if not self.gx.assertions:
-                    line += " -D__SS_NOASSERT"
-                if self.gx.int32:
-                    line += " -D__SS_INT32"
-                if self.gx.int64:
-                    line += " -D__SS_INT64"
-                if self.gx.int128:
-                    line += " -D__SS_INT128"
-                if self.gx.float32:
-                    line += " -D__SS_FLOAT32"
-                if self.gx.float64:
-                    line += " -D__SS_FLOAT64"
-                if self.gx.backtrace:
-                    line += " -D__SS_BACKTRACE -rdynamic -fno-inline"
-                if self.gx.nogc:
-                    line += " -D__SS_NOGC"
-                if self.gx.pyextension_product:
-                    if PLATFORM == "Windows":
-                        line += " -I%s\\include -D__SS_BIND" % self.py.prefix
-                    else:
-                        line += " -g -fPIC -D__SS_BIND " + includes
-
+                line = self._update_cxx_flags(line, includes)
             elif variable == "LFLAGS":
-                line += ldflags
-                if PLATFORM == "Darwin" and os.path.isdir("/opt/local/lib"):  # XXX
-                    line += " -L/opt/local/lib"
-                if PLATFORM == "Darwin" and os.path.isdir("/usr/local/lib"):  # XXX
-                    line += " -L/usr/local/lib"
-                if self.gx.pyextension_product:
-                    if PLATFORM == "Windows":
-                        line += " -shared -L%s\\libs -lpython%s" % (self.py.prefix, self.py.ver)
-                    elif PLATFORM == "Darwin":
-                        line += " -bundle -undefined dynamic_lookup " + ldflags
-                    elif PLATFORM == "SunOS":
-                        line += " -shared -Xlinker " + ldflags
-                    else:
-                        line += " -Wno-register -shared -Xlinker -export-dynamic " + ldflags
-
-                if "re" in [m.ident for m in self.modules]:
-                    line += " -lpcre"
-                if "socket" in (m.ident for m in self.modules):
-                    if PLATFORM == "Windows":
-                        line += " -lws2_32"
-                    elif PLATFORM == "SunOS":
-                        line += " -lsocket -lnsl"
-                if "os" in (m.ident for m in self.modules):
-                    if PLATFORM not in ["Windows", "Darwin", "SunOS"]:
-                        line += " -lutil"
-                if "hashlib" in (m.ident for m in self.modules):
-                    line += " -lcrypto"
-
+                line = self._update_linker_flags(line, ldflags)
+                
             self.write(line)
             self.write()
-            self.write("CPPFILES=%s\n" % cppfiles_str)
-            self.write("HPPFILES=%s\n" % hppfiles_str)
-
-            # tests for static
-            MATCH = re.match(r"^LFLAGS=(.+)(\$\(LDFLAGS\).+)", line)
-            if PLATFORM == "Darwin" and self.homebrew_prefix() and MATCH:
-                self.is_static = True
-                self.write("STATIC_PREFIX=$(shell brew --prefix)")
-                self.write("STATIC_LIBDIR=$(STATIC_PREFIX)/lib")
-                self.write("STATIC_INCLUDE=$(STATIC_PREFIX)/include")
-                self.write()
-                self.write("GC_STATIC=$(STATIC_LIBDIR)/libgc.a")
-                self.write("GCCPP_STATIC=$(STATIC_LIBDIR)/libgccpp.a")
-                self.write("GC_INCLUDE=$(STATIC_INCLUDE)/include")
-                self.write("PCRE_STATIC=$(STATIC_LIBDIR)/libpcre.a")
-                self.write("PCRE_INCLUDE=$(STATIC_INCLUDE)/include")
-                self.write()
-                self.write("STATIC_LIBS=$(GC_STATIC) $(GCCPP_STATIC) $(PCRE_STATIC)")
-                self.write("STATIC_CXXFLAGS=$(CXXFLAGS) -I$(GC_INCLUDE) -I$(PCRE_INCLUDE)")
-                self.write("STATIC_LFLAGS=" + MATCH.group(2))
-                self.write()
+            
+            self._handle_static_flags(line)
+            
+    def _get_flags_file(self) -> pathlib.Path:
+        """Get the appropriate flags file for the current platform"""
+        if self.gx.flags:
+            return self.gx.flags
+        elif os.path.isfile("FLAGS"):
+            return pathlib.Path("FLAGS")
+        elif os.path.isfile("/etc/shedskin/FLAGS"):
+            return pathlib.Path("/etc/shedskin/FLAGS")
+        elif PLATFORM == "Windows":
+            return self.gx.shedskin_flags / "FLAGS.mingw"
+        elif PLATFORM == "Darwin":
+            self._setup_homebrew()
+            return self.gx.shedskin_flags / "FLAGS.osx"
+        return self.gx.shedskin_flags / "FLAGS"
+        
+    def _setup_homebrew(self) -> None:
+        """Configure Homebrew paths if available"""
+        if self.homebrew_prefix():
+            self.add_include_dirs("$(HOMEBREW_INCLUDE)")
+            self.add_link_dirs("$(HOMEBREW_LIB)")
+            
+    def _update_cxx_flags(self, line: str, includes: str) -> str:
+        """Update C++ compiler flags"""
+        line += " -I. -I%s" % env_var("SHEDSKIN_LIBDIR")
+        line += "".join(" -I" + libdir for libdir in self.shedskin_libdirs[:-1])
+        line += " " + includes
+        
+        if PLATFORM == "Darwin":
+            if os.path.isdir("/usr/local/include"):
+                line += " -I/usr/local/include"
+            if os.path.isdir("/opt/local/include"):
+                line += " -I/opt/local/include"
+                
+        line = self._add_feature_flags(line)
+        line = self._add_extension_flags(line, includes)
+        return line
+        
+    def _add_feature_flags(self, line: str) -> str:
+        """Add feature-specific compiler flags"""
+        if not self.gx.wrap_around_check:
+            line += " -D__SS_NOWRAP"
+        if not self.gx.bounds_checking:
+            line += " -D__SS_NOBOUNDS"
+        if not self.gx.assertions:
+            line += " -D__SS_NOASSERT"
+        if self.gx.int32:
+            line += " -D__SS_INT32"
+        if self.gx.int64:
+            line += " -D__SS_INT64"
+        if self.gx.int128:
+            line += " -D__SS_INT128"
+        if self.gx.float32:
+            line += " -D__SS_FLOAT32"
+        if self.gx.float64:
+            line += " -D__SS_FLOAT64"
+        if self.gx.backtrace:
+            line += " -D__SS_BACKTRACE -rdynamic -fno-inline"
+        if self.gx.nogc:
+            line += " -D__SS_NOGC"
+        return line
+        
+    def _add_extension_flags(self, line: str, includes: str) -> str:
+        """Add Python extension-specific flags"""
+        if self.gx.pyextension_product:
+            if PLATFORM == "Windows":
+                line += " -I%s\\include -D__SS_BIND" % self.py.prefix
+            else:
+                line += " -g -fPIC -D__SS_BIND " + includes
+        return line
+        
+    def _update_linker_flags(self, line: str, ldflags: str) -> str:
+        """Update linker flags"""
+        line += ldflags
+        
+        if PLATFORM == "Darwin":
+            if os.path.isdir("/opt/local/lib"):
+                line += " -L/opt/local/lib"
+            if os.path.isdir("/usr/local/lib"):
+                line += " -L/usr/local/lib"
+                
+        line = self._add_extension_linker_flags(line, ldflags)
+        line = self._add_module_linker_flags(line)
+        return line
+        
+    def _add_extension_linker_flags(self, line: str, ldflags: str) -> str:
+        """Add Python extension-specific linker flags"""
+        if self.gx.pyextension_product:
+            if PLATFORM == "Windows":
+                line += " -shared -L%s\\libs -lpython%s" % (self.py.prefix, self.py.ver)
+            elif PLATFORM == "Darwin":
+                line += " -bundle -undefined dynamic_lookup " + ldflags
+            elif PLATFORM == "SunOS":
+                line += " -shared -Xlinker " + ldflags
+            else:
+                line += " -Wno-register -shared -Xlinker -export-dynamic " + ldflags
+        return line
+        
+    def _add_module_linker_flags(self, line: str) -> str:
+        """Add module-specific linker flags"""
+        module_ids = [m.ident for m in self.modules]
+        
+        if "re" in module_ids:
+            line += " -lpcre"
+        if "socket" in module_ids:
+            if PLATFORM == "Windows":
+                line += " -lws2_32"
+            elif PLATFORM == "SunOS":
+                line += " -lsocket -lnsl"
+        if "os" in module_ids:
+            if PLATFORM not in ["Windows", "Darwin", "SunOS"]:
+                line += " -lutil"
+        if "hashlib" in module_ids:
+            line += " -lcrypto"
+        return line
+        
+    def _handle_static_flags(self, line: str) -> None:
+        """Handle static linking configuration"""
+        MATCH = re.match(r"^LFLAGS=(.+)(\$\(LDFLAGS\).+)", line)
+        if PLATFORM == "Darwin" and self.homebrew_prefix() and MATCH:
+            self.is_static = True
+            self._write_static_vars(MATCH.group(2))
+            
+    def _write_static_vars(self, ldflags: str) -> None:
+        """Write static linking variables"""
+        self.write("STATIC_PREFIX=$(shell brew --prefix)")
+        self.write("STATIC_LIBDIR=$(STATIC_PREFIX)/lib")
+        self.write("STATIC_INCLUDE=$(STATIC_PREFIX)/include")
+        self.write()
+        self.write("GC_STATIC=$(STATIC_LIBDIR)/libgc.a")
+        self.write("GCCPP_STATIC=$(STATIC_LIBDIR)/libgccpp.a")
+        self.write("GC_INCLUDE=$(STATIC_INCLUDE)/include")
+        self.write("PCRE_STATIC=$(STATIC_LIBDIR)/libpcre.a")
+        self.write("PCRE_INCLUDE=$(STATIC_INCLUDE)/include")
+        self.write()
+        self.write("STATIC_LIBS=$(GC_STATIC) $(GCCPP_STATIC) $(PCRE_STATIC)")
+        self.write("STATIC_CXXFLAGS=$(CXXFLAGS) -I$(GC_INCLUDE) -I$(PCRE_INCLUDE)")
+        self.write("STATIC_LFLAGS=" + ldflags)
+        self.write()
 
     def _write_targets(self) -> None:
         """Write targets to the Makefile"""
-        ident = self._get_target_name()
-        self.write("all:\t" + ident + "\n")
+        self.write("all:\t" + self.target_name + "\n")
 
         # executable (normal, debug, profile) or extension module
         _out = "-o "
@@ -462,13 +541,13 @@ class MakefileGenerator:
             targets += [("_prof", "-pg -ggdb"), ("_debug", "-g -ggdb")]
 
         for suffix, options in targets:
-            self.write(ident + suffix + ":\t$(CPPFILES) $(HPPFILES)")
+            self.write(self.target_name + suffix + ":\t$(CPPFILES) $(HPPFILES)")
             self.write(
                 "\t$(CXX) "
                 + options
                 + " $(CXXFLAGS) $(CPPFILES) $(LFLAGS) "
                 + _out
-                + ident
+                + self.target_name
                 + suffix
                 + _ext
                 + "\n"
@@ -479,19 +558,18 @@ class MakefileGenerator:
             # static option
             self.write("static: $(CPPFILES) $(HPPFILES)")
             self.write(
-                f"\t$(CXX) $(STATIC_CXXFLAGS) $(CPPFILES) $(STATIC_LIBS) $(STATIC_LFLAGS) -o {ident}\n"
+                f"\t$(CXX) $(STATIC_CXXFLAGS) $(CPPFILES) $(STATIC_LIBS) $(STATIC_LFLAGS) -o {self.target_name}\n"
             )
 
     def _write_clean(self) -> None:
         """Write clean target to the Makefile"""
-        ident = self._get_target_name()
         ext = ""
         if PLATFORM == "Windows" and not self.gx.pyextension_product:
             ext = ".exe"
         self.write("clean:")
-        _targets = [ident + ext]
+        _targets = [self.target_name + ext]
         if not self.gx.pyextension_product:
-            _targets += [ident + "_prof" + ext, ident + "_debug" + ext]
+            _targets += [self.target_name + "_prof" + ext, self.target_name + "_debug" + ext]
         self.write("\trm -f %s" % " ".join(_targets))
         if PLATFORM == "Darwin":
             self.write("\trm -rf %s.dSYM\n" % " ".join(_targets))
@@ -506,23 +584,7 @@ class MakefileGenerator:
         phony += "\n"
         self.write(phony)
 
-    def generate(self) -> None:
-        """Generate the Makefile"""
-        if self.gx.nomakefile:
-            return
-            
-        self._setup_platform()
-        self._add_user_dirs()
-        
-        makefile_path = self._get_makefile_path()
-        self.writer = MakefileWriter(makefile_path)
-        
-        self._write_variables()
-        self._write_targets()
-        self._write_clean()
-        self._write_phony()
-        
-        self.writer.close()
+
 
 def generate_makefile(gx: "config.GlobalInfo") -> None:
     """Generate a makefile for the Shedskin-compiled code"""
