08ef676a6aabc1628670f3c6a4eebf3daedd1ddf
Mark Dufour <m.dufour@knmp.nl>
2025-11-06 18:41:20 +0100

    softrender: remove a few lines
    cloc < 500 would be nice

--- examples/softrender/main.py
+++ examples/softrender/main.py
@@ -42,7 +42,7 @@ def main():
     texture = load_texture("buddha2.jpg")
     transform = Transform(Vector4(0.0, 0.3, 3.0))
     transform = transform.rotate(quaternion_from_axis_angle(Vector4(1, 0, 0), 80))
-    lightDir = Vector4(0, 1, -1)
+    lightDir = Vector4(0, 0, -1)
 
 #    mesh = Mesh("monkey0.obj")
 #    texture = None
@@ -54,17 +54,15 @@ def main():
 #    transform = Transform(Vector4(0.0, 0, 3.0))
 #    lightDir = Vector4(1, 0, 0)
 
-    camera = Camera(Matrix4().init_perspective(math.radians(70.0), float(WIDTH)/HEIGHT, 0.1, 1000.0))
+    camera = Camera(Matrix4().init_perspective(math.radians(70.0), WIDTH/HEIGHT, 0.1, 1000.0))
 
     target = RenderContext(WIDTH, HEIGHT)
 
     while True:
         t0 = time.time()
 
-        # draw
         target.clear()
         target.clear_zbuffer()
-
         mesh.draw(target, camera.get_view_projection(), transform.get_transformation(), texture, lightDir)
 
         img = pygame.image.frombuffer(target.bitmap.components, screen, 'RGBA')
