4892337db4b9b799333749f7873ed7eb34798bbf
Mark Dufour <m.dufour@knmp.nl>
2025-11-06 15:29:26 +0100

    softrender: prepare single-entry extmod

---
+++ examples/softrender/render.py
@@ -0,0 +1,12 @@
+from bitmap import Bitmap
+from camera import Camera
+from matrix4 import Matrix4
+from mesh import Mesh
+from quaternion import Quaternion
+from rendercontext import RenderContext
+from transform import Transform
+from vector4 import Vector4
+
+
+if __name__ == '__main__':
+    pass
