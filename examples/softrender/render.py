8f53c127c15a855dc12a7e7595c9bf9c74e1123e
Mark Dufour <m.dufour@knmp.nl>
2025-11-06 15:36:40 +0100

    softrender: extmod model types.. unfortunately a lot in this case

--- examples/softrender/render.py
+++ examples/softrender/render.py
@@ -9,4 +9,13 @@ from vector4 import Vector4
 
 
 if __name__ == '__main__':
-    pass
+    mesh = Mesh("buddha2.obj")
+    texture = Bitmap("buddha2.jpg")
+    transform = Transform(Vector4(0.0, 0.3, 3.0))
+    transform = transform.rotate(Quaternion.from_axis_angle(Vector4(1, 0, 0), 80))
+    target = RenderContext(0, 0)
+    target.clear(0)
+    target.clear_zbuffer()
+    camera = Camera(Matrix4().init_perspective(1.0, 1.0, 0.1, 1000.0))
+    lightDir = Vector4(0, 0, -1)
+    mesh.draw(target, camera.get_view_projection(), transform.get_transformation(), texture, lightDir)
