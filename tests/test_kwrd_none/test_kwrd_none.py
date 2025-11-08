dd67299734674d055aa133dfd0876004a5551402
Mark Dufour <m.dufour@knmp.nl>
2025-11-08 17:04:42 +0100

    more tests and asserts in tests/test_kwrd_none

--- tests/test_kwrd_none/test_kwrd_none.py
+++ tests/test_kwrd_none/test_kwrd_none.py
@@ -6,19 +6,36 @@ def test_none():
     b = str(x)
     a = [[1]]
     c = [None, [2]]
-    assert a != c 
+
+    assert a != c
+    assert not a == c
+    assert not a is c
 
     d = [3]
     d = None
     e = [4]
     e = None
 
-    d == e
-    e == None
-    d == None
-    a == None
-    c[0] == None
-    c[1] == None
+    assert d == e
+    assert d is e
+    assert not d != e
+
+    assert e == None
+    assert e is None
+
+    assert d == None
+    assert d is None
+
+    assert not a == None
+    assert not a is None
+
+    assert c[0] == None
+    assert c[0] is None
+    assert not c[0] != None
+
+    assert c[1] != None
+    assert not c[1] == None
+    assert not c[1] is None
 
 
 def test_all():
