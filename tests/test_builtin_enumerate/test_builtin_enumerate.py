28ece23f80e6668c3854d057c3929c1012d91177
Mark Dufour <m.dufour@knmp.nl>
2025-11-08 17:00:17 +0100

    test enumerate(iter)

--- tests/test_builtin_enumerate/test_builtin_enumerate.py
+++ tests/test_builtin_enumerate/test_builtin_enumerate.py
@@ -1,13 +1,19 @@
 
 
-def test_e1():
+def test_list():
     assert [(b, a) for a, b in enumerate([1, 2, 3])] == [(1, 0), (2, 1), (3, 2)]
-    assert [(a, "%.1f" % b)for a, b in enumerate([1.1, 2.2, 3.3])] == [(0, '1.1'), (1, '2.2'), (2, '3.3')]
+    assert [(a, "%.1f" % b) for a, b in enumerate([1.1, 2.2, 3.3])] == [(0, '1.1'), (1, '2.2'), (2, '3.3')]
 
+
+def test_iter():
+    it = iter([9, 8, 7])
+    assert list(enumerate(it)) == [(0, 9), (1, 8), (2, 7)]
 
 
 def test_all():
-    test_e1()
+    test_list()
+    test_iter()
+
 
 if __name__ == '__main__':
     test_all()
