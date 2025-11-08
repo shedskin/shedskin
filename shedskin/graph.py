23de66581aedda242184e464deae8b9d30f07d44
Mark Dufour <m.dufour@knmp.nl>
2025-11-08 10:41:43 +0100

    for isinstance, do not visit arguments.
    since we just convert it to True, avoid any extra errors/warnings.

--- shedskin/graph.py
+++ shedskin/graph.py
@@ -2266,6 +2266,7 @@ class ModuleVisitor(ast_utils.BaseNodeVisitor):
                     mv=getmv(),
                     warning=True,
                 )
+                return
 
             if python.lookup_var(ident, func, getmv()):
                 self.visit(node.func, func)
