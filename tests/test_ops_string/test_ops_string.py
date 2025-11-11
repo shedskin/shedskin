c2e1d63e92108f8d460629342e993d6b230e094b
Mark Dufour <m.dufour@knmp.nl>
2025-11-11 14:30:31 +0100

    go over test_ops_string comments

--- tests/test_ops_string/test_ops_string.py
+++ tests/test_ops_string/test_ops_string.py
@@ -1,3 +1,4 @@
+# TODO %g works, just a bit differently
 
 
 def test_classic1():
@@ -5,12 +6,6 @@ def test_classic1():
     assert "%s" % "255" == '255'
     assert "%x" % 255 == 'ff'
     assert b"%c" % 6 == b'\x06'
-
-#    assert "%g" % (-496.0 / 3.0) == '-165.333'  # TODO
-#    assert "%g" % (496.0 / 3.0) == '165.333'
-#    assert "%g" % (-496.0 / -3.0) == '165.333'
-#    assert "%g" % (496.0 / -3.0) == '-165.333'
-
     assert "%.2f" % 4.1 == '4.10'
     assert "%d %x %d" % (10, 11, 12) == '10 b 12'
     assert "%d %s" % (1, "een") == '1 een'
@@ -24,7 +19,7 @@ def test_classic2():
     assert "%02x" % 0x1234 == '1234'
 
     assert "%o" % 10 == '12'
-#    assert "%.4s %.4r\n" % ("abcdefg", "\0hoplakee") == "abcd '\\x0\n"
+    assert "%.4s %.4r\n" % ("abcdefg", "\0hoplakee") == "abcd '\\x0\n"
 
     assert "?%% %c?" % 70 == '?% F?'
     assert "?%c?%%" % 0 == '?\x00?%'
@@ -35,6 +30,7 @@ def test_classic2():
     assert "aha %s %r" % (18, 19) == 'aha 18 19'
     assert "%i%%-%i%%" % (1, 2) == '1%-2%'
     assert "%i%%-%s%%" % (12, "21") == '12%-21%'
+
 
 def test_classic3():
     v = "1 %(aap)s, 1 %(aap)s, %% 2 %(bert)s.."
@@ -54,22 +50,21 @@ def test_classic3():
     t3 = (70, 71, 72, 73, 74)
     assert ("%c %d %x %s %r" % t3) == 'F 71 48 73 74'
 
-#    assert ("%(aap)s %(bert)d %% %(bert)c" % {"aap": "hallo", "bert": 72}) == 'hallo 72 % H'
+    assert ("%(aap)s %(bert)s %% %(bert)c" % {"aap": "hallo", "bert": "H"}) == 'hallo H % H'
 
 
 def test_str_precision():
     assert ('%.8s' % 'abracadabra') == 'abracada'
     assert ('%.8r' % 'abracadabra') == "'abracad"
-
-    assert ('%.8s' % b'abracadabra') == "b'abraca"
+#    assert ('%.8s' % b'abracadabra') == "b'abraca"  # TODO github issue
     assert ('%.8r' % b'abracadabra') == "b'abraca"
+
 
 def test_all():
     test_classic1()
     test_classic2()
     test_classic3()
-
-#    test_str_precision()  # TODO
+    test_str_precision()
 
 
 if __name__ == "__main__":
