d1554cd328ce50ea7b9c960457d7436d6a5a3d6a
Mark Dufour <m.dufour@knmp.nl>
2025-11-17 13:41:23 +0100

    bisect key arg

--- shedskin/lib/bisect.py
+++ shedskin/lib/bisect.py
@@ -1,22 +1,25 @@
 # Copyright 2005-2025 Mark Dufour and contributors; License Expat (See LICENSE)
 
+
 def insort_right(a, x, lo=0, hi=0, __kw_key=None):
-    insort(a, x, key=key)
+    insort(a, x, key=__kw_key)
 def insort_left(a, x, lo=0, hi=0, __kw_key=None):
-    insort(a, x, key=key)
+    insort(a, x, key=__kw_key)
 def insort(a, x, lo=0, hi=0, __kw_key=None):
     a.append(x)
     __cmp(x, x)
-    __kw_key(x)
+    unit = iter(a).__next__()
+    __cmp(__kw_key(unit), __kw_key(x))
+
 
 def bisect_right(a, x, lo=0, hi=0, __kw_key=None):
-    bisect(a, x, key=key)
+    bisect(a, x, key=__kw_key)
     return 1
 def bisect_left(a, x, lo=0, hi=0, __kw_key=None):
-    bisect(a, x, key=key)
+    bisect(a, x, key=__kw_key)
     return 1
 def bisect(a, x, lo=0, hi=0, __kw_key=None):
     __cmp(x, x)
-    __kw_key(x)
+    unit = iter(a).__next__()
+    __cmp(__kw_key(unit), x)
     return 1
-
