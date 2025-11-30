9e95103626c3d5c94b0f211f40e1ddb0da70e310
Mark Dufour <m.dufour@knmp.nl>
2025-11-30 12:05:18 +0100

    ok

--- test2.cpp
+++ test2.cpp
@@ -9,6 +9,44 @@ str *const_0;
 str *__name__;
 
 
+unsigned char *SP;
+unsigned char *SPEND;
+unsigned char stackie[1024*1024];
+
+
+class StackFrame {
+public:
+    unsigned char *__SP;
+
+    StackFrame();
+    ~StackFrame();
+
+    template<class T> T *neu();
+};
+
+
+StackFrame::StackFrame() {
+    __SP = SP;
+}
+
+StackFrame::~StackFrame() {
+    SP = __SP;
+}
+
+template<class T> T *StackFrame::neu() {
+    unsigned char *mymem = SP;
+
+    size_t sz = sizeof(T);
+
+    if(SP + sz >= SPEND) { // DEZE CHECKT MAAKT ALLES 10 KEER TRAGER :S
+        printf("DOE HEAP!!");
+        return 0;
+    } else {
+        SP += sz;
+        return (T *)mymem; // reinterpret or whatever?
+    }
+}
+
 
 __ss_int woef(__ss_int x, __ss_int y, __ss_int z) {
     tuple<__ss_int> *v;
@@ -34,6 +72,9 @@ void __init() {
     const_0 = new str("__main__");
 
     __name__ = new str("__main__");
+
+    SP = stackie;
+    SPEND = stackie+1024*1024;
 
     if (__eq(__test2__::__name__, const_0)) {
         __ss_main();
