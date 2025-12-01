300512b5517ea279c9abfa260ad9c5b87bdf251e
Mark Dufour <m.dufour@knmp.nl>
2025-12-01 14:15:38 +0100

    test4

--- test4.py
+++ test4.py
@@ -1,16 +1,18 @@
 
 
-def f(a, b):
+def f(a, b):  # a, b escape, v doesn't
     global g
     g = b
 
     v = 'haha' # doesn't escape
-    return a
+    return a # escapes one level
 
 
-def g():
-    c = 18 # global escape
+def g():  # c escapes via g, j doesn't escape
+    c = 18
     f(1, c)
 
+    for j in range(10):
+        pass
 
 g()
