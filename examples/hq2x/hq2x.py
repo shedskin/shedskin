091bbca6e3c6b34937bd7863e91d3caa4ff96101
Mark Dufour <m.dufour@knmp.nl>
2025-12-15 18:22:23 +0100

    examples/hq2x

--- examples/hq2x/hq2x.py
+++ examples/hq2x/hq2x.py
@@ -1575,9 +1575,9 @@ def init_LUTs():
 
 def main():
     init_LUTs()
-    print('scaling randam.ppm to randam2.ppm (100 times)..')
+    print('scaling randam.ppm to randam2.ppm (1000 times)..')
     ppm = PPM.load('randam.ppm')
-    for i in range(100):
+    for i in range(1000):
         rgb = hq2x(ppm.w, ppm.h, ppm.rgb)
     PPM(2*ppm.w, 2*ppm.h, rgb).save('randam2.ppm')
 
