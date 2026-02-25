5936bdc787eb29587b850aedec53307dd1847ea2
Mark Dufour <m.dufour@knmp.nl>
2026-02-25 10:52:40 +0100

    math.integer: redirect functions, add tests

---
+++ shedskin/lib/math/integer.cpp
@@ -0,0 +1,16 @@
+/* Copyright 2026 Mark Dufour and contributors; License Expat (See LICENSE) */
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include "integer.hpp"
+
+namespace __math__ {
+namespace __integer__ {
+
+void __init() {
+}
+
+} // __integer__
+} // __math__
+
