a8e9028ac9444baa6f24307fc88925e04966220b
Mark Dufour <m.dufour@knmp.nl>
2026-02-27 14:06:45 +0100

    math.integer: actually add lib/math/integer.py

---
+++ shedskin/lib/math/integer.py
@@ -0,0 +1,8 @@
+# Copyright 2026 Mark Dufour and contributors; License Expat (See LICENSE)
+
+def comb(n, k): return 1
+def factorial(x): return 1
+def gcd(*args): return 1
+def lcm(*args): return 1
+def isqrt(x): return 1
+def perm(n, k=None): return 1
