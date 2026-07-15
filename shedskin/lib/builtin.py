08befba3fa0282576a7d3cb9a3237f403f599fbd
Mark Dufour <m.dufour@knmp.nl>
2026-07-15 07:35:14 +0200

    now make max(default) a template arg
    and use constexpr to check if it was passed

--- shedskin/lib/builtin.py
+++ shedskin/lib/builtin.py
@@ -1131,11 +1131,11 @@ def range(a, b=1, s=1):  # TODO use class directly
 
 __void = 0
 
-def max(__kw_key=__void, __kw_default=__void, *arg):
+def max(__kw_key=0, __kw_default=__void, *arg):  # 0
     __cmp(arg, arg)
     __kw_key(arg)
     return arg
-def __max1(arg, __kw_key=__void, __kw_default=__void):
+def __max1(arg, __kw_key=0, __kw_default=__void):
     elem = iter(arg).__next__()
     __cmp(elem, elem)
     __kw_key(elem)
@@ -1145,7 +1145,7 @@ def min(__kw_key=0, *arg): # XXX 0
     __cmp(arg, arg)
     __kw_key(arg)
     return arg
-def __min1(arg, __kw_key=0, __kw_default=0):
+def __min1(arg, __kw_key=0):
     elem = iter(arg).__next__()
     __cmp(elem, elem)
     __kw_key(elem)
