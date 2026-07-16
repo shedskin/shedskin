5ea080d8c7186faf9fd9cb27ddd3cb335030c677
Mark Dufour <m.dufour@knmp.nl>
2026-07-16 21:40:24 +0200

    colorsys fix
    claude

--- shedskin/lib/colorsys.cpp
+++ shedskin/lib/colorsys.cpp
@@ -30,16 +30,16 @@ __ss_float ONE_SIXTH, ONE_THIRD, TWO_THIRD;
 
 tuple2<__ss_float, __ss_float> *rgb_to_yiq(__ss_float r, __ss_float g, __ss_float b) {
     __ss_float y = (((0.3*r)+(0.59*g))+(0.11*b));
-    __ss_float i = (((0.6*r)-(0.28*g))-(0.32*b));
-    __ss_float q = (((0.21*r)-(0.52*g))+(0.31*b));
+    __ss_float i = ((0.74*(r-y))-(0.27*(b-y)));
+    __ss_float q = ((0.48*(r-y))+(0.41*(b-y)));
 
     return (new tuple2<__ss_float, __ss_float>(3,y,i,q));
 }
 
 tuple2<__ss_float, __ss_float> *yiq_to_rgb(__ss_float y, __ss_float i, __ss_float q) {
-    __ss_float r = std::clamp((y+(0.948262*i))+(0.624013*q), 0., 1.);
-    __ss_float g = std::clamp((y-(0.276066*i))-(0.63981*q), 0., 1.);
-    __ss_float b = std::clamp((y-(1.10545*i))+(1.72986*q), 0., 1.);
+    __ss_float r = std::clamp((y+(0.9468822170900693*i))+(0.6235565819861433*q), 0., 1.);
+    __ss_float g = std::clamp((y-(0.27478764629897834*i))-(0.6356910791873801*q), 0., 1.);
+    __ss_float b = std::clamp((y-(1.1085450346420322*i))+(1.7090069284064666*q), 0., 1.);
 
     return (new tuple2<__ss_float, __ss_float>(3,r,g,b));
 }
