03adc3e9f1d56e5aa113d87bb432338eddb923df
Mark Dufour <m.dufour@knmp.nl>
2026-07-28 12:28:15 +0200

    remove partial __all__ stuff

--- shedskin/lib/configparser.cpp
+++ shedskin/lib/configparser.cpp
@@ -97,7 +97,6 @@ namespace __configparser__ {
 tuple<str *> *const_2;
 str *const_0, *const_1, *const_10, *const_11, *const_12, *const_13, *const_14, *const_15, *const_16, *const_17, *const_18, *const_19, *const_20, *const_21, *const_22, *const_23, *const_24, *const_25, *const_26, *const_27, *const_28, *const_29, *const_3, *const_30, *const_31, *const_32, *const_33, *const_34, *const_35, *const_36, *const_37, *const_38, *const_39, *const_4, *const_40, *const_41, *const_42, *const_43, *const_44, *const_45, *const_46, *const_47, *const_48, *const_5, *const_50, *const_51, *const_52, *const_53, *const_6, *const_7, *const_8, *const_9;
 
-list<str *> *__all__;
 str *DEFAULTSECT, *__name__;
 __ss_int MAX_INTERPOLATION_DEPTH;
 
@@ -958,7 +957,6 @@ void __init() {
     cl_ConfigParser = new class_("ConfigParser");
     ConfigParser::_KEYCRE = __re__::compile(const_39);
 
-    __all__ = (new list<str *>(12, const_40, const_41, const_42, const_43, const_44, const_45, const_46, const_47, const_48, const_50, const_51, const_52));
     DEFAULTSECT = const_53;
     MAX_INTERPOLATION_DEPTH = 10;
     default_0 = const_17;
