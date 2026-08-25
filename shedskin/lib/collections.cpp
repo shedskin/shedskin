40532ec212c3e92f07cd745853629f12d7ac0d59
Mark Dufour <m.dufour@knmp.nl>
2026-08-25 14:47:21 +0200

    collections.Counter

--- shedskin/lib/collections.cpp
+++ shedskin/lib/collections.cpp
@@ -7,11 +7,13 @@ namespace __collections__ {
 str *__name__;
 class_ *cl_deque;
 class_ *cl_defaultdict;
+class_ *cl_counter;
 
 void __init() {
     __name__ = new str("collections");
     cl_deque = new class_("deque");
     cl_defaultdict = new class_("defaultdict");
+    cl_counter = new class_("Counter");
 
 
 }
