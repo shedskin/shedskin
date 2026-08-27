64d5cf610df6c0705b9aef01ad800953f5f3ba33
Mark Dufour <m.dufour@knmp.nl>
2026-08-27 07:57:15 +0200

    0.9.13-pre1

--- shedskin/__init__.py
+++ shedskin/__init__.py
@@ -593,7 +593,7 @@ class Shedskin:
 
         ss = cls(args)
 
-        ss.log.info("*** SHED SKIN Python-to-C++ Compiler 0.9.13 ***")
+        ss.log.info("*** SHED SKIN Python-to-C++ Compiler 0.9.13-pre1 ***")
         ss.log.info(
             "Copyright 2005-2026 Mark Dufour and contributors; License GNU GPL version 3 (See LICENSE)"
         )
